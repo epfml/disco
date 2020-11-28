96cb7d1e3b2df9ece5e3bb5d7230633083242a57
giorgio <giorgiosaza@gmail.com>
2020-11-28 17:40:38 +0100

    exchange model with tf.save

--- peerjs-backend/peer.js
+++ peerjs-backend/peer.js
@@ -17,13 +17,13 @@ class PeerJS {
         this.handle_data = handle_data
         this.handle_data_args = handle_data_args
 
-        console.log(local_peer)
+        console.log("peer", local_peer)
         this.local_peer.on("connection", (conn) => {
             console.log("new connection")
-            conn.on("data", (data) => {
+            conn.on("data", async (data) => {
                 this.data = data
                 this.new_data = true
-                this.handle_data(data, ...this.handle_data_args)
+                await this.handle_data(data, ...this.handle_data_args)
             })
         })
     }
@@ -40,5 +40,69 @@ class PeerJS {
         this.new_data = false
         return this.data
     }
+}
+
+class ModelStorage {
+
+    static get BASEDIR() {
+        return "tensorflowjs_models"
+    }
+
+    static get FILENAMES() {
+        return [
+            "info",
+            "model_metadata",
+            "model_topology",
+            "weight_data",
+            "weight_specs"
+        ]
+    }
+
+    static async store(model, name) {
+        await model.save("localstorage://" + name)
+    }
+
+    static inject(model_data, name) {
+        for(var i = 0; i < this.FILENAMES; i++) {
+            var key = this.FILENAMES[i]
+            var fpath = this.BASEDIR + '/' + name + '/' + key
+            var content = model_data[key]
+            localStorage.setItem(fpath, content)
+        }
+    }
+
+    static get_serialized_model(name) {
+        var serialized = {}
+        for(var i = 0; i < this.FILENAMES.length; i++) {
+            var key = this.FILENAMES[i]
+            var fpath = this.BASEDIR + '/' + name + '/' + key
+
+            var model_data = localStorage.getItem(fpath) // this is a JSON string
+            serialized[key] = model_data
+        }
+        serialized.name = name
+        return serialized
+    }
+
+}
+
+async function send_model(model, peerjs, receiver, name) {
+    await ModelStorage.store(model, name)
+    var serialized = ModelStorage.get_serialized_model(name)
+    const send_data = {
+        cmd_code    : CMD_CODES.MODEL_INFO,
+        payload     : serialized
+    }
+    console.log("Sending data")
+    console.log(send_data)
+    peerjs.send(receiver, send_data)
+}
+
+async function load_model(model_data) {
+    var name = model_data.name
+    ModelStorage.inject(model_data, name)
+    const model = await tf.loadLayersModel("localstorage://" + name)
+    
+    return model
 }
 
