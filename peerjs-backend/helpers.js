1f47968870882680e7abc787d86b6cf9486c8f77
giorgio <giorgiosaza@gmail.com>
2020-12-02 12:27:06 +0100

    cleanup

--- peerjs-backend/helpers.js
+++ peerjs-backend/helpers.js
@@ -60,3 +60,15 @@ function* labelGenerator() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+// for random string
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
