459d963dfcfa6596f017912badbb0828183e7f45
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-03-09 10:41:24 +0100

    Adding Use model screen draft

---
+++ mobile-browser-based-version/mobile-app/src/components/UseImageModel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>Use {{task_name}} model</h1>
+    <input type="file">
+    <button>
+        Test
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UseImageModel',
+  props: {
+    msg: String,
+    task_name: String,
+    task_labels: Array[String],
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
