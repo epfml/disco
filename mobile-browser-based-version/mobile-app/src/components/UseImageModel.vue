e91076305320055247f3ea691a2c93360bc2c3d7
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-03-11 15:32:12 +0100

    Add Tailwind to project

--- mobile-browser-based-version/mobile-app/src/components/UseImageModel.vue
+++ mobile-browser-based-version/mobile-app/src/components/UseImageModel.vue
@@ -1,21 +1,54 @@
 <template>
   <div class="hello">
-    <h1>Use {{task_name}} model</h1>
-    <input type="file">
-    <button>
+    <h1>Use {{TaskStore.task_name}} model</h1>
+    <base-image-input />
+    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" v-on:click="test">
         Test
     </button>
+    <h3>
+      {{predictedValue}}
+    </h3>
   </div>
 </template>
 
 <script>
+import * as tf from "@tensorflow/tfjs"
+import * as imageRec from "@tensorflow-models/mobilenet"
+import TaskStore from '../store/taskStore'
+import BaseImageInput from '../components/BaseImageInput'
+import ImageStore from '../store/imageStore'
 export default {
   name: 'UseImageModel',
   props: {
-    msg: String,
-    task_name: String,
-    task_labels: Array[String],
+    msg: String
+  },
+  data(){
+    return{
+      predictedValue:'Click on test!',
+      TaskStore: TaskStore.data,
+      ImageStore: ImageStore.data,
+      tensorflowModel: null,
+    }
+  },
+  methods:{
+    async test(){
+      this.tensorflowModel = await imageRec.load()
+      console.log("Testing")
+      console.log(this.ImageStore.image)
+      const prediction =  await this.tensorflowModel.classify(this.ImageStore.image);
+      console.log(prediction)
+      this.predictedValue = prediction
+    }
+  },
+  components:{
+    BaseImageInput
   }
+}
+
+const loadModel = async ()=>{
+  const recognizer = await imageRec.load();
+  console.log("model loaded")
+  this.tensorflowModel = recognizer
 }
 </script>
 
