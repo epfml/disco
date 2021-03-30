4cc3cff1c53a2b3873b0b76713c45f618b1dbbeb
PaulMansat <paul.mansat@epfl.ch>
2021-03-30 18:52:34 +0200

    Update Architecture
    - Fix error model loading in the training script. When a model si created in a script with a call to the tensor library and is then re-used in another script with another tensorflow import command, we need to re-load the model into the new tensorflow environment by calling load model.
    - Updated model storage handling: when a user joins a task, the model is created (locally) and stored into the browser’s local storage. When the user trains the model, the training function loads the model stored into the user’s local storage and updates it at the end of the training.
    - Updated Architecture for Titanic dataset with new defined standards

--- mobile-browser-based-version/mobile-app/src/helpers/training-script.js
+++ mobile-browser-based-version/mobile-app/src/helpers/training-script.js
@@ -1,24 +1,49 @@
-async function training(model, trainData, labels, batchSize, validationSplit, trainEpochs, updateUI){
+import * as tf from '@tensorflow/tfjs';
 
-    await model.fit(trainData, labels, {
-        batchSize,
-        validationSplit,
-        epochs: trainEpochs,
-        callbacks: {
-          onEpochEnd: async (epoch, logs) => {
-            valAcc = logs.val_acc;
-            document.getElementById("val_accuracy").textContent = valAcc
-            
-            if (onIteration) {
-              onIteration('onEpochEnd', epoch, logs);
-            }
+/**
+ * Trains the model given as argument
+ * @param {String} model_name the name of the model used to store the model in memory
+ * @param {Tensor} trainData the tensor holding the training data
+ * @param {1D-Tensor} labels the tensor holding the labels
+ * @param {Number} _batchSize the batch size used for training
+ * @param {Number} _validationSplit the size of the validation set
+ * @param {Number} _epochs the number of epochs used for training
+ * @param {function} updateUI a function called to update the UI to give feedbacks on the training
+ */
+export default async function training(model_name, trainData, labels, _batchSize, _validationSplit, _epochs, updateUI) {
+  console.log('Start Training')
+  console.log(labels)
 
-            updateUI(epoch+1,(logs.acc * 100).toFixed(2) ,(logs.val_acc * 100).toFixed(2))
-            
-            await tf.nextFrame();
-          }
-        }
-    });
+  const model_path = "localstorage://".concat(model_name);
+  const loadedModel = await tf.loadLayersModel(model_path);
 
-    // TODO: Enter distributed training loop
+  // TO DO: access these parameters as training argument
+  loadedModel.compile({
+    optimizer: "rmsprop",
+    loss: "binaryCrossentropy",
+    metrics: ["accuracy"],
+  });
+
+  await loadedModel.fit(trainData, labels, {
+    batchSize: _batchSize,
+    epochs: _epochs,
+    validationSplit: _validationSplit,
+    callbacks: {
+      onEpochEnd: async (epoch, logs) => {
+        updateUI(epoch + 1, (logs.acc * 100).toFixed(2), (logs.val_acc * 100).toFixed(2))
+        console.log(`EPOCH (${epoch + 1}): Train Accuracy: ${(
+          logs.acc * 100
+        ).toFixed(2)},
+             Val Accuracy:  ${(
+            logs.val_acc * 100
+          ).toFixed(2)}\n`);
+        //await tf.nextFrame();
+      }
+    }
+  });
+
+  const saveResults = await loadedModel.save(model_path);
+
+
+  // TODO: Enter distributed training loop
 }
