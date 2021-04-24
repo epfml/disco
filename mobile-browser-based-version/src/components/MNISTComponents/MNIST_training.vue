9b3cbead39f319541acbb4e6f262ffc57624976f
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-04-24 10:34:00 +0200

    Simplify MNIST testing component

--- mobile-browser-based-version/src/components/MNISTComponents/MNIST_training.vue
+++ mobile-browser-based-version/src/components/MNISTComponents/MNIST_training.vue
@@ -74,6 +74,7 @@
             }}</span>
           </div>
 
+          <!-- Data Point Example -->
           <img src="../../../example_training_data/9-mnist-example.png" alt="">
         </div>
       </div>
@@ -332,7 +333,8 @@ import * as tf from "@tensorflow/tfjs";
 
 import * as Chart from "chart.js";
 import {training, training_distributed} from "../../helpers/training-script.js"
-import { onEpochEnd_common } from "../../helpers/helpers";
+import {onEpochEnd_common } from "../../helpers/helpers";
+import {create_model} from "./MNIST_script"
 
 import ImageUploadFrame from '../ImageUploadFrame'
 
@@ -355,9 +357,9 @@ var model_train_data = {
 //TODO
 const batchSize = 320;
 
-      const validationSplit = 0.15;
+const validationSplit = 0.15;
     
-      const trainEpochs = 10
+const trainEpochs = 10
 export default {
   data() {
     return {
@@ -548,80 +550,6 @@ export default {
     goToTesting() {
       this.$router.push({ path: "/MNIST-model/testing" });
     },
-
-    /**
-     * Creates the tf.model
-     * TO DO: fetch model from local storage
-     * @returns Returns a tf.model for training
-     */
-    get_model() {
-      if(!this.model){
-        this.model = this.create_model()
-      }
-      return this.model
-    },
-
-    create_model(){
-      return this.createConvModel()
-    },
-    /**
-     * Creates a convolutional neural network (Convnet) for the MNIST data.
-     *
-     * @returns {tf.Model} An instance of tf.Model.
-     */
-    createConvModel(){
-      const IMAGE_H = 28;
-      const IMAGE_W = 28;
-      // Create a sequential neural network model. tf.sequential provides an API
-      // for creating "stacked" models where the output from one layer is used as
-      // the input to the next layer.
-      const model = tf.sequential();
-    
-      // The first layer of the convolutional neural network plays a dual role:
-      // it is both the input layer of the neural network and a layer that performs
-      // the first convolution operation on the input. It receives the 28x28 pixels
-      // black and white images. This input layer uses 16 filters with a kernel size
-      // of 5 pixels each. It uses a simple RELU activation function which pretty
-      // much just looks like this: __/
-      model.add(tf.layers.conv2d({
-        inputShape: [IMAGE_H, IMAGE_W, 3],
-        kernelSize: 3,
-        filters: 16,
-        activation: 'relu'
-      }));
-    
-      // After the first layer we include a MaxPooling layer. This acts as a sort of
-      // downsampling using max values in a region instead of averaging.
-      // https://www.quora.com/What-is-max-pooling-in-convolutional-neural-networks
-      model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
-    
-      // Our third layer is another convolution, this time with 32 filters.
-      model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
-    
-      // Max pooling again.
-      model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
-    
-      // Add another conv2d layer.
-      model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
-    
-      // Now we flatten the output from the 2D filters into a 1D vector to prepare
-      // it for input into our last layer. This is common practice when feeding
-      // higher dimensional data to a final classification output layer.
-      model.add(tf.layers.flatten({}));
-    
-      model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-    
-      // Our last layer is a dense layer which has 10 output units, one for each
-      // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9). Here the classes actually
-      // represent numbers, but it's the same idea if you had classes that
-      // represented other entities like dogs and cats (two output classes: 0, 1).
-      // We use the softmax function as the activation for the output layer as it
-      // creates a probability distribution over our 10 classes so their output
-      // values sum to 1.
-      model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
-    
-      return model;
-    },
   },
   async mounted() {
     // This method is called when the component is created
@@ -642,7 +570,7 @@ export default {
       this.DataExample = display_informations.dataExample
 
       // Create the model 
-      model = this.create_model();
+      model = create_model();
       const save_path = "localstorage://".concat(this.model_name);
       const saveResults = await model.save(save_path);
 
