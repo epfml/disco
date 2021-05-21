30b4d71d42d44097cb6586eeab26d97f40b3f152
PaulMansat <paul.mansat@epfl.ch>
2021-05-21 14:12:26 +0200

    Add changes requested for modularisation
    - rename the model memory to “My Model Library”
    - change "titanic model information" to "titanic task information"
    - add Node version to the readme
    - naming convention update (snake case)
    - folder name (no space, respect the snake case convention)
    - remove peers server from readme & add the new peer credentials (test with gcloud, martin m has set up credentials already)

---
+++ mobile-browser-based-version/src/components/main_frames/MainDescriptionFrame.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="flex flex-col pt-4 items-right justify-start flex-1 h-full min-h-screen p-4 overflow-x-hidden overflow-y-auto"
+  >
+    <div>
+      <DescriptionFrame
+        v-bind:OverviewText="OverviewText"
+        v-bind:LimitationsText="LimitationsText"
+        v-bind:TradeOffsText="TradeOffsText"
+        v-bind:Id="ModelId"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import DescriptionFrame from "../building_frames/DescriptionFrame";
+
+export default {
+  name: 'MainDescriptionFrame', 
+  props: {
+    Id: String,
+    Task: Object,
+  },
+  data() {
+    return {
+      OverviewText: "",
+      LimitationsText: "",
+      TradeOffsText: "",
+      ModelId: "",
+    };
+  },
+  components: {
+    DescriptionFrame,
+  },
+  mounted() {
+    this.OverviewText = this.Task.displayInformation.overview;
+    this.LimitationsText = this.Task.displayInformation.limitations;
+    this.TradeOffsText = this.Task.displayInformation.tradeoffs;
+    this.ModelId = this.Task.trainingInformation.modelId;
+  },
+};
+</script>
