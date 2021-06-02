290853749efb6b288be776c294b61380771b3259
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-06-02 10:38:55 +0200

    Fix default threshold

--- mobile-browser-based-version/src/helpers/communication_script/helpers.js
+++ mobile-browser-based-version/src/helpers/communication_script/helpers.js
@@ -184,7 +184,7 @@ export async function onEpochEndCommon(model, epoch, receivers, recvBuffer, user
     const serializedWeights = await serializeWeights(model)
     var epochWeights = { epoch: epoch, weights: serializedWeights }
 
-    if(threshold !== undefined){
+    if(threshold == undefined){
         threshold = 1
     }
 
