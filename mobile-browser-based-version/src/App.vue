8ae556341357388c98f4f15e97dbfc13d7f0e133
Martin Milenkoski <martinmilenkoski53@gmail.com>
2021-09-06 18:44:33 +0200

    Go to testing page when model is clicked in library

--- mobile-browser-based-version/src/App.vue
+++ mobile-browser-based-version/src/App.vue
@@ -543,7 +543,7 @@
                     <div
                       class="flex items-center grid-cols-3 justify-between px-4 py-2 space-x-4 transition-colors border rounded-md hover:text-gray-900 hover:border-gray-900 dark:border-primary dark:hover:text-primary-100 dark:hover:border-primary-light focus:outline-none focus:ring focus:ring-primary-lighter focus:ring-offset-2 dark:focus:ring-offset-dark dark:focus:ring-primary-dark"
                     >
-                      <div class='w-2/3'>
+                      <div class="cursor-pointer w-2/3" v-on:click="openTesting(item[1].name)">
                         <span>
                           {{ item[1].name.substr(12) }} <br />
                           <span class="text-xs">
@@ -761,6 +761,13 @@ export default {
       console.log(modelName);
       this.modelMap.delete(modelName);
       await tf.io.removeModel(modelName);
+    },
+
+    async openTesting(modelName) {
+      if (modelName.includes('_')) {
+        const task = modelName.split('_').pop();
+        this.$router.push({name: task.concat('.testing')})
+      }
     },
 
     async downloadModel(modelName) {
