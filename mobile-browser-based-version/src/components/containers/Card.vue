d173d0963649515c10572a169cae07561937493d
Lucas Giordano <giordano3102lucas@gmail.com>
2021-11-05 14:27:57 +0100

    refactor Information.vue

--- mobile-browser-based-version/src/components/containers/Card.vue
+++ mobile-browser-based-version/src/components/containers/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Titanic's card-->
   <div
-    class="group flex-col items-center justify-between p-4 bg-white rounded-md dark:bg-darker dark:bg-dark"
+    v-bind:class="'group flex-col items-center justify-between p-4 bg-white rounded-md dark:bg-darker dark:bg-dark ' + customClass"
   >
     <slot></slot>
   </div>
@@ -10,5 +10,8 @@
 <script>
 export default {
   name: "card",
+  props: {
+    customClass: { default: "", type: String },
+  },
 };
 </script>
