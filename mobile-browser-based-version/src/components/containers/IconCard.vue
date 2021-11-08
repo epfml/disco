a78b71c05c699b274a2b7a6ce26e4e558662cad7
Lucas Giordano <giordano3102lucas@gmail.com>
2021-11-08 21:28:55 +0100

    All svg files are stored in assets/svg. Should use vue-inline-svg to read them

---
+++ mobile-browser-based-version/src/components/containers/IconCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <!--  Card -->
+  <a id="overview-target">
+    <div class="grid grid-cols-1 p-4 space-y-8 lg:gap-8">
+      <div class="col-span-1 bg-white rounded-lg dark:bg-darker">
+        <!-- Card header -->
+        <div
+          class="flex items-center justify-between p-4 border-b dark:border-primary"
+        >
+          <h4 class="text-lg font-semibold text-gray-500 dark:text-light">
+            {{ descripton }}
+          </h4>
+          <div class="flex items-center">
+            <span aria-hidden="true">
+              <inline-svg :src="require(`../../assets/svg/${icon}.svg`)" />
+            </span>
+          </div>
+        </div>
+        <!-- Descrition -->
+        <div class="relative p-4 overflow-x-scroll">
+          <span class="text-sm text-gray-500 dark:text-light">{{
+            descripton
+          }}</span>
+        </div>
+        <!-- Extra -->
+        <slot name="extra" v-if="hasExtraSlot"></slot>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "icon-card",
+  props: {
+    header: { type: String },
+    descripton: { type: String },
+    icon: { type: String },
+    customClass: { default: "", type: String },
+  },
+  computed: {
+    hasExtraSlot() {
+      return this.$slots.extra;
+    },
+  },
+};
+</script>
