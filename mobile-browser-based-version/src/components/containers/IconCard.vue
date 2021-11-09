e5e7b72bf1fc0e58065d25a2fad30deb96894231
Lucas Giordano <giordano3102lucas@gmail.com>
2021-11-09 12:51:24 +0100

    fix dislplay bugs (svg not working yet)

--- mobile-browser-based-version/src/components/containers/IconCard.vue
+++ mobile-browser-based-version/src/components/containers/IconCard.vue
@@ -1,43 +1,43 @@
 <template>
   <!--  Card -->
-  <a id="overview-target">
-    <div class="grid grid-cols-1 p-4 space-y-8 lg:gap-8">
-      <div class="col-span-1 bg-white rounded-lg dark:bg-darker">
-        <!-- Card header -->
-        <div
-          class="flex items-center justify-between p-4 border-b dark:border-primary"
-        >
-          <h4 class="text-lg font-semibold text-gray-500 dark:text-light">
-            {{ descripton }}
-          </h4>
-          <div class="flex items-center">
-            <span aria-hidden="true">
-              <inline-svg :src="require(`../../assets/svg/${icon}.svg`)" />
-            </span>
-          </div>
+  <div class="grid grid-cols-1 p-4 space-y-8 lg:gap-8">
+    <div class="col-span-1 bg-white rounded-lg dark:bg-darker">
+      <!-- Card header -->
+      <div
+        class="flex items-center justify-between p-4 border-b dark:border-primary"
+      >
+        <h4 class="text-lg font-semibold text-gray-500 dark:text-light">
+          {{ header }}
+        </h4>
+        <div class="flex items-center">
+          <span aria-hidden="true">
+            <img :src="`../../assets/svg/${icon}.svg`" />
+          </span>
         </div>
-        <!-- Descrition -->
-        <div class="relative p-4 overflow-x-scroll">
-          <span class="text-sm text-gray-500 dark:text-light">{{
-            descripton
-          }}</span>
-        </div>
-        <!-- Extra -->
-        <slot name="extra" v-if="hasExtraSlot"></slot>
       </div>
+      <!-- Descrition -->
+      <div class="relative p-4 overflow-x-scroll">
+        <span class="text-sm text-gray-500 dark:text-light">
+          {{ description }}
+        </span>
+      </div>
+      <!-- Extra -->
+      <slot name="extra" v-if="hasExtraSlot"></slot>
     </div>
-  </a>
+  </div>
 </template>
 
 <script>
+import InlineSvg from "vue-inline-svg";
 export default {
   name: "icon-card",
   props: {
     header: { type: String },
-    descripton: { type: String },
+    description: { type: String },
     icon: { type: String },
     customClass: { default: "", type: String },
   },
+  component: { InlineSvg },
   computed: {
     hasExtraSlot() {
       return this.$slots.extra;
--- mobile-browser-based-version/src/main.js
+++ mobile-browser-based-version/src/main.js
@@ -5,12 +5,10 @@ import "@/assets/css/tailwind.css";
 import "@/assets/css/styles.css";
 import Toaster from "@meforma/vue-toaster";
 import { store } from "./store/store";
-import { InlineSvgPlugin } from "vue-inline-svg";
 
 const app = createApp(App);
 
 app.use(store);
-app.use(InlineSvgPlugin);
 
 app
   .use(Toaster)
