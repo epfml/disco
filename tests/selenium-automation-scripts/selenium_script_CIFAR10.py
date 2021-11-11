d349322cbb1b5bef1e87200a4b8e10906011a92e
Your laurynas.lopata@epfl.ch <laurynas.lopata@epfl.ch>
2021-11-11 16:24:33 +0100

    Refactored and modularised simulation code for CIFAR10

--- tests/selenium-automation-scripts/selenium_script_CIFAR10.py
+++ tests/selenium-automation-scripts/selenium_script_CIFAR10.py
@@ -12,19 +12,14 @@ How to run:
 python selenium_script_CIFAR10.py  .py
 """
 
-import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 import os
-import math
-import csv
 from webdriver_manager.chrome import ChromeDriverManager
 
-from util import calculate_epoch_per_second
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
+from util import calculate_epoch_per_second, create_csv, find_task_page, get_files, img_csv_partition, img_csv_r_parirtion, img_csv_s_parirtion, print_train_acc, read_csv, start_training
 
 
 #Platform
@@ -33,7 +28,7 @@ PLATFORM = 'https://epfml.github.io/DeAI/#' #"https://epfml.github.io/DeAI/#/" f
 NUM_PEERS  = 2
 # Defines the way to split the data, could be 'partition' for even size partitions, 'rpartition' for random size partitions
 # spartition for parition of size passed as an argument RATIOS.
-DATA_SPLIT = 'iid'
+DATA_SPLIT = 'spartition'
 RATIOS = [5/7, 2/7]
 # Should match the name of the task in the task list and is case sensitive
 TASK_NAME = 'CIFAR10'
@@ -42,136 +37,27 @@ TRAINING_TYPE = 'Train Distributed'
 # paths to the file containing the CSV file of Titanic passengers with 12 columns
 IMAGE_FILE_PATH = r'preprocessed_images/CIFAR10'
 LABEL_FILE_PATH = 'labels.csv'
-NUM_IMAGES = 15
+NUM_IMAGES = 10
 
 
 # Download and extract chromedriver from here: https://sites.google.com/a/chromium.org/chromedriver/downloads
 # Not neccesary after ChromeDriverManager was imported
 op = webdriver.ChromeOptions()
-# op.add_argument('headless') 
-# d = DesiredCapabilities.CHROME
-# d['loggingPrefs'] = { 'browser':'ALL' }
+op.add_argument('headless') 
 # You can add options=op for chrome headless mode
 # drivers = [webdriver.Chrome(ChromeDriverManager().install(), options=op) for i in range(NUM_PEERS)]
 drivers = [webdriver.Chrome(ChromeDriverManager().install()) for i in range(NUM_PEERS)]
 start_time = time.time()
-
-def get_files(directory, num_images):
-    files = []
-    for dirpath,_,filenames in os.walk(directory):
-        for f in filenames:
-            if '.png' in f:
-                files.append(os.path.abspath(os.path.join(dirpath, f)))
-    return files[:num_images]
-
-def read_csv(file_path):
-    with open(file_path, newline='') as f:
-        reader = csv.reader(f)
-        results = dict(reader) 
-    return results
-
-def create_csv(dic, name):
-    with open(name, 'w') as f:
-        for key in dic.keys():
-            f.write(f"{key},{dic[key]}\n")
-
-def partition(list_in, n):
-    x = list(enumerate(list_in))
-    random.shuffle(x)
-    indices, ls = zip(*x)
-    labels = read_csv(LABEL_FILE_PATH)
-
-    for i in range(n):
-        temp_dic = {str(k): labels[str(k)] for k in indices[i::n]}
-        create_csv(temp_dic, str(i) + '_partition.csv')
-    return [(ls[i::n]) for i in range(n)]
-
-def r_parirtion(list_in, n):
-    x = list(enumerate(list_in))
-    random.shuffle(x)
-    indices, ls = zip(*x)
-    labels = read_csv(LABEL_FILE_PATH)
-    partition_indices = []
-    indices_out = [] 
-    list_out = []
-    for _ in range(n - 1):
-        rand_index = random.randint(1, len(list_in) - 1)
-        while rand_index in partition_indices:
-            rand_index = random.randint(1, len(list_in) - 1)
-        partition_indices.append(rand_index)
-    partition_indices = sorted(partition_indices)
-
-    for i in range(len(partition_indices)):
-        if i == 0:
-            list_out.append(ls[0:partition_indices[i]])
-            indices_out.append(indices[0:partition_indices[i]])
-        else:
-            list_out.append(ls[partition_indices[i - 1]:partition_indices[i]])
-            indices_out.append(indices[partition_indices[i - 1]:partition_indices[i]])
-
-    list_out.append(ls[partition_indices[len(partition_indices) - 1]:])
-    indices_out.append(indices[partition_indices[len(partition_indices) - 1]:])
-
-    for i in range(n):
-        temp_dic = {str(k): labels[str(k)] for k in indices_out[i]}
-        create_csv(temp_dic, str(i) + '_partition.csv')
-
-    return list_out
-
-def s_parirtion(list_in, ratios):
-    x = list(enumerate(list_in))
-    random.shuffle(x)
-    indices, ls = zip(*x)
-    labels = read_csv(LABEL_FILE_PATH)
-    partition_indices = []
-    indices_out = [] 
-    list_out = []
-    for i in range(len(ratios)):
-        curr_slice_index = math.ceil(ratios[i] * len(ls))
-        if i == 0:
-            partition_indices.append(int(curr_slice_index))
-        else:
-            partition_indices.append(int(partition_indices[i - 1] + curr_slice_index))
-    for i in range(len(partition_indices)):
-        if i == 0:
-            list_out.append(ls[0:partition_indices[i]])
-            indices_out.append(indices[0:partition_indices[i]])
-        else:
-            list_out.append(ls[partition_indices[i - 1]:partition_indices[i]])
-            indices_out.append(indices[partition_indices[i - 1]:partition_indices[i]])
-    for i in range(len(indices_out)):
-        temp_dic = {str(k): labels[str(k)] for k in indices_out[i]}
-        create_csv(temp_dic, str(i) + '_partition.csv')
-    return list_out
  
 if DATA_SPLIT == 'partition':
-    partitions = partition(get_files(IMAGE_FILE_PATH, NUM_IMAGES), NUM_PEERS)
+    partitions = img_csv_partition(get_files(IMAGE_FILE_PATH, NUM_IMAGES, '.png'), NUM_PEERS, LABEL_FILE_PATH)
 elif DATA_SPLIT == 'spartition':
-    partitions = s_parirtion(get_files(IMAGE_FILE_PATH, NUM_IMAGES), RATIOS)  
+    partitions = img_csv_s_parirtion(get_files(IMAGE_FILE_PATH, NUM_IMAGES, '.png'), RATIOS, LABEL_FILE_PATH)  
 elif DATA_SPLIT == 'rpartition':
-    partitions = r_parirtion(get_files(IMAGE_FILE_PATH, NUM_IMAGES), NUM_PEERS)
+    partitions = img_csv_r_parirtion(get_files(IMAGE_FILE_PATH, NUM_IMAGES, '.png'), NUM_PEERS, LABEL_FILE_PATH)
 
 for index, driver in enumerate(drivers):
-    # Click 'Start Building' on home page
-    driver.get(PLATFORM)
-    elements = driver.find_elements_by_tag_name('button')
-    for elem in elements:
-        if 'Start building' in elem.get_attribute('innerHTML'):
-            elem.click()
-
-    # Find CIFAR10 task and click 'Join' on task list page
-    time.sleep(2)
-    elements = driver.find_elements_by_css_selector('div.group')
-    for element in elements:
-        if TASK_NAME in element.get_attribute('innerHTML'):
-            button = element.find_element_by_tag_name('button')
-    button.click()
-
-    # Click 'Join Training' on Task Description page
-    elements = driver.find_elements_by_tag_name('button')
-    for elem in elements:
-        if 'Join Training' in elem.get_attribute('innerHTML'):
-            elem.click()
+    find_task_page(driver, PLATFORM, TASK_NAME)
 
     # Upload files on Task Training
     time.sleep(6)
@@ -179,34 +65,15 @@ for index, driver in enumerate(drivers):
         driver.find_element_by_id('hidden-input_cifar10-model_Images').send_keys(' \n '.join(partitions[index]))
         driver.find_element_by_id('hidden-input_cifar10-model_Labels').send_keys(os.path.abspath(str(index) + '_partition.csv'))
     else:
-        driver.find_element_by_id('hidden-input_cifar10-model_Images').send_keys(' \n '.join(get_files(IMAGE_FILE_PATH, NUM_IMAGES)))
+        driver.find_element_by_id('hidden-input_cifar10-model_Images').send_keys(' \n '.join(get_files(IMAGE_FILE_PATH, NUM_IMAGES, '.png')))
         driver.find_element_by_id('hidden-input_cifar10-model_Labels').send_keys(os.path.abspath(LABEL_FILE_PATH))
 
 # Start training on each driver
-for driver in drivers:
-    elements = driver.find_elements_by_tag_name('button')
-    for elem in elements:
-        if TRAINING_TYPE in elem.get_attribute('innerHTML'):
-            driver.execute_script("arguments[0].scrollIntoView();", elem)
-            elem.click()
-            break
+start_training(drivers, TRAINING_TYPE)
 
+print_train_acc(drivers, start_time, 'val_trainingAccuracy_cifar10-model', 'val_validationAccuracy_cifar10-model')
 
-#Print out the Accuracy after training
-continue_searcing = True
-
-while continue_searcing:
-    if len(drivers[0].find_elements_by_xpath("//*[@class='c-toast c-toast--success c-toast--bottom-right']")) > 0:
-        for f in drivers[0].find_elements_by_xpath("//*[@class='c-toast c-toast--success c-toast--bottom-right']"):
-            # print("Im here")
-            if 'has finished training' in f.text:
-                print(f"Train accuracy = {drivers[0].find_element_by_id('val_trainingAccuracy_cifar10-model').text}")
-                print(f"Validation accuracy = {drivers[0].find_element_by_id('val_validationAccuracy_cifar10-model').text}")
-                print(f"Total Training time was: {round(time.time() - start_time, 2)} seconds")
-                continue_searcing = False
-                break
-
-calculate_epoch_per_second(drivers, 4, 3)
+calculate_epoch_per_second(drivers, 3, 2)
 
 for driver in drivers:
     driver.quit()
--- tests/selenium-automation-scripts/util.py
+++ tests/selenium-automation-scripts/util.py
@@ -1,3 +1,10 @@
+import os
+import random
+import csv
+import time
+import math
+
+
 def calculate_epoch_per_second(drivers, time_index, epoch_index):
     total_averaging_count = 0
     total_wait_time = 0.0
@@ -6,3 +13,142 @@ def calculate_epoch_per_second(drivers, time_index, epoch_index):
         total_averaging_count += int(elems[epoch_index].text)
         total_wait_time += float((elems[time_index].text)[:len(elems[time_index].text) - 4])
     print(f'Epochs/s was: {round(total_averaging_count / total_wait_time, 2)}')
+
+def get_files(directory, num_images, file_type):
+    files = []
+    for dirpath,_,filenames in os.walk(directory):
+        for f in filenames:
+            if file_type in f:
+                files.append(os.path.abspath(os.path.join(dirpath, f)))
+    return files[:num_images]
+
+def img_partition(list_in, n):
+    random.shuffle(list_in)
+    return [list_in[i::n] for i in range(n)]
+
+def read_csv(file_path):
+    with open(file_path, newline='') as f:
+        reader = csv.reader(f)
+        results = dict(reader) 
+    return results
+
+def create_csv(dic, name):
+    with open(name, 'w') as f:
+        for key in dic.keys():
+            f.write(f"{key},{dic[key]}\n")
+
+def img_csv_partition(list_in, n, label_path):
+    x = list(enumerate(list_in))
+    random.shuffle(x)
+    indices, ls = zip(*x)
+    labels = read_csv(label_path)
+
+    for i in range(n):
+        temp_dic = {str(k): labels[str(k)] for k in indices[i::n]}
+        create_csv(temp_dic, str(i) + '_partition.csv')
+    return [(ls[i::n]) for i in range(n)]
+
+def img_csv_r_parirtion(list_in, n, label_path):
+    x = list(enumerate(list_in))
+    random.shuffle(x)
+    indices, ls = zip(*x)
+    labels = read_csv(label_path)
+    partition_indices = []
+    indices_out = [] 
+    list_out = []
+    for _ in range(n - 1):
+        rand_index = random.randint(1, len(list_in) - 1)
+        while rand_index in partition_indices:
+            rand_index = random.randint(1, len(list_in) - 1)
+        partition_indices.append(rand_index)
+    partition_indices = sorted(partition_indices)
+
+    for i in range(len(partition_indices)):
+        if i == 0:
+            list_out.append(ls[0:partition_indices[i]])
+            indices_out.append(indices[0:partition_indices[i]])
+        else:
+            list_out.append(ls[partition_indices[i - 1]:partition_indices[i]])
+            indices_out.append(indices[partition_indices[i - 1]:partition_indices[i]])
+
+    list_out.append(ls[partition_indices[len(partition_indices) - 1]:])
+    indices_out.append(indices[partition_indices[len(partition_indices) - 1]:])
+
+    for i in range(n):
+        temp_dic = {str(k): labels[str(k)] for k in indices_out[i]}
+        create_csv(temp_dic, str(i) + '_partition.csv')
+
+    return list_out
+
+def img_csv_s_parirtion(list_in, ratios, label_path):
+    x = list(enumerate(list_in))
+    random.shuffle(x)
+    indices, ls = zip(*x)
+    labels = read_csv(label_path)
+    partition_indices = []
+    indices_out = [] 
+    list_out = []
+    for i in range(len(ratios)):
+        curr_slice_index = math.ceil(ratios[i] * len(ls))
+        if i == 0:
+            partition_indices.append(int(curr_slice_index))
+        else:
+            partition_indices.append(int(partition_indices[i - 1] + curr_slice_index))
+    for i in range(len(partition_indices)):
+        if i == 0:
+            list_out.append(ls[0:partition_indices[i]])
+            indices_out.append(indices[0:partition_indices[i]])
+        else:
+            list_out.append(ls[partition_indices[i - 1]:partition_indices[i]])
+            indices_out.append(indices[partition_indices[i - 1]:partition_indices[i]])
+    for i in range(len(indices_out)):
+        temp_dic = {str(k): labels[str(k)] for k in indices_out[i]}
+        create_csv(temp_dic, str(i) + '_partition.csv')
+    return list_out
+
+def print_train_acc(drivers, start_time, train_acc_element_id, val_acc_element_id):
+
+    continue_searcing = True
+
+    while continue_searcing:
+        if len(drivers[len(drivers) - 1].find_elements_by_xpath("//*[@class='c-toast c-toast--success c-toast--bottom-right']")) > 0:
+            for f in drivers[len(drivers) - 1].find_elements_by_xpath("//*[@class='c-toast c-toast--success c-toast--bottom-right']"):
+                # print("Im here")
+                if 'has finished training' in f.text:
+                    print(f"Train accuracy = {drivers[len(drivers) - 1].find_element_by_id(train_acc_element_id).text}")
+                    print(f"Validation accuracy = {drivers[len(drivers) - 1].find_element_by_id(val_acc_element_id).text}")
+                    print(f"Total Training time was: {round(time.time() - start_time, 2)} seconds")
+                    continue_searcing = False
+                    break
+
+def start_training(drivers, training_type):
+    for driver in drivers:
+        elements = driver.find_elements_by_tag_name('button')
+        for elem in elements:
+            if training_type in elem.get_attribute('innerHTML'):
+                driver.execute_script("arguments[0].scrollIntoView();", elem)
+                elem.click()
+                break
+
+def find_task_page(driver, platform, task_name):
+    driver.get(platform)
+    elements = driver.find_elements_by_tag_name('button')
+    for elem in elements:
+        if 'Start building' in elem.get_attribute('innerHTML'):
+            elem.click()
+
+    # Find CIFAR10 task and click 'Join' on task list page
+    time.sleep(2)
+    elements = driver.find_elements_by_css_selector('div.group')
+    for element in elements:
+        if task_name in element.get_attribute('innerHTML'):
+            button = element.find_element_by_tag_name('button')
+    button.click()
+
+    # Click 'Join Training' on Task Description page
+    elements = driver.find_elements_by_tag_name('button')
+    for elem in elements:
+        if 'Join Training' in elem.get_attribute('innerHTML'):
+            elem.click()
+
+
