d3f5fb5c897c16886d58b68c0259b14c8a7b4bc7
Nacho114 <ethereal.gas@protonmail.com>
2022-01-20 09:36:10 +0100

    updated tasks.json for more info on cifar data format, added card div to fed/dec schematics in home and removed redundant SvgCard file (should have not been added in the first place)

--- mobile-browser-based-version/src/components/Home.vue
+++ mobile-browser-based-version/src/components/Home.vue
@@ -38,32 +38,37 @@
     </section>
 
     <!-- TODO: This section is not algined with the top, not sure why -->
-    <section class="flex flex-row items-start pl-10">
+    <div class="flex flex-row items-start pl-10">
       <custom-button v-on:click="goToTaskList()">
         {{ $t('home.startBuildingButtonText') }}
       </custom-button>
-      <div class="w-1/6 flex flex-col space-y-sm mx-auto">
-        <decentralized-image class="w-full h-full" />
-        <p class="text-xs items-center pt-10">
-          <span class="text-primary-dark dark:text-primary-light"
-            >Decentralised learning</span
-          >
-          uses peer2peer communication to share weights among the users keeping
-          your data local at all times.
-        </p>
+
+      <div class="w-1/6 flex flex-col items-center mx-auto">
+        <card customClass="space-y-sm">
+          <decentralized-image class="w-full h-full" />
+          <p class="text-xs items-center pt-10">
+            <span class="text-primary-dark dark:text-primary-light"
+              >Decentralised learning</span
+            >
+            uses peer2peer communication to share weights among the users
+            keeping your data local at all times.
+          </p>
+        </card>
       </div>
 
-      <div class="w-1/6 flex flex-col space-y-sm items-center mr-auto">
-        <federated-image class="w-full h-full" />
-        <p class="text-xs items-center pt-10">
-          <span class="text-primary-dark dark:text-primary-light"
-            >Federated learning</span
-          >
-          uses a central server to share and aggregate weights of the users
-          while keeping your data local at all times.
-        </p>
+      <div class="w-1/6 flex flex-col items-center mr-auto">
+        <card customClass="space-y-sm">
+          <federated-image class="w-full h-full" />
+          <p class="text-xs items-center pt-10">
+            <span class="text-primary-dark dark:text-primary-light"
+              >Federated learning</span
+            >
+            uses a central server to share and aggregate weights of the users
+            while keeping your data local at all times.
+          </p>
+        </card>
       </div>
-    </section>
+    </div>
 
     <section class="flex-col items-center justify-center p-4 space-y-4">
       <div class="grid grid-cols-1 gap-4 p-4 lg:grid-cols-1 xl:grid-cols-1">
@@ -95,6 +100,7 @@ import TitleCard from './containers/TitleCard.vue';
 import FederatedImage from '../assets/svg/FederatedImage.vue';
 import SettingsIcon from '../assets/svg/SettingsIcon.vue';
 import DecentralizedImage from '../assets/svg/DecentralizedImage.vue';
+import Card from './containers/Card.vue';
 
 import { useI18n } from 'vue-i18n';
 import { mapMutations } from 'vuex';
@@ -112,6 +118,7 @@ export default {
     FederatedImage,
     DecentralizedImage,
     SettingsIcon,
+    Card,
   },
   methods: {
     ...mapMutations(['setActivePage']),
