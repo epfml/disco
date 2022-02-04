4d0e97c01df133c3dc22a09b7c0310542cfbf762
Lucas Giordano <giordano3102lucas@gmail.com>
2022-02-04 12:57:58 +0100

    separate training steps

--- mobile-browser-based-version/src/helpers/training/trainer.js
+++ mobile-browser-based-version/src/helpers/training/trainer.js
@@ -1,6 +1,7 @@
 import { Actor } from '../actor.js';
 import { TrainingInformant } from './decentralised/training_informant.js';
 import { TrainingManager } from './training_manager.js';
+import { FileUploadManager } from '../data_validation/file_upload_manager';
 import { getClient } from '../communication/helpers.js';
 
 // number of files that should be loaded (required by the task)
@@ -12,12 +13,20 @@ export class Trainer extends Actor {
   constructor(task, platform, useIndexedDB, logger, helper) {
     super(task, logger, nbrFiles(task), helper);
     this.isConnected = false;
+    this.isTraining = false;
+    this.distributedTraining = false;
     this.useIndexedDB = useIndexedDB;
-    // Manager that returns feedbacks when training
+    this.platform = platform;
+    // Delivers training feedback to the user
     this.trainingInformant = new TrainingInformant(10, this.task.taskID);
+    // Handles the file uploading process
+    this.fileUploadManager = new FileUploadManager(this.nbrClasses, this);
+  }
+
+  created() {
     // Take care of communication processes
     this.client = getClient(
-      platform,
+      this.platform,
       this.task,
       null //TODO: this.$store.getters.password(this.Id)
     );
@@ -32,21 +41,14 @@ export class Trainer extends Actor {
 
   // wrapper for Vue interface update
   setIndexedDB(newValue) {
-    this.useIndexedDB = newValue;
+    if (newValue !== undefined) {
+      this.useIndexedDB = newValue;
+    }
   }
 
   // connect
-  async connect(useIndexedDB) {
-    if (useIndexedDB !== undefined) {
-      this.setIndexedDB(useIndexedDB);
-    }
-    // Create the training manager
-    this.trainingManager = new TrainingManager(
-      this.task,
-      this.client,
-      this.trainingInformant,
-      this.useIndexedDB
-    );
+  async connectClientToServer(useIndexedDB) {
+    this.setIndexedDB(useIndexedDB);
     // Connect to centralized server
     this.isConnected = await this.client.connect();
     if (this.isConnected) {
@@ -67,9 +69,13 @@ export class Trainer extends Actor {
 
   async joinTraining(distributed) {
     if (distributed && !this.isConnected) {
-      this.logger.error('Distributed training is not available.');
-      return;
+      await this.connectClientToServer();
+      if (!this.isConnected) {
+        distributed = false;
+        this.logger.error('Distributed training is not available.');
+      }
     }
+    this.distributedTraining = distributed;
     const nbrFiles = this.fileUploadManager.numberOfFiles();
     // Check that the user indeed gave a file
     if (nbrFiles == 0) {
@@ -92,12 +98,7 @@ export class Trainer extends Actor {
           this.task.trainingInformation
         );
       }
-      if (!statusValidation.accepted) {
-        // print error message
-        this.logger.error(
-          `Invalid input format : Number of data points with valid format: ${statusValidation.nr_accepted} out of ${nbrFiles}`
-        );
-      } else {
+      if (statusValidation.accepted) {
         // preprocess data
         const processedDataset = await this.taskHelper.dataPreprocessing(
           filesElement
@@ -106,8 +107,22 @@ export class Trainer extends Actor {
           `Data preprocessing has finished and training has started`
         );
         this.trainingManager.trainModel(processedDataset, distributed);
+      } else {
+        // print error message
+        this.logger.error(
+          `Invalid input format : Number of data points with valid format: ${statusValidation.nr_accepted} out of ${nbrFiles}`
+        );
       }
     }
+  }
+  async stopTraining() {
+    this.trainingManager.stopTraining();
+    if (this.isConnected) {
+      await this.client.disconnect();
+      this.isConnected = false;
+    }
+    this.logger.success('Training was successfully interrupted.');
+    this.isTraining = false;
   }
 }
 
