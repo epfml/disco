841aa64018441b717f0de544473948b168e0eb41
Lucas Giordano <giordano3102lucas@gmail.com>
2022-02-06 09:39:30 +0100

    add JS documentation (2)

--- mobile-browser-based-version/src/helpers/logging/logger.js
+++ mobile-browser-based-version/src/helpers/logging/logger.js
@@ -5,13 +5,21 @@ import chalk from 'chalk';
  * @class Logger
  */
 class Logger {
+  /**
+   * Logs sucess message on the console (in green)
+   * @param {String} message - message to be displayed
+   */
   success(message) {
     console.log(chalk.green(message));
   }
-
+  /**
+   * Logs error message on the console (in red)
+   * @param {String} message - message to be displayed
+   */
   error(message) {
     console.log(chalk.red(message));
   }
 }
 
+// creates logger object
 export const logger = new Logger();
