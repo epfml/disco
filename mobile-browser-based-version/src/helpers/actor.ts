940790824f5b359b7d80b141340792f8c89cdc6d
Lucas Giordano <giordano3102lucas@gmail.com>
2022-02-11 19:35:18 +0100

    merge (part 2(

--- mobile-browser-based-version/src/helpers/actor.ts
+++ mobile-browser-based-version/src/helpers/actor.ts
@@ -1,10 +1,17 @@
 import { FileUploadManager } from './data_validation/file_upload_manager'
+import { Task } from './task_definition/base/task'
+import { TaskHelper } from './task_definition/base/taskHelper'
 import { createTaskHelper } from './task_definition/helper'
+import { Logger } from './logging/logger'
 /**
  * Base class for all actors of the system (e.g. trainer, tester, etc.)
  * containing commonly used parameters
  */
 export class Actor {
+  task: Task
+  logger: Logger
+  taskHelper: TaskHelper<Task>
+  fileUploadManager: FileUploadManager
   /**
    * Constructor for Actor
    * @param {Task} task - task on which the tasking shall be performed
