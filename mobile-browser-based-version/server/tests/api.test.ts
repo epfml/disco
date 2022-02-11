7991c784360b780083c19a6862101b20d6de88c6
Nacho114 <ethereal.gas@protonmail.com>
2022-02-11 11:41:59 +0100

    added supertest

--- mobile-browser-based-version/server/tests/api.test.ts
+++ mobile-browser-based-version/server/tests/api.test.ts
@@ -1,26 +1,27 @@
 /* eslint-disable no-unused-expressions */
 import { expect } from 'chai'
-// import app from '../src/run_server'
-import * as api from '../src/test/api'
+import app from '../src/run_server'
+import { agent as request } from 'supertest'
+// import { request } from 'supertest'
 
 /** Source for test setup:
  * https://dev.to/matteobruni/mocha-chai-with-typescript-37f
  */
 
-const ids = ['a', 'b', 'c']
-const task = 'titanic'
-const nonExistingTask = 'non_existing_task'
-
-describe('Connection tests', () => { // the tests container
+describe('Connection tests 2', () => { // the tests container
   it('connect to valid task', async () => { // the single test
-    const response = await api.connect(task, ids[0])
+    const appResp = await request(app).get('/')
 
-    expect(response.ok).to.be.true
+    expect(appResp.ok).to.be.true
   })
 
   it('connect to non existing task', async () => { // the single test
-    const response = await api.connect(nonExistingTask, ids[0])
+    const appResp = await request(app).get('/feai/')
 
-    expect(response.ok).to.be.false
+    expect(appResp.ok).to.be.true
+  })
+
+  after((done) => {
+    done()
   })
 })
