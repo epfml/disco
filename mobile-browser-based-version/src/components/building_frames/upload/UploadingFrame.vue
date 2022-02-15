a6260a6fe5013cc0c81cf2402e84123f038a4f10
Nacho114 <ethereal.gas@protonmail.com>
2022-02-15 15:45:26 +0100

    fixed bug related to cifar-10 and added comments to clarify related code

--- mobile-browser-based-version/src/components/building_frames/upload/UploadingFrame.vue
+++ mobile-browser-based-version/src/components/building_frames/upload/UploadingFrame.vue
@@ -2,7 +2,7 @@
   <icon-card header="Upload my data">
     <template v-slot:icon><upload /></template>
     <template v-slot:extra>
-      <div v-for="item in formatLabels" :key="item">
+      <div v-for="item in getDataTypes" :key="item">
         <single-upload-frame
           :id="id"
           :task="task"
@@ -31,18 +31,42 @@ export default {
     Upload,
     IconCard
   },
+  methods: {
+    /**
+     * Returns true if the user needs to add a separate file with labels.
+     *
+     * mnist: false, since per label there is a single upload frame where the user must add images.
+     *
+     * cifar-10: true, the user needs to add both images and a csv file with the labels of these images.
+     *
+     */
+    taskRequiresToUploadLabelFile () {
+      return this.task.trainingInformation.LABEL_ASSIGNMENT !== undefined
+    }
+  },
   computed: {
     preview () {
       // Preview only for csv (since there is no, "show only first n images").
       return this.task.trainingInformation.dataType === 'csv'
     },
-    formatLabels () {
-      // Need to upload a sperated file just for the labels
-      const uploadLabelFile =
-        this.task.trainingInformation.LABEL_ASSIGNMENT !== undefined
+    /**
+     * Returns a list of the data that the user needs to upload, if the user does not require to upload a labels file,
+     * then for example:
+     *
+     * mnist: returns ['0', '1', ..., '9']
+     *
+     * If however labels are required separately then:
+     *
+     * cifar-10: returns ['Images', 'Labels']
+     *
+     * TODO: this is hard coded, currently we only support csv or images, so only for images does a separate label file make sense.
+     * Also note, we are mixing data types: Images, Labels, with labels: 0, 1, 2... (for cifar). This should be improved.
+     */
+    getDataTypes () {
       // labels to be displayed. If no labels are available => binary classification => display 1
       const labels = this.task.trainingInformation.LABEL_LIST ?? ['']
-      return uploadLabelFile ? ['Features', 'Labels'] : labels
+      const imagesAndLabels = ['Images', 'Labels']
+      return this.taskRequiresToUploadLabelFile() ? imagesAndLabels : labels
     }
   }
 }
