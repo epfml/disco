e78555fed408058e488780130aa6ef07059c4236
Nacho114 <ethereal.gas@protonmail.com>
2022-02-16 11:50:56 +0100

    added basic api test

---
+++ mobile-browser-based-version/tests/api.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import * as api from '../src/helpers/communication/federated/api'
+
+const task = 'titanic'
+const user = 'a'
+const nonValidTask = 'nonValidTask'
+
+describe('API test', () => {
+  it('Connect to valid task', () => {
+    api.connect(task, user).then((resp) => {
+      expect(resp.ok).true
+    })
+  })
+
+  it('disconnect from valid task', () => {
+    api.disconnect(task, user).then((resp) => {
+      expect(resp.ok).true
+    })
+  })
+
+  it('Connect to non valid task', () => {
+    api.connect(nonValidTask, user).then((resp) => {
+      expect(resp.ok).false
+    })
+  })
+})
