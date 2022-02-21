de618131f42e45a5a39250c6087cb017e34e9314
Nacho114 <ethereal.gas@protonmail.com>
2022-02-21 14:25:01 +0100

    added setup + fixed await issues with testing

--- mobile-browser-based-version/tests/api.test.ts
+++ mobile-browser-based-version/tests/api.test.ts
@@ -3,24 +3,18 @@ import * as api from '../src/helpers/communication/federated/api'
 
 const task = 'titanic'
 const user = 'a'
-// const nonValidTask = 'nonValidTask'
+const nonValidTask = 'nonValidTask'
 
 describe('API test', () => { // the tests container
-  it('Connect to valid task', () => {
-    api.connect(task, user).then((resp) => {
-      expect(resp.status).equal(200)
-    })
+  it('Connect to valid task', async () => {
+    expect((await api.connect(task, user)).status).equal(200)
   })
 
-  it('disconnect from valid task', () => {
-    api.disconnect(task, user).then((resp) => {
-      expect(resp.status).equal(200)
-    })
+  it('disconnect from valid task', async () => {
+    expect((await api.disconnect(task, user)).status).equal(200)
   })
 
-  // it('Connect to non valid task', () => {
-  //   api.connect(nonValidTask, user).then((resp) => {
-  //     expect(resp.ok).false
-  //   })
-  // })
+  it('Connect to non valid task', async () => {
+    expect((await api.connect(nonValidTask, user)).status).equal(404)
+  })
 })
--- mobile-browser-based-version/tests/trainer.test.ts
+++ mobile-browser-based-version/tests/trainer.test.ts
@@ -8,17 +8,11 @@ import { logger } from '../src/helpers/logging/ConsoleLogger'
 const platform = 'feai'
 
 describe('train test', () => { // the tests container
-  before(() => {
-    process.env.NODE_ENV = 'development'
-    // Load .env.development
-    require('dotenv-flow').config()
-  })
-
   it('connect to titanic task', async () => {
     const tasks = await loadTasks()
     const task = tasks[0]
     const helper = new CsvTaskHelper(task)
     const trainer = new Trainer(task, platform, logger, helper)
-    trainer.connectClientToServer().then((isConnected) => expect(isConnected).true)
+    await trainer.connectClientToServer().then((isConnected) => expect(isConnected).true)
   })
 })
