1f61ba3a682533f606b2757a98e269535c61156e
s314cy <hugo.elguedj@epfl.ch>
2022-02-23 16:37:15 +0100

    client docs

--- mobile-browser-based-version/src/helpers/communication/client.ts
+++ mobile-browser-based-version/src/helpers/communication/client.ts
@@ -1,5 +1,11 @@
 import * as tf from '@tensorflow/tfjs'
 
+/**
+ * Client interface representing the web app's node. Handles communication
+ * with any sort of centralized server used throughout the training of a task
+ * by the node. Subclasses correspond to specific training schemes, differing
+ * in communication processes. Provides a few default methods to subclasses.
+ */
 export class Client {
   serverURL: any;
   task: any;
@@ -9,47 +15,71 @@ export class Client {
   }
 
   /**
-   * Handles the connection process from the client to any sort of
+   * Abstract method. Handles the connection process from the client to any sort of
    * centralized server.
+   * @param {number} epochs
+   * @throws Throws an error. Meant to be an abstract method and thus
+   * overridden by subclasses.
    */
   async connect (epochs?): Promise<any> {
     throw new Error('Abstract method')
   }
 
   /**
-   * Handles the disconnection process of the client from any sort
+   * Abstract method. Handles the disconnection process of the client from any sort
    * of centralized server.
+   * @throws Throws an error, meant to be an abstract method and thus
+   * overridden by subclasses.
    */
   disconnect () {
     throw new Error('Abstract method')
   }
 
+  /**
+   * Default method. The training manager matches this function with the training loop's
+   * onTrainBegin callback when training a TF.js model. See the training manager for more
+   * details. Executes a general (communication) routine before the start of the task's
+   * training loop.
+   * @param {any} model The task's TF.js model
+   * @param {any} trainingInformant The task's training informant
+   */
   async onTrainBeginCommunication (model, trainingInformant) {
 
   }
 
   /**
-   * The training manager matches this function with the training loop's
-   * onTrainEnd callback when training a TFJS model object. See the
-   * training manager for more details.
+   * Default method. The training manager matches this function with the training loop's
+   * onTrainEnd callback when training a TF.js model. See the training manager for more
+   * details. Executes a general (communication) routine before the start of the task's
+   * training loop.
+   * @param {any} model The task's TF.js model
+   * @param {any} trainingInformant The task's training informant
    */
   async onTrainEndCommunication (model, trainingInformant) {
     trainingInformant.addMessage('Training finished.')
   }
 
   /**
-   * The training manager matches this function with the training loop's
-   * onEpochBegin callback when training a TFJS model object. See the
-   * training manager for more details.
+   * Default method. The training manager matches this function with the training loop's
+   * onEpochBegin callback when training a TF.js model. See the training manager for more
+   * details. Executes a general (communication) routine before the start of each epoch
+   * of the task's training loop.
+   * @param {any} model The task's TF.js model
+   * @param {number} epoch The task's current epoch
+   * @param {any} trainingInformant The task's training informant
    */
   async onEpochBeginCommunication (model, epoch, trainingInformant) {
 
   }
 
   /**
-   * The training manager matches this function with the training loop's
-   * onEpochEnd callback when training a TFJS model object. See the
-   * training manager for more details.
+   * Default method. The training manager matches this function with the training loop's
+   * onEpochEnd callback when training a TF.js model. See the training manager for more
+   * details. Executes a general (communication) routine after the end of each epoch
+   * of the task's training loop.
+   * @param {any} model The task's TF.js model
+   * @param {number} epoch The task's current epoch
+   * @param {any} trainingInformant The task's training informant
    */
   async onEpochEndCommunication (model?, epoch?, trainingInformant?) {
     console.log(
