1043ca2d5f496c3b39427a95b9fb927170adbd29
Ignacio Aleman <sukarno.bastian@outlook.com>
2022-03-07 11:42:33 +0100

    changing deai -> disco extension for build path

--- mobile-browser-based-version/vue.config.js
+++ mobile-browser-based-version/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/DeAI/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/disco/' : '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
-      args[0].title = 'DeAI'
+      args[0].title = 'Disco'
       return args
     })
   }
