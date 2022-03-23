8a1e2af399c1749d059f1fe2aad5420058c32c8f
s314cy <hugo.elguedj@epfl.ch>
2022-03-23 14:25:34 +0100

    work in progress

--- mobile-browser-based-version/src/core/communication/client.ts
+++ mobile-browser-based-version/src/core/communication/client.ts
@@ -1,8 +1,9 @@
-import { Task } from '../task/base/task'
+import { Task } from '../task/task'
 import { TrainingInformant } from '../training/training_informant'
 import { Platform } from '../../platforms/platform'
 import { DecentralisedClient } from './decentralised/decentralised_client'
 import { FederatedClient } from './federated/federated_client'
+import { LayersModel } from '@tensorflow/tfjs'
 
 export abstract class Client {
   serverURL: string;
@@ -29,7 +30,7 @@ export abstract class Client {
    * onTrainEnd callback when training a TFJS model object. See the
    * training manager for more details.
    */
-  abstract onTrainEndCommunication (model, trainingInformant: TrainingInformant): Promise<void>
+  abstract onTrainEndCommunication (model: LayersModel, trainingInformant: TrainingInformant): Promise<void>
 
   /**
    * This function will be called whenever a local round has ended.
@@ -38,18 +39,18 @@ export abstract class Client {
    * @param round
    * @param trainingInformant
    */
-  abstract onRoundEndCommunication (model, round: number, trainingInformant: TrainingInformant): Promise<void>
+  abstract onRoundEndCommunication (model: LayersModel, round: number, trainingInformant: TrainingInformant): Promise<void>
 
   static getClient (platform: Platform, task: Task, password: string): Client {
-      switch (platform) {
-        case Platform.decentralized:
-          return new DecentralisedClient(
-            process.env.VUE_APP_DEAI_SERVER,
-            task,
-            password
-          )
-        case Platform.federated:
-          return new FederatedClient(process.env.VUE_APP_FEAI_SERVER, task)
-      }
+    switch (platform) {
+      case Platform.decentralized:
+        return new DecentralisedClient(
+          process.env.VUE_APP_DEAI_SERVER,
+          task,
+          password
+        )
+      case Platform.federated:
+        return new FederatedClient(process.env.VUE_APP_FEAI_SERVER, task)
+    }
   }
 }
--- mobile-browser-based-version/src/core/training/training_manager.ts
+++ mobile-browser-based-version/src/core/training/training_manager.ts
@@ -6,6 +6,9 @@ import { Client } from '../communication/client'
 import { Task } from '../task/task'
 import { Logger } from '../logging/logger'
 import { Platform } from '../../platforms/platform'
+import { RoundTracker } from './trainer/round_tracker'
+import * as memory from '../memory/utils'
+import * as tf from '@tensorflow/tfjs'
 
 /**
  * Handles the training loop, server communication & provides the user with feedback.
@@ -39,18 +42,6 @@ export class TrainingManager {
   }
 
   /**
-   * Build the appropriate training class (either local or distributed)
-   */
-  private async initTrainer (dataset: any) {
-    const params = [this.task, this.trainingInformant, this.useIndexedDB] as const
-    if (this.distributedTraining) {
-      this.trainer = new DistributedTrainer(...params, this.client)
-    } else {
-      this.trainer = new LocalTrainer(...params)
-    }
-  }
-
-  /**
    * Connects the TrainingManager to the server
    */
   async connect () {
@@ -80,8 +71,7 @@ export class TrainingManager {
    * @param {any} dataset The preprocessed dataset to train on
    * @param {boolean} distributed Whether to train in a distributed or local fashion
    */
-  async startTraining (dataset: any, distributed: boolean) {
-    const params = [this.task] as const
+  async startTraining (dataset: tf.data.Dataset<tf.TensorContainer>, distributed: boolean) {
     if (distributed && !this.isConnected) {
       await this.connect()
       if (!this.isConnected) {
@@ -90,12 +80,13 @@ export class TrainingManager {
       }
     }
     this.distributedTraining = distributed
-    if (dataset.size() === 0) {
+    if (dataset.size === 0) {
       this.logger.error('Training aborted. No uploaded file given as input.')
     } else {
       this.logger.success(
         'Thank you for your contribution. Data preprocessing has started'
       )
+      this.initTrainer(dataset)
       this.trainer.trainModel(dataset)
       this.isTraining = true
     }
@@ -112,5 +103,71 @@ export class TrainingManager {
     }
     this.logger.success('Training was successfully interrupted.')
     this.isTraining = false
+  }
+
+  /**
+   * Build the appropriate training class (either local or distributed)
+   */
+  private async initTrainer (dataset: any) {
+    const roundTracker = new RoundTracker(
+      this.task.trainingInformation.roundDuration,
+      dataset.size(),
+      this.task.trainingInformation.batchSize
+    )
+    const params = [
+      this.task,
+      this.trainingInformant,
+      this.useIndexedDB,
+      roundTracker,
+      await this.getModel()
+    ] as const
+    if (this.distributedTraining) {
+      this.trainer = new DistributedTrainer(...params, this.client)
+    } else {
+      this.trainer = new LocalTrainer(...params)
+    }
+  }
+
+  private async getModel () {
+    const model = await this.getModelFromMemoryOrFetchIt()
+    return this.updateModelInformation(model)
+  }
+
+  /**
+   *
+   * @returns
+   */
+  private async getModelFromMemoryOrFetchIt () {
+    const modelExistsInMemory = await memory.getWorkingModelMetadata(
+      this.task.taskID,
+      this.task.trainingInformation.modelID
+    )
+
+    if (this.useIndexedDB && modelExistsInMemory) {
+      await memory.getWorkingModel(
+        this.task.taskID,
+        this.task.trainingInformation.modelID
+      )
+    }
+    return await this.client.getModel()
+  }
+
+  private updateModelInformation (model: tf.LayersModel) {
+    // Continue local training from previous epoch checkpoint
+    if (model.getUserDefinedMetadata() === undefined) {
+      model.setUserDefinedMetadata({ epoch: 0 })
+    }
+
+    const info = this.task.trainingInformation
+    model.compile(info.modelCompileData)
+
+    if (info.learningRate) {
+      // TODO: Not the right way to change learningRate and hence we cast to any
+      // the right way is to construct the optimiser and pass learningRate via
+      // argument.
+      (model.optimizer as any).learningRate = info.learningRate
+    }
+
+    return model
   }
 }
