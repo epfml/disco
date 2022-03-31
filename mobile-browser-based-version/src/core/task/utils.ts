e146e37e0f822760ebfaec9713a200e0df6edcf2
s314cy <hugo.elguedj@epfl.ch>
2022-03-31 16:03:20 +0200

    linting

--- mobile-browser-based-version/src/core/task/utils.ts
+++ mobile-browser-based-version/src/core/task/utils.ts
@@ -8,29 +8,11 @@ import * as tf from '@tensorflow/tfjs'
  * Should be considered as a regular API call and regrouped accordingly, it does
  * not belong to this file.
  */
-export async function loadTasks () {
+export async function loadTasks (): Promise<Array<Task>> {
   const tasksURL = process.env.VUE_APP_FEAI_SERVER.concat('tasks')
   const response = await axios.get(tasksURL)
   const rawTasks = response.data
   return _.map(rawTasks, (task) => new Task(task.taskID, task.displayInformation, task.trainingInformation))
-}
-
-/**
- * TODO: @s314cy
- * Can be removed.
- */
-export function onFileLoad (filesElement, callback, readAs = 'text') {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onload = async (e) => {
-      // Preprocess the data and get object of the form {accepted: True/False, Xtrain: training data, ytrain: lavels}
-      const res = await callback(e)
-      resolve(res)
-    };
-    (readAs === 'text' ? reader.readAsText : reader.readAsDataURL)(
-      filesElement
-    )
-  })
 }
 
 /**
--- mobile-browser-based-version/tests/core/dataset/data_loader/tabular_loader.test.ts
+++ mobile-browser-based-version/tests/core/dataset/data_loader/tabular_loader.test.ts
@@ -5,16 +5,16 @@ import { loadTasks } from '../../../../src/core/task/utils'
 const inputFiles = ['file://./example_training_data/titanic.csv']
 
 describe('tabular loader test', () => {
-    it('titanic csv columns', async () => {
-        const titanic = (await loadTasks())[0]
-        const loader = new TabularLoader(',')
-        const features = titanic.trainingInformation.inputColumns
-        const labels = titanic.trainingInformation.outputColumns
-        const dataset = loader.load(
-            inputFiles,
-            features,
-            labels
-        )
-        expect(await dataset.columnNames()).eql(labels.concat(features))
-    })
+  it('titanic csv columns', async () => {
+    const titanic = (await loadTasks())[0]
+    const loader = new TabularLoader(',')
+    const features = titanic.trainingInformation.inputColumns
+    const labels = titanic.trainingInformation.outputColumns
+    const dataset = loader.load(
+      inputFiles,
+      features,
+      labels
+    )
+    expect(await dataset.columnNames()).eql(labels.concat(features))
+  })
 })
