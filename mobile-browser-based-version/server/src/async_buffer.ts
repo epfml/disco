5842f5ee73dada21e8dc740f49dbb712d63ad23a
tharvik <tharvik@users.noreply.github.com>
2022-04-06 12:49:21 +0200

    server/async_buffer: add types

---
+++ mobile-browser-based-version/server/src/async_buffer.ts
@@ -0,0 +1,87 @@
+import { AsyncInformant } from './async_informant'
+import { TaskID } from './tasks'
+
+/**
+ * The AsyncWeightsBuffer class holds and manipulates information about the
+ * async weights buffer. It works as follows:
+ *
+ * Setup: Init round to zero and create empty buffer (a map from user id to weights)
+ *
+ * - When a user adds weights only do so when they are recent weights: i.e. this.round - round <= roundCutoff.
+ * - If a user already added weights, update them. (-> there can be at most one entry of weights per id in a buffer).
+ * - When the buffer is full, call aggregateAndStoreWeights with the weights in the buffer and then increment round by one  and reset the buffer.
+ *
+ * @remarks
+ * taskID: corresponds to the task that weights correspond to.
+ * bufferCapacity: size of the buffer.
+ * buffer: holds a map of users to their added weights.
+ * round: the latest round of the weight buffer.
+ * roundCutoff: cutoff for accepted rounds.
+ */
+export class AsyncBuffer<T> {
+  buffer: Map<string, T>
+  round: number
+  private observer: AsyncInformant<T> | undefined
+
+  constructor (
+    public readonly taskID: TaskID,
+    private readonly bufferCapacity: number,
+    private readonly aggregateAndStoreWeights: (weights: T[]) => Promise<void>,
+    private readonly roundCutoff = 0
+  ) {
+    this.buffer = new Map<string, T>()
+    this.round = 0
+  }
+
+  registerObserver (observer: AsyncInformant<T>): void {
+    this.observer = observer
+  }
+
+  // TODO do not test private
+  bufferIsFull (): boolean {
+    return this.buffer.size >= this.bufferCapacity
+  }
+
+  private async updateWeightsIfBufferIsFull (): Promise<void> {
+    if (this.bufferIsFull()) {
+      const allWeights = Array.from(this.buffer.values())
+      await this.aggregateAndStoreWeights(allWeights)
+
+      this.round += 1
+      this.observer?.update()
+      this.buffer.clear()
+
+      console.log('\n************************************************************')
+      console.log(`Buffer is full; Aggregating weights and starting round: ${this.round}\n`)
+    }
+  }
+
+  // TODO do not test private
+  isNotWithinRoundCutoff (round: number): boolean {
+    // Note that always this.round >= round
+    return this.round - round > this.roundCutoff
+  }
+
+  /**
+     * Add weights originating from weights of a given round.
+     * Only add to buffer if the given round is not old.
+     * @param weights
+     * @param round
+     * @returns true if weights were added, and false otherwise
+     */
+  async add (id: string, weights: T, round: number): Promise<boolean> {
+    if (this.isNotWithinRoundCutoff(round)) {
+      console.log(`Did not add weights of ${id} to buffer. Due to old round update: ${round}, current round is ${this.round}`)
+      return false
+    }
+
+    const weightsUpdatedByUser = this.buffer.has(id)
+    const msg = weightsUpdatedByUser ? '\tUpdating' : '-> Adding new'
+    console.log(`${msg} weights of ${id} to buffer.`)
+
+    this.buffer.set(id, weights)
+    await this.updateWeightsIfBufferIsFull()
+
+    return true
+  }
+}
