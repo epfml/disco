84e9b87a40ced3e045bff61d97199b7b91129de5
tharvik <tharvik@users.noreply.github.com>
2022-04-06 16:59:28 +0200

    commonize averaging

---
+++ mobile-browser-based-version/src/core/aggregation.ts
@@ -0,0 +1,18 @@
+import { Set } from 'immutable'
+import * as tf from '@tensorflow/tfjs'
+
+import { Weights } from '../types'
+
+export function averageWeights (peersWeights: Set<Weights>): Weights {
+  const firstWeightSize = peersWeights.first()?.length
+  if (firstWeightSize === undefined) {
+    throw new Error('no weights to average')
+  }
+  if (!peersWeights.rest().every((ws) => ws.length === firstWeightSize)) {
+    throw new Error('variable weights size')
+  }
+
+  return peersWeights.map((peerWeights) =>
+    tf.layers.average().apply(peerWeights) as tf.Tensor // TODO do not cast
+  ).toArray()
+}
