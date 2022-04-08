9eeb94d9a73cd30f7253b845e8b23e508fa9b6f8
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2022-04-08 17:50:33 +0200

    initial result_io, currently via json

---
+++ mobile-browser-based-version/benchmark/core/result_io.ts
@@ -0,0 +1,35 @@
+import { BenchmarkResult } from './benchmark'
+import fs from 'fs'
+import path from 'path'
+
+export interface SaveConfig {
+  path: string,
+  fileName: string
+}
+
+interface BenchmarkDict {
+  [key: string]: string
+}
+
+function resultToDict (benchmarkResult: BenchmarkResult): BenchmarkDict {
+  const benchmarkDict = {}
+  benchmarkResult.forEach((value, key) => (benchmarkDict[key] = value))
+  return benchmarkDict
+}
+
+export function save (benchmarkResult: BenchmarkResult, config: SaveConfig) {
+  // If folder does not exist, make it
+  if (!fs.existsSync(config.path)) {
+    fs.mkdirSync(config.path, { recursive: true })
+  }
+
+  // Map data to dict (easier to parse to json)
+  const benchmarkDict = resultToDict(benchmarkResult)
+
+  // stringify
+  const data = JSON.stringify(benchmarkDict)
+
+  // Save to file
+  const filePath = path.join(config.path, config.fileName)
+  fs.writeFileSync(filePath, data)
+}
