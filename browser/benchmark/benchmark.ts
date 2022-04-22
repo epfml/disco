0b08999ae884529b50fed5b7dfa8a38226a0b4a3
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2022-04-22 14:46:34 +0200

    added benchmark - still testing - alternating acc 0 to 1

---
+++ browser/benchmark/benchmark.ts
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+import { loadTasks } from '../src/tasks'
+import dotenv from 'dotenv-flow'
+import { dataset, ConsoleLogger } from 'discojs'
+import fs from 'fs'
+import * as tfNode from '@tensorflow/tfjs-node'
+import { Disco } from '../src/training/disco'
+import { Platform } from '../src/platforms/platform'
+
+// Setup ENV
+process.env.NODE_ENV = 'development'
+console.log('Setting up NODE_ENV to', process.env.NODE_ENV)
+console.log('***********************************\n\n')
+// Load .env.development
+dotenv.config()
+
+class NodeImageLoader extends dataset.ImageLoader<string> {
+  async readImageFrom (source: string): Promise<tfNode.Tensor3D> {
+    return tfNode.node.decodeImage(fs.readFileSync(source)) as tfNode.Tensor3D
+  }
+}
+
+async function loadData () {
+  const dir = './example_training_data/simple_face/'
+  const foldersInDir = fs.readdirSync(dir).filter(f => fs.statSync(dir + f).isDirectory())
+
+  const filesPerFolder = foldersInDir.map((folder) => {
+    const folderPath = dir.concat(folder, '/')
+    const filesInFolder = fs.readdirSync(folderPath)
+    return filesInFolder.map((file) => folderPath.concat(file))
+  })
+
+  const labels = filesPerFolder.flatMap((files, index) => Array(files.length).fill(index))
+
+  const files = filesPerFolder.flat()
+
+  console.log({ labels })
+  console.log({ files })
+
+  const simpleFace = (await loadTasks())[4]
+  return await new NodeImageLoader(simpleFace).loadAll(files, { labels: labels })
+}
+
+// Cannot run await in main context
+async function main () {
+  const tasks = await loadTasks()
+
+  const task = tasks[4]
+
+  const data = await loadData()
+
+  const logger = new ConsoleLogger()
+  const disco = new Disco(task, Platform.federated, logger, false)
+  await disco.startTraining(data, false)
+}
+
+const runMain = async () => {
+  await main()
+}
+
+runMain()
