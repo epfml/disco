3aa71e3f71b6b62747df8a9b5cafbf0920eedcbb
tharvik <tharvik@users.noreply.github.com>
2022-04-28 14:15:04 +0200

    discojs/serialization: readd isSerializedWeights

--- discojs/src/serialization.ts
+++ discojs/src/serialization.ts
@@ -2,13 +2,48 @@ import * as tf from '@tensorflow/tfjs'
 
 import { Weights } from '@/types'
 
-export type SerializedWeights = {
+interface SerializedWeight {
   shape: number[]
   data: Float32Array
-}[]
+}
+
+function isSerializedWeight (raw: unknown): raw is SerializedWeight {
+  if (typeof raw !== 'object') {
+    return false
+  }
+  if (!('shape' in raw && 'data' in raw)) {
+    return false
+  }
+  const { shape, data } = raw as Record<'shape' | 'data', unknown>
+
+  if (
+    !(Array.isArray(shape) && shape.every((e) => typeof e === 'number')) ||
+    !(data instanceof Float32Array)
+  ) {
+    return false
+  }
+
+  // eslint-disable-next-line
+  const _: SerializedWeight = { shape, data }
+
+  return true
+}
+
+export type SerializedWeights = SerializedWeight[]
 
 export function isSerializedWeights (obj: unknown): obj is SerializedWeights {
-  return true // FIXME
+  if (!Array.isArray(obj)) {
+    return false
+  }
+  const arr: unknown[] = obj
+  if (!arr.every(isSerializedWeight)) {
+    return false
+  }
+
+  // eslint-disable-next-line
+  const _: SerializedWeights = arr
+
+  return true
 }
 
 export async function serializeWeights (weights: Weights): Promise<SerializedWeights> {
