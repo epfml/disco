3cc6fcd743561a6f869724b8feee45fabd73ef26
tharvik <tharvik@users.noreply.github.com>
2022-05-05 18:28:30 +0200

    discojs: mv clients

--- discojs/src/communication/federated_client.ts
+++ discojs/src/communication/federated_client.ts
@@ -1,11 +1,10 @@
 import * as msgpack from 'msgpack-lite'
 import * as tf from '@tensorflow/tfjs'
+import axios from 'axios'
 
-import { Client, Task, TrainingInformant, MetadataID } from '../..'
+import { serialization, Weights, Client, Task, TrainingInformant, MetadataID } from '..'
 
-import { makeID } from '../authentication'
-import * as api from './federated_api'
-import { Weights } from '@/types'
+import { makeID } from './authentication'
 /**
  * Class that deals with communication with the centralized server when training
  * a specific task.
@@ -13,7 +12,7 @@ import { Weights } from '@/types'
 export class FederatedClient extends Client {
   clientID: string
   peer: any
-  modelUpdateIsBasedOnRoundNumber: number
+  private round: number
 
   /**
    * Prepares connection to a centralized server for training a given task.
@@ -23,7 +22,16 @@ export class FederatedClient extends Client {
   constructor (serverURL: string, task: Task) {
     super(serverURL, task)
     this.clientID = ''
-    this.modelUpdateIsBasedOnRoundNumber = -1 // The server starts at round 0, in the beginning we are behind
+    this.round = -1 // The server starts at round 0, in the beginning we are behind
+  }
+
+  private urlTo (category: string): string {
+    return [
+      this.serverURL,
+      category,
+      this.task.taskID,
+      this.clientID
+    ].join('/')
   }
 
   /**
@@ -37,60 +45,47 @@ export class FederatedClient extends Client {
      * API requests may be made.
      */
     this.clientID = makeID(10)
-    const response = await api.connect(this.task.taskID, this.clientID)
-    if (response.status !== 200) {
-      throw new Error('connect api')
-    }
+
+    await axios.get(this.urlTo('connect'))
   }
 
   /**
    * Disconnection process when user quits the task.
    */
   async disconnect (): Promise<void> {
-    const response = await api.disconnect(this.task.taskID, this.clientID)
-    if (response.status !== 200) {
-      throw new Error('bad status on disconnect')
-    }
+    await axios.get(this.urlTo('disconnect'))
   }
 
-  private async postWeightsToServer (weights: Weights): Promise<boolean> {
-    const response = await api.postWeights(
-      this.task.taskID,
-      this.clientID,
-      weights,
-      this.modelUpdateIsBasedOnRoundNumber
-    )
-    return response.status === 200
+  private async postWeightsToServer (weights: Weights): Promise<void> {
+    await axios({
+      method: 'post',
+      url: this.urlTo('weights'),
+      data: {
+        weights: await serialization.encodeWeights(weights),
+        round: this.round
+      }
+    })
   }
 
-  async postMetadata (metadataID: string, metadata: string): Promise<boolean> {
-    const response = api.postMetadata(
-      this.task.taskID,
-      this.modelUpdateIsBasedOnRoundNumber,
-      this.clientID,
-      metadataID,
-      metadata
-    )
-    return (await response).status === 200
+  async postMetadata (metadataID: string, metadata: string): Promise<void> {
+    await axios({
+      method: 'post',
+      url: `${this.serverURL}/metadata/${metadataID}/${this.task.taskID}/${this.round}/${this.clientID}`,
+      data: {
+        metadataID: metadata
+      }
+    })
   }
 
   async getMetadataMap (metadataID: MetadataID): Promise<Map<string, unknown>> {
-    const response = await api.getMetadataMap(
-      this.task.taskID,
-      this.modelUpdateIsBasedOnRoundNumber,
-      this.clientID,
-      metadataID
-    )
-    if (response.status === 200) {
-      const body = await response.data
-      return new Map(msgpack.decode(body[metadataID]))
-    } else {
-      return new Map()
-    }
+    const response = await axios.get(`${this.serverURL}/metadata/${metadataID}/${this.task.taskID}/${this.round}/${this.clientID}`)
+
+    const body = await response.data
+    return new Map(msgpack.decode(body[metadataID]))
   }
 
-  private async getLatestServerRound (): Promise<number> {
-    const response = await api.getRound(this.task.taskID, this.clientID)
+  async getLatestServerRound (): Promise<number> {
+    const response = await axios.get(this.urlTo('round'))
 
     if (response.status === 200) {
       return response.data.round
@@ -101,7 +96,8 @@ export class FederatedClient extends Client {
 
   private async updateLocalModel (model: tf.LayersModel): Promise<void> {
     // get latest model from the server
-    const weights = await api.getWeights(this.task.taskID, this.clientID)
+    const response = await axios.get(this.urlTo('weights'))
+    const weights = serialization.decodeWeights(response.data)
     model.setWeights(weights)
 
     console.log('Updated local model')
@@ -111,17 +107,17 @@ export class FederatedClient extends Client {
     // get server round of latest model
     const serverRound = await this.getLatestServerRound()
 
-    const localRoundIsOld = this.modelUpdateIsBasedOnRoundNumber < serverRound
+    const localRoundIsOld = this.round < serverRound
     if (localRoundIsOld) {
-      this.modelUpdateIsBasedOnRoundNumber = serverRound
+      this.round = serverRound
       // update local model from server
       await this.updateLocalModel(model)
     }
   }
 
-  private async fetchServerStatisticsAndUpdateInformant (trainingInformant: TrainingInformant): Promise<void> {
-    const serverStatistics = await api.getAsyncWeightInformantStatistics(this.task.taskID, this.clientID)
-    trainingInformant.updateWithServerStatistics(serverStatistics.data.statistics)
+  async fetchServerStatisticsAndUpdateInformant (trainingInformant: TrainingInformant): Promise<void> {
+    const response = await axios.get(this.urlTo('statistics'))
+    trainingInformant.updateWithServerStatistics(response.data.statistics)
   }
 
   async onRoundEndCommunication (model: tf.LayersModel, _: number, trainingInformant: TrainingInformant): Promise<void> {
---
+++ server/tests/federated_client.ts
@@ -0,0 +1,98 @@
+import { expect } from 'chai'
+import * as http from 'http'
+
+import { FederatedClient, Task, TrainingInformant, TrainingSchemes } from 'discojs'
+
+import app from '../src/run_server'
+import { CONFIG } from '../src/config'
+import { getTasks } from '../src/tasks/tasks_io'
+
+const taskID = 'titanic'
+
+describe('federated client', () => { // the tests container
+  let server: http.Server
+
+  before(async () => {
+    server = http.createServer(app).listen()
+    await new Promise((resolve, reject) => {
+      server.once('listening', resolve)
+      server.once('error', reject)
+    })
+  })
+
+  after(() => {
+    server?.close()
+  })
+
+  const getClient = async (task?: Task): Promise<FederatedClient> => {
+    const addr = server?.address()
+    if (addr === undefined || addr === null) {
+      throw new Error('server not started')
+    }
+
+    let url: string
+    if (typeof addr === 'string') {
+      url = addr
+    } else if (typeof addr === 'object') {
+      if (addr.family === '4') {
+        url = `${addr.address}:${addr.port}`
+      } else {
+        url = `[${addr.address}]:${addr.port}`
+      }
+    } else {
+      throw new Error('unable to get address to server')
+    }
+    url = `http://${url}/feai`
+
+    const t = task ?? (await getTasks(CONFIG.tasksFile))[0]
+    return new FederatedClient(url, t)
+  }
+
+  it('connect to valid task', async () => {
+    const client = await getClient()
+    await client.connect()
+  })
+
+  it('disconnect from valid task', async () => {
+    const client = await getClient()
+    await client.connect()
+    await client.disconnect()
+  })
+
+  it('Connect to non valid task', async () => {
+    const client = await getClient(new Task('nonValidTask'))
+
+    try {
+      await client.connect()
+    } catch {
+      return
+    }
+
+    throw new Error("connect didn't fail")
+  })
+
+  it('checks that getRound returns a value greater or equal to zero', async () => {
+    const client = await getClient()
+    await client.connect()
+
+    const round = await client.getLatestServerRound()
+    expect(round).to.be.greaterThanOrEqual(0) // Since the server you are running might have trained and round > 0
+
+    await client.disconnect()
+  })
+
+  it('checks that getAsyncWeightInformantStatistics returns a JSON with the expected statistics', async () => {
+    const client = await getClient()
+    await client.connect()
+
+    const ti = new TrainingInformant(0, taskID, TrainingSchemes.FEDERATED)
+    await client.fetchServerStatisticsAndUpdateInformant(ti)
+
+    expect(ti.currentRound).to.be.greaterThanOrEqual(0) // Since the server you are running might have trained and round > 0
+    expect(ti.currentNumberOfParticipants).to.be.greaterThanOrEqual(0)
+    expect(ti.totalNumberOfParticipants).to.be.greaterThanOrEqual(0)
+    expect(ti.averageNumberOfParticipants).to.be.greaterThanOrEqual(0)
+
+    await client.disconnect()
+  })
+})
