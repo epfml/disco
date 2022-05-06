0ea0e8c78d3d1c5e7a488bda50f3e75e6f90f6e0
tharvik <tharvik@users.noreply.github.com>
2022-05-06 17:10:58 +0200

    server: start on 8080

--- browser/src/main.ts
+++ browser/src/main.ts
@@ -14,7 +14,7 @@ import '@/assets/css/styles.css'
   process.env.NODE_ENV === 'development' &&
   process.env.DEV_TOOLS === 'enabled'
 ) {
-  devtools.connect('http://localhost', 8081);
+  devtools.connect('http://localhost', 8080);
 } */
 // create vue app
 const app = createApp(App)
