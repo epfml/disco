3f4535159b63f66b08c932c5868193b66f8a0141
tharvik <tharvik@users.noreply.github.com>
2022-05-17 17:33:48 +0200

    discojs: rm password support

--- browser/src/communication/client_builder.ts
+++ browser/src/communication/client_builder.ts
@@ -1,17 +1,10 @@
 import { Client, Task, TrainingSchemes, DecentralizedClient, FederatedClient, LocalClient } from 'discojs'
 
-export function getClient (trainingScheme: TrainingSchemes, task: Task, password?: string): Client {
+export function getClient (trainingScheme: TrainingSchemes, task: Task): Client {
   switch (trainingScheme) {
     case TrainingSchemes.DECENTRALIZED:
-      return new DecentralizedClient(
-        process.env.VUE_APP_DEAI_SERVER,
-        task,
-        password
-      )
+      return new DecentralizedClient(process.env.VUE_APP_DEAI_SERVER, task)
     case TrainingSchemes.FEDERATED:
-      if (password !== undefined) {
-        throw new Error('unexpected password for federated client')
-      }
       return new FederatedClient(process.env.VUE_APP_FEAI_SERVER, task)
     case TrainingSchemes.LOCAL:
       return new LocalClient(task)
