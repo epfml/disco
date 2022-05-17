8b696c3c0c87c44489bf72c1f31689e4494db366
s314cy <hugo.elguedj@epfl.ch>
2022-05-17 14:09:20 +0200

    privacy: better arg handling

--- discojs/src/privacy.ts
+++ discojs/src/privacy.ts
@@ -4,27 +4,41 @@ import { Task } from '@/task'
 import * as tf from '@tensorflow/tfjs'
 
 export function addDifferentialPrivacy (previousRoundWeights: Weights, currentRoundWeights: Weights, task: Task): Weights {
-  const noiseScale = task.trainingInformation?.noiseScale ?? 0
-  if (noiseScale === 0) {
-    return currentRoundWeights
-  }
-  const clippingRadius = Math.max(1, task.trainingInformation?.clippingRadius ?? 1)
+  const noiseScale = task.trainingInformation?.noiseScale
+  const clippingRadius = task.trainingInformation?.clippingRadius
 
   const weightsDiff = List(currentRoundWeights)
     .zip(List(previousRoundWeights))
     .map(([w1, w2]) => w1.add(-w2))
 
-  // Frobenius norm
-  const norm = Math.sqrt(weightsDiff.map((w) => w.square().sum().dataSync()[0]).reduce((a: number, b) => a + b))
+  let newWeightsDiff: List<tf.Tensor>
 
-  const noisyWeightsDiff = weightsDiff.map((w) => {
-    const clipped = w.div(Math.max(1, norm / clippingRadius))
-    const noise = tf.randomNormal(w.shape, 0, (noiseScale * noiseScale) * (clippingRadius * clippingRadius))
-    return clipped.add(noise)
-  })
+  if (clippingRadius !== undefined) {
+    // Frobenius norm
+    const norm = Math.sqrt(weightsDiff.map((w) => w.square().sum().dataSync()[0]).reduce((a: number, b) => a + b))
+
+    newWeightsDiff = weightsDiff.map((w) => {
+      const clipped = w.div(Math.max(1, norm / clippingRadius))
+      if (noiseScale !== undefined) {
+        // Add clipping and noise
+        const noise = tf.randomNormal(w.shape, 0, (noiseScale * noiseScale) * (clippingRadius * clippingRadius))
+        return clipped.add(noise)
+      } else {
+        // Add clipping without any noise
+        return clipped
+      }
+    })
+  } else {
+    if (noiseScale !== undefined) {
+      // Add noise without any clipping
+      newWeightsDiff = weightsDiff.map((w) => tf.randomNormal(w.shape, 0, (noiseScale * noiseScale)))
+    } else {
+      return currentRoundWeights
+    }
+  }
 
   return List(previousRoundWeights)
-    .zip(noisyWeightsDiff)
+    .zip(newWeightsDiff)
     .map(([w, d]) => w.add(d))
     .toArray()
 }
