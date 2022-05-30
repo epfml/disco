975098dd80186d1cfca84bacc6694de47df84e9a
s314cy <hugo.elguedj@epfl.ch>
2022-05-30 10:49:47 +0200

    browser/tasklist: css

--- browser/src/components/pages/TaskList.vue
+++ browser/src/components/pages/TaskList.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="grid grid-cols-1 gap-8">
+  <div class="grid grid-cols-1 gap-8 mt-8">
     <div
       v-for="[taskID, task] in tasks"
       :key="taskID"
-      class="group"
     >
-      <TitleCard :title="task.displayInformation.taskTitle">
-        <ul class="text-base ont-semibold text-slate-500 dark:text-light py-3">
+      <TitleCard
+        class="
+          transition
+          duration-200
+          hover:-translate-y-1 hover:scale-[101%]
+          hover:outline hover:outline-2 hover:outline-disco-cyan"
+        :title="task.displayInformation.taskTitle"
+      >
+        <ul class="text-base ont-semibold text-slate-500 py-3">
           <span v-html="task.displayInformation.summary" />
         </ul>
         <div class="pt-2">
