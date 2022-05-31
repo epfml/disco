acdb94a5965e0836bcd6cba7ac6db4a12bf33f1e
tharvik <tharvik@users.noreply.github.com>
2022-05-31 14:24:03 +0200

    server/router: add Router class

--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import expressWS from 'express-ws'
+import { Set } from 'immutable'
 
 import { Task } from 'discojs'
 
@@ -8,99 +9,126 @@ import { writeNewTask, getTasks } from '../tasks/tasks_io'
 import { CONFIG } from '../config'
 import { SignalingServer } from './signaling_server'
 
-// enable websocket
-const wsApplier = expressWS(express())
+import models from '../tasks/models'
 
-const tasks = getTasks(CONFIG.tasksFile)
+export class Router {
+  private tasks = Set<Task>()
 
-// WebSocket server
-const signalingServer = new SignalingServer()
+  // TODO choose between federated and/or decentralised
 
-// General tasks routes
-const tasksRouter = express.Router()
-wsApplier.applyTo(tasksRouter)
+  async init (): Promise<express.Router> {
+    this.tasks = await getTasks(CONFIG.tasksFile)
+    // TODO store models
+    await Promise.all(models.map(async (create) => await create()))
 
-tasksRouter.get('/', handlers.getTasksMetadata)
-// POST method route (task-creation-form)
-tasksRouter.post('/', (req, res, next) => {
-  const modelFile = req.body.modelFiles.modelFile
-  const weightsFile = req.body.modelFiles.weightsFile
-  const newTask = req.body
-  if (
-    typeof modelFile !== 'string' ||
-    (weightsFile instanceof Uint8Array) ||
-    !Task.isTask(newTask)
-  ) {
-    res.status(400)
-    return
+    const wsApplier = expressWS(express())
+    const signalingServer = new SignalingServer()
+
+    const tasksRouter = this.setupTasksRouter(wsApplier, signalingServer)
+    const federatedRouter = this.setupFederatedRouter(tasksRouter)
+    const decentralisedRouter = this.setupDecentralizedRouter(tasksRouter)
+
+    const router = express.Router()
+    wsApplier.applyTo(router)
+
+    router.use('/deai', decentralisedRouter)
+    router.use('/feai', federatedRouter)
+    router.get('/', (_, res, next) => {
+      res.send('Server for DeAI & FeAI')
+      next()
+    })
+
+    return router
   }
-  if (newTask.taskID in tasks) {
-    console.log('Cannot add new task (key is already defined in Tasks.json)')
-    res.status(400)
-    return
+
+  private setupTasksRouter (wsApplier: expressWS.Instance, signalingServer: SignalingServer): express.Router {
+    const router = express.Router()
+    wsApplier.applyTo(router)
+
+    router.get('/', handlers.getTasksMetadata)
+
+    router.post('/', (req, res, next) => {
+      const modelFile = req.body.modelFiles.modelFile
+      const weightsFile = req.body.modelFiles.weightsFile
+      const newTask = req.body
+      if (
+        typeof modelFile !== 'string' ||
+        (weightsFile instanceof Uint8Array) ||
+        !Task.isTask(newTask)
+      ) {
+        res.status(400)
+        return
+      }
+      if (this.tasks.map((t) => t.taskID).contains(newTask.taskID)) {
+        console.log('task already existing')
+        res.status(400)
+        return
+      }
+
+      writeNewTask(this.tasks, newTask, modelFile, weightsFile, CONFIG).catch(next)
+      this.tasks = this.tasks.add(newTask)
+      console.log(`new task: ${newTask.taskID}`)
+
+      router.ws(`/${newTask.taskID}`, (ws) => signalingServer.handle(newTask.taskID, ws))
+
+      res.end('Successfully upload')
+    })
+
+    router.ws('/:task', (ws, req) => {
+      const task = this.tasks.find((task) => task.taskID === req.params.task)
+      if (task === undefined) {
+        ws.close(undefined, 'no such task')
+        return
+      }
+
+      signalingServer.handle(task.taskID, ws)
+    })
+
+    router.get('/:task/:file', handlers.getLatestModel)
+
+    return router
   }
 
-  // create new task and server
-  tasks.then((ts) => ts.push(newTask)).catch(next)
-  console.log(`new task: ${newTask.taskID}`)
-  tasksRouter.ws(`/${newTask.taskID}`, (ws) => signalingServer.handle(newTask.taskID, ws))
-  // store results in json file
-  writeNewTask(newTask, modelFile, weightsFile, CONFIG).catch(next)
-  // answer vue app
-  res.end('Successfully upload')
-})
+  private setupFederatedRouter (tasksRouter: express.Router): express.Router {
+    const router = express.Router()
+    router.get('/', (_, res) => res.send('FeAI server'))
 
-tasksRouter.ws('/:task', (ws, req, next) => {
-  tasks.then(async () => {
-    const task = (await tasks).find((task) => task.taskID === req.params.task)
-    if (task === undefined) {
-      ws.close(undefined, 'no such task')
-      return
-    }
-    signalingServer.handle(task.taskID, ws)
-  }).catch(next)
-})
+    router.get('/connect/:task/:id', handlers.connect)
+    router.get('/disconnect/:task/:id', handlers.disconnect)
 
-tasksRouter.get('/:task/:file', handlers.getLatestModel)
+    router.post('/weights/:task/:id', (req, res, next) => {
+      handlers.postWeights(req, res).catch(next)
+    })
 
-// Declare federated routes
-const federatedRouter = express.Router()
-federatedRouter.get('/', (_, res) => res.send('FeAI server'))
+    router.get('/round/:task/:id', (req, res, next) => {
+      handlers.getRound(req, res).catch(next)
+    })
+    router.get('/weights/:task/:id', (req, res, next) => {
+      handlers.getWeightsHandler(req, res).catch(next)
+    })
 
-federatedRouter.get('/connect/:task/:id', handlers.connect)
-federatedRouter.get('/disconnect/:task/:id', handlers.disconnect)
+    router.get('/statistics/:task/:id', (req, res, next) => {
+      handlers.getAsyncWeightInformantStatistics(req, res).catch(next)
+    })
 
-federatedRouter.post('/weights/:task/:id', (req, res, next) => {
-  handlers.postWeights(req, res).catch(next)
-})
+    router
+      .route('/metadata/:metadata/:task/:round/:id')
+      .get(handlers.getMetadataMap)
+      .post(handlers.postMetadata)
 
-federatedRouter.get('/round/:task/:id', (req, res, next) => {
-  handlers.getRound(req, res).catch(next)
-})
-federatedRouter.get('/weights/:task/:id', (req, res, next) => {
-  handlers.getWeightsHandler(req, res).catch(next)
-})
+    router.get('/logs', handlers.queryLogs)
 
-federatedRouter.get('/statistics/:task/:id', (req, res, next) => {
-  handlers.getAsyncWeightInformantStatistics(req, res).catch(next)
-})
+    router.use('/tasks', tasksRouter)
 
-federatedRouter
-  .route('/metadata/:metadata/:task/:round/:id')
-  .get(handlers.getMetadataMap)
-  .post(handlers.postMetadata)
+    return router
+  }
 
-federatedRouter.get('/logs', handlers.queryLogs)
+  private setupDecentralizedRouter (tasksRouter: express.Router): express.Router {
+    const router = express.Router()
 
-federatedRouter.use('/tasks', tasksRouter)
+    router.use('/tasks', tasksRouter)
+    router.get('/', (_, res) => res.send('DeAI server'))
 
-// =======================================================================
-
-// Declaire decentralised routes
-const decentralisedRouter = express.Router()
-wsApplier.applyTo(decentralisedRouter)
-
-decentralisedRouter.use('/tasks', tasksRouter)
-decentralisedRouter.get('/', (_, res) => res.send('DeAI server'))
-
-export { federatedRouter, decentralisedRouter }
+    return router
+  }
+}
