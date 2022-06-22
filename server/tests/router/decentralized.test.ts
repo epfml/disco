3402d9759aaca1c1e14dca402916446bd28a327a
Alyssa Unell <aunell@mit.edu>
2022-06-22 16:26:13 +0200

    unfinished testing, finished first implementation of decenrtalized

---
+++ server/tests/router/decentralized.test.ts
@@ -0,0 +1,84 @@
+// import { agent as request } from 'supertest'
+//
+// import { serialization, Weights } from 'discojs'
+//
+// import * as tf from '@tensorflow/tfjs-node'
+// import { getApp } from '../../src/get_server'
+//
+// const platformID = 'deai'
+// const clients = {
+//   one: 'one',
+//   two: 'two'
+// }
+// const task = 'titanic'
+//
+// const weights: Weights = [tf.tensor([1, 1]), tf.tensor([1, 1])]
+//
+// const newRound = 1
+//
+// function connectHeader (
+//   platformID: string,
+//   taskID: string,
+//   clientID: string
+// ): string {
+//   return `/${platformID}/connect/${taskID}/${clientID}`
+// }
+//
+// function disconnectHeader (
+//   platformID: string,
+//   taskID: string,
+//   clientID: string
+// ): string {
+//   return `/${platformID}/disconnect/${taskID}/${clientID}`
+// }
+//
+// describe(`${platformID} simple connection tests`, function () {
+//   this.timeout(30_000)
+//
+//   it('connect and then disconnect to valid task', async () => {
+//     const app = await getApp()
+//
+//     await request(app)
+//       .get(connectHeader(platformID, task, clients.one))
+//       .expect(200)
+//     await request(app)
+//       .get(disconnectHeader(platformID, task, clients.one))
+//       .expect(200)
+//   })
+//
+//   it('connect to non existing task', async () => {
+//     // the single test
+//     const app = await getApp()
+//     await request(app)
+//       .get(connectHeader(platformID, 'fakeTask', clients.one))
+//       .expect(404)
+//   })
+// })
+//
+// describe(`${platformID} weight sharing tests`, function () {
+//   this.timeout(10_000)
+//
+//   it('GET /weights', async () => { // the single test
+//     await request(await getApp())
+//       .get(`/${platformID}/weights/${task}/${clients.one}`)
+//       .expect(200)
+//   })
+//
+//   it('POST /weights', async () => {
+//     const app = await getApp()
+//
+//     await request(app)
+//       .get(connectHeader(platformID, task, clients.one))
+//       .expect(200)
+//
+//     await request(app)
+//       .post(`/${platformID}/weights/${task}/${clients.one}`)
+//       .send({
+//         weights: await serialization.weights.encode(weights),
+//         round: newRound
+//       })
+//       .expect(200)
+//   })
+//
+//   // TODO: Add a test with a whole round, etc
+// })
