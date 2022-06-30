421c6ed50e7c36c17001fcb13a41085e319e96a5
s314cy <hugo.elguedj@epfl.ch>
2022-06-30 16:38:34 +0200

    browser/task-list: add scheme filters

--- browser/src/components/pages/TaskList.vue
+++ browser/src/components/pages/TaskList.vue
@@ -1,46 +1,90 @@
 <template>
-  <div class="flex flex-col gap-8 mt-8">
-    <div
-      v-for="[taskID, task] in tasks"
-      :key="taskID"
-    >
-      <ButtonCard
-        :click="() => goToSelection(taskID)"
-        :button-placement="'left'"
+  <div class="space-y-8 pt-8">
+    <div class="flex flex-row gap-8">
+      <ToggleButton
+        v-for="(filter, idx) in filters"
+        :key="idx"
+        @click="toggle(filter)"
       >
-        <template
-          #title
+        {{ filter.name }}
+      </ToggleButton>
+    </div>
+    <div class="flex flex-col gap-8 mt-8">
+      <div
+        v-for="task in filteredTasks"
+        :key="task.taskID"
+      >
+        <ButtonCard
+          :click="() => goToSelection(task.taskID)"
+          button-placement="left"
         >
-          {{ task.displayInformation.taskTitle }} - {{ task.trainingInformation.scheme }}
-        </template>
-        <template #text>
-          <div v-html="task.displayInformation.summary" />
-        </template>
-        <template #button>
-          Join
-        </template>
-      </ButtonCard>
+          <template
+            #title
+          >
+            {{ task.displayInformation.taskTitle }} - {{ task.trainingInformation.scheme }}
+          </template>
+          <template #text>
+            <div v-html="task.displayInformation.summary" />
+          </template>
+          <template #button>
+            Join
+          </template>
+        </ButtonCard>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import ButtonCard from '@/components/containers/ButtonCard.vue'
-
+import { defineComponent } from 'vue'
 import { mapState } from 'vuex'
 
-export default {
-  name: 'TaskList',
-  components: {
-    ButtonCard
-  },
-  computed: {
-    ...mapState(['tasks'])
-  },
-  methods: {
-    goToSelection (taskID: string) {
-      this.$router.push({ path: `/${taskID}` })
-    }
+import { Task } from 'discojs'
+
+import ButtonCard from '@/components/containers/ButtonCard.vue'
+import ToggleButton from '@/components/simple/ToggleButton.vue'
+
+class Filter {
+  public active: boolean
+
+  constructor (
+    public readonly name: string,
+    public readonly apply: (task: Task) => boolean
+  ) {
+    this.active = false
   }
 }
+
+export default defineComponent({
+  name: 'TaskList',
+  components: {
+    ButtonCard,
+    ToggleButton
+  },
+  data (): { filters: Filter[] } {
+    return {
+      filters: ['Decentralized', 'Federated']
+        .map((scheme: string) =>
+          new Filter(scheme, (task: Task) =>
+            task.trainingInformation.scheme === scheme))
+    }
+  },
+  computed: {
+    ...mapState(['tasks']),
+    filteredTasks (): Task[] {
+      return (Array.from(this.tasks.values()) as Task[])
+        .filter((task: Task) =>
+          this.filters.every((filter: Filter) =>
+            filter.active ? filter.apply(task) : true))
+    }
+  },
+  methods: {
+    goToSelection (taskID: string): void {
+      this.$router.push({ path: `/${taskID}` })
+    },
+    toggle (filter: Filter): void {
+      filter.active = !filter.active
+    }
+  }
+})
 </script>
