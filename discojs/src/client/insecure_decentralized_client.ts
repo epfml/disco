bb93df082bf9d36f3611710fb552abf6ba896267
Alyssa Unell <aunell@mit.edu>
2022-07-07 22:45:54 +0200

    updated secure and insecure decentralized with server keeping track of connections, both still have bug with peer.connected() functionaliy. This bug makes secure decentralized fail end to end test. Also, insecure decentralized will now fail the connection test because they will not be connected unless training is complete

--- discojs/src/client/insecure_decentralized_client.ts
+++ discojs/src/client/insecure_decentralized_client.ts
@@ -7,9 +7,58 @@ import * as decentralizedGeneral from './decentralized'
 
 import { aggregation, privacy, serialization, TrainingInformant, Weights } from '..'
 
-// import { Base } from './base'
+type PeerID = number
+
 import { DecentralizedGeneral } from './decentralized'
 interface PeerMessage { epoch: number, weights: serialization.weights.Encoded }
+interface ConnectedPeerIDsMessage {peerIDs: Array<number>}
+interface ClientReadyMessage { peerId: PeerID, epoch: number}
+
+function isClientReadyMessage (data: unknown): data is ClientReadyMessage {
+  if (typeof data !== 'object') {
+    return false
+  }
+  if (data === null) {
+    return false
+  }
+
+  if (!Set(Object.keys(data)).equals(Set.of('peerId', 'epoch'))) {
+    return false
+  }
+  const { peerId, epoch } = data as Record<'peerId' | 'epoch', unknown>
+
+  if (
+    typeof epoch !== 'number' ||
+      typeof peerId !== 'number'
+  ) {
+    return false
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _: ClientReadyMessage = { peerId, epoch }
+  return true
+}
+
+function isConnectedPeerIDsMessage (data: unknown): data is ConnectedPeerIDsMessage {
+  if (typeof data !== 'object') {
+    return false
+  }
+  if (data === null) {
+    return false
+  }
+
+  if (!Set(Object.keys(data)).equals(Set.of('peerIDs'))) {
+    return false
+  }
+  const { peerIDs } = data as Record<'peerIDs', unknown>
+
+  if (!(peerIDs instanceof Array)
+  ) {
+    return false
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _: ConnectedPeerIDsMessage = { peerIDs }
+  return true
+}
 
 // Time to wait between network checks in milliseconds.
 const TICK = 100
@@ -23,22 +72,46 @@ const MAX_WAIT_PER_ROUND = 10_000
  */
 export class InsecureDecentralized extends DecentralizedGeneral {
   private readonly receivedWeights = Map<SimplePeer.Instance, List<Weights | undefined>>()
+  private ID: number = 0
 
   peerOnData (peer: SimplePeer.Instance, peerID: number, data: any): void {
     const message = msgpack.decode(data)
-    if (!decentralizedGeneral.isPeerMessage(message)) {
-      throw new Error(`invalid message received from ${peerID}`)
-    }
-    const weights = serialization.weights.decode(message.weights)
+    if (decentralizedGeneral.isPeerMessage(message)) {
+      const weights = serialization.weights.decode(message.weights)
+      console.debug('peer', peerID, 'sent weights', weights)
 
-    console.debug('peer', peerID, 'sent weights', weights)
-
-    if (this.receivedWeights.get(peer)?.get(message.epoch) !== undefined) {
-      throw new Error(`weights from ${peerID} already received`)
+      if (this.receivedWeights.get(peer)?.get(message.epoch) !== undefined) {
+        throw new Error(`weights from ${peerID} already received`)
+      }
+      this.receivedWeights.set(peer,
+          this.receivedWeights.get(peer, List<Weights>())
+              .set(message.epoch, weights))
     }
-    this.receivedWeights.set(peer,
-      this.receivedWeights.get(peer, List<Weights>())
-        .set(message.epoch, weights))
+    else if (isConnectedPeerIDsMessage(message)){
+            let peerIDList: Array<number> = message.peerIDs
+      for (peerID of peerIDList){
+        let initiator = false
+        if(this.ID<=peerID){ //THIS.ID IS NOT WORKING
+          initiator = true
+        }
+        this.connectNewPeer(peerID, initiator)
+      }
+    }
+  }
+  // sends message to server that they are ready to share
+  private sendReadyMessage (epoch: number, trainingInformant: TrainingInformant
+  ): void {
+    // Broadcast our readiness
+    const msg: ClientReadyMessage = {
+      peerId: this.ID,
+      epoch: epoch
+    }
+
+    const encodedMsg = msgpack.encode(msg)
+    if (this.server === undefined){
+      throw new Error('server undefined, could not connect peers')
+    }
+    this.server.send(encodedMsg)
   }
 
   async onRoundEndCommunication (
@@ -48,6 +121,8 @@ export class InsecureDecentralized extends DecentralizedGeneral {
     trainingInformant: TrainingInformant
   ): Promise<Weights> {
     // send message to server that we ready
+    this.sendReadyMessage(epoch, trainingInformant)
+
     const noisyWeights = privacy.addDifferentialPrivacy(updatedWeights, staleWeights, this.task)
     // Broadcast our weights
     const msg: PeerMessage = {
@@ -57,6 +132,7 @@ export class InsecureDecentralized extends DecentralizedGeneral {
     const encodedMsg = msgpack.encode(msg)
 
     // wait until receive message of peers from server
+    console.log('waiting for message of peers')
     this.peers
       .filter((peer) => peer.connected)
       .forEach((peer, peerID) => {
@@ -65,7 +141,6 @@ export class InsecureDecentralized extends DecentralizedGeneral {
         console.log('Sending weights to peer', peerID)
         peer.send(encodedMsg)
       })
-    // this.weights is empty
 
     // Get weights from the others
     const getWeights = (): Seq.Indexed<Weights | undefined> =>
