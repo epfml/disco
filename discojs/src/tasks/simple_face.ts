62ac34673683c4abb191ee6f021ff02f0b9e92d4
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2022-07-07 15:26:14 +0200

    simple face edit, note, maybe linter broken?

--- discojs/src/tasks/simple_face.ts
+++ discojs/src/tasks/simple_face.ts
@@ -2,39 +2,39 @@ import * as tf from '@tensorflow/tfjs'
 
 import { Task } from '../task'
 
-export const task: Task =
-  {
-    taskID: 'simple_face',
-    displayInformation: {
-      taskTitle: 'Simple Face',
-      summary: 'Can you detect if the person in a picture is a child or an adult?',
-      overview: 'Simple face is a small subset of face_task from Kaggle',
-      limitations: 'The training data is limited to small images of size 200x200.',
-      tradeoffs: 'Training success strongly depends on label distribution',
-      dataFormatInformation: '',
-      dataExampleText: 'Bellow you can find an example',
-      dataExampleImage: './simple_face-example.png'
+export const task: Task = {
+  taskID: 'simple_face',
+  displayInformation: {
+    taskTitle: 'Simple Face',
+    summary: 'Can you detect if the person in a picture is a child or an adult?',
+    overview: 'Simple face is a small subset of face_task from Kaggle',
+    limitations: 'The training data is limited to small images of size 200x200.',
+    tradeoffs: 'Training success strongly depends on label distribution',
+    dataFormatInformation: '',
+    dataExampleText: 'Bellow you can find an example',
+    dataExampleImage: './simple_face-example.png'
+  },
+  trainingInformation: {
+    modelID: 'simple_face-model',
+    epochs: 50,
+    roundDuration: 1,
+    validationSplit: 0.2,
+    batchSize: 10,
+    preprocessFunctions: [],
+    learningRate: 0.001,
+    modelCompileData: {
+      optimizer: 'sgd',
+      loss: 'categoricalCrossentropy',
+      metrics: ['accuracy']
     },
-    trainingInformation: {
-      modelID: 'simple_face-model',
-      epochs: 50,
-      roundDuration: 10,
-      validationSplit: 0.2,
-      batchSize: 10,
-      preprocessFunctions: [],
-      modelCompileData: {
-        optimizer: 'adam',
-        loss: 'categoricalCrossentropy',
-        metrics: ['accuracy']
-      },
-      dataType: 'image',
-      csvLabels: false,
-      IMAGE_H: 200,
-      IMAGE_W: 200,
-      LABEL_LIST: ['child', 'adult'],
-      scheme: 'Federated'
-    }
+    dataType: 'image',
+    csvLabels: false,
+    IMAGE_H: 200,
+    IMAGE_W: 200,
+    LABEL_LIST: ['child', 'adult'],
+    scheme: 'Federated'
   }
+}
 
 export function model (imageWidth = 200, imageHeight = 200, imageChannels = 3, numOutputClasses = 2): tf.LayersModel {
   const model = tf.sequential()
