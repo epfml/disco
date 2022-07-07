2d47ea998a0674dcd067cc6e4c5028f54097bbcb
Alyssa Unell <aunell@mit.edu>
2022-07-07 14:40:45 +0200

    beginning of implementation of federated approach to obtaining peers to connect to (using the signaling server), can uncomment the code block in signaling_server.ts for end_to_end_decentralized.ts to run appropriately for decentralized insecure clients

--- server/tests/end_to_end_decentralized.ts
+++ server/tests/end_to_end_decentralized.ts
@@ -33,66 +33,40 @@ describe('end to end', function () {
     server?.close()
   })
 
-  // it('Connect 2 clients to titanic and check that they connect to each other.',
-  //   async () => {
-  //     const result = await titanicConnectTest()
-  //     expect(result).to.equal(true)
-  //   }
-  // )
-  //
-  // async function titanicUser (): Promise<CLIENT_TYPE> {
-  //   const titanic = tasks.titanic.task
-  //
-  //   const cli = await getClient(CLIENT_CONSTRUCTOR, server, titanic)
-  //   await cli.connect()
-  //   return cli
-  // }
+  it('Connect 2 clients to titanic and check that they connect to each other.',
+    async () => {
+      const result = await titanicConnectTest()
+      expect(result).to.equal(true)
+    }
+  )
 
-  // async function titanicConnectTest (): Promise<boolean> {
-  //   const clients = await Promise.all([titanicUser(), titanicUser()])
-  //
-  //   await new Promise<void>(resolve => setTimeout(resolve, CONNECTION_TIMEOUT))
-  //   console.log('\n---------------------------\n')
-  //   for (const i in clients) {
-  //     // eslint-disable-next-line @typescript-eslint/no-base-to-string
-  //     console.log('client', i, 'is connected to peers:', clients[i].getPeerIDs().toString())
-  //     console.log('\n')
-  //   }
-  //   return (clients[0].getPeerIDs().size === 1 && clients[1].getPeerIDs().size === 1)
-  // }
-  //
-  // it('onRoundEndCommunication leads to correct aggregation of weights')
-  //
-  // it('runs cifar 10 with two decentralized users', async () =>
-  //   await Promise.all([cifar10user(), cifar10user(), cifar10user()]))
-  //
-  // async function cifar10user (): Promise<void> {
-  //   const dir = '../discojs/example_training_data/CIFAR10/'
-  //   const files = (await fs.readdir(dir)).map((file) => path.join(dir, file))
-  //   const labels = Range(0, 24).map((label) => (label % 10).toString()).toArray()
-  //
-  //   const cifar10 = tasks.cifar10.task
-  //
-  //   const loaded = await new NodeImageLoader(cifar10).loadAll(files, { labels: labels })
-  //
-  //   const cli = await getClient(client.InsecureDecentralized, server, cifar10)
-  //   await cli.connect()
-  //
-  //   const disco = new training.Disco(
-  //     cifar10,
-  //     new ConsoleLogger(),
-  //     new EmptyMemory(),
-  //     SCHEME,
-  //     new TrainingInformant(10, cifar10.taskID, SCHEME),
-  //     cli
-  //   )
-  //   await disco.startTraining(loaded)
-  // }
+  async function titanicUser (): Promise<CLIENT_TYPE> {
+    const titanic = tasks.titanic.task
 
-it('runs cifar 10 with three secure decentralized users', async () =>
-    await Promise.all([cifar10userIn(), cifar10userIn(), cifar10userIn()]))
+    const cli = await getClient(CLIENT_CONSTRUCTOR, server, titanic)
+    await cli.connect()
+    return cli
+  }
 
-  async function cifar10userIn (): Promise<void> {
+  async function titanicConnectTest (): Promise<boolean> {
+    const clients = await Promise.all([titanicUser(), titanicUser()])
+
+    await new Promise<void>(resolve => setTimeout(resolve, CONNECTION_TIMEOUT))
+    console.log('\n---------------------------\n')
+    for (const i in clients) {
+      // eslint-disable-next-line @typescript-eslint/no-base-to-string
+      console.log('client', i, 'is connected to peers:', clients[i].getPeerIDs().toString())
+      console.log('\n')
+    }
+    return (clients[0].getPeerIDs().size === 1 && clients[1].getPeerIDs().size === 1)
+  }
+
+  it('onRoundEndCommunication leads to correct aggregation of weights')
+
+  it('runs cifar 10 with two decentralized users', async () =>
+    await Promise.all([cifar10user(), cifar10user()]))
+
+  async function cifar10user (): Promise<void> {
     const dir = '../discojs/example_training_data/CIFAR10/'
     const files = (await fs.readdir(dir)).map((file) => path.join(dir, file))
     const labels = Range(0, 24).map((label) => (label % 10).toString()).toArray()
@@ -101,7 +75,7 @@ it('runs cifar 10 with three secure decentralized users', async () =>
 
     const loaded = await new NodeImageLoader(cifar10).loadAll(files, { labels: labels })
 
-    const cli = await getClient(client.SecureDecentralized, server, cifar10)
+    const cli = await getClient(client.InsecureDecentralized, server, cifar10)
     await cli.connect()
 
     const disco = new training.Disco(
@@ -114,5 +88,31 @@ it('runs cifar 10 with three secure decentralized users', async () =>
     )
     await disco.startTraining(loaded)
   }
+
+  // it('runs cifar 10 with three secure decentralized users', async () =>
+  //   await Promise.all([cifar10userIn(), cifar10userIn(), cifar10userIn()]))
+  //
+  // async function cifar10userIn (): Promise<void> {
+  //   const dir = '../discojs/example_training_data/CIFAR10/'
+  //   const files = (await fs.readdir(dir)).map((file) => path.join(dir, file))
+  //   const labels = Range(0, 24).map((label) => (label % 10).toString()).toArray()
+  //
+  //   const cifar10 = tasks.cifar10.task
+  //
+  //   const loaded = await new NodeImageLoader(cifar10).loadAll(files, { labels: labels })
+  //
+  //   const cli = await getClient(client.SecureDecentralized, server, cifar10)
+  //   await cli.connect()
+  //
+  //   const disco = new training.Disco(
+  //     cifar10,
+  //     new ConsoleLogger(),
+  //     new EmptyMemory(),
+  //     SCHEME,
+  //     new TrainingInformant(10, cifar10.taskID, SCHEME),
+  //     cli
+  //   )
+  //   await disco.startTraining(loaded)
+  // }
 }
 )
