684d1dd49937158383f9c9e9fc859331417eeb7c
Alyssa Unell <aunell@mit.edu>
2022-07-07 14:40:45 +0200

    beginning of implementation of federated approach to obtaining peers to connect to (using the signaling server), can uncomment the code block in signaling_server.ts for end_to_end_decentralized.ts to run appropriately for decentralized insecure clients

--- server/src/router/signaling_server.ts
+++ server/src/router/signaling_server.ts
@@ -5,12 +5,14 @@ import WebSocket from 'ws'
 import { TaskID } from 'discojs'
 
 type PeerID = number
+type epoch = number
 type EncodedSignal = Buffer
 
-type OpeningMessage = PeerID[]
-type PeerMessage = [PeerID, EncodedSignal]
+type connectedPeerIDsMessage = PeerID[]
+type ServerPeerMessage = [PeerID, EncodedSignal]
+type ReadyMessage = [PeerID, epoch]
 
-function isPeerMessage (msg: unknown): msg is PeerMessage {
+function isServerPeerMessage (msg: unknown): msg is ServerPeerMessage {
   if (!(msg instanceof Array)) {
     return false
   }
@@ -27,7 +29,26 @@ function isPeerMessage (msg: unknown): msg is PeerMessage {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const _: PeerMessage = [id, signal]
+  const _: ServerPeerMessage = [id, signal]
+
+  return true
+}
+
+function isReadyMessage (msg: unknown): msg is ReadyMessage {
+  if (!(msg instanceof Array)) {
+    return false
+  }
+  if (msg.length !== 2) {
+    return false
+  }
+
+  const [id, epoch] = msg
+  if (typeof id !== 'number' || typeof epoch!== 'number') {
+    return false
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _: ReadyMessage = [id, epoch]
 
   return true
 }
@@ -45,39 +66,54 @@ function isPeerMessage (msg: unknown): msg is PeerMessage {
 export class SignalingServer {
   private rooms: Map<TaskID, Map<PeerID, WebSocket>> = Map()
   private clientCounter: PeerID = 0
+  private readyClients: PeerID[] = []
+  private minConnected: number = 3
 
   handle (taskID: TaskID, ws: WebSocket): void {
     const peerID: PeerID = this.clientCounter++
 
     console.info('peer', peerID, 'joined', taskID)
 
-    const msg: OpeningMessage =
-      this.rooms.get(taskID, Map<PeerID, WebSocket>())
-        .keySeq()
-        .toArray()
+    // const msg: connectedPeerIDsMessage =
+    //   this.rooms.get(taskID, Map<PeerID, WebSocket>())
+    //     .keySeq()
+    //     .toArray()
 
-    const oldRoom = this.rooms.get(taskID, Map<PeerID, WebSocket>())
-    this.rooms = this.rooms.set(taskID, oldRoom.set(peerID, ws))
-
-    ws.send(msgpack.encode(msg), { binary: true })
+    // const oldRoom = this.rooms.get(taskID, Map<PeerID, WebSocket>())
+    // this.rooms = this.rooms.set(taskID, oldRoom.set(peerID, ws))
+    //
+    // ws.send(msgpack.encode(msg), { binary: true })
 
     ws.on('message', (data: Buffer) => {
       try {
-        const peerMessage = msgpack.decode(data)
-        if (!isPeerMessage(peerMessage)) {
-          throw new Error('recv msg is not valid')
-        }
-
-        console.debug('peer', peerID, 'send message to', peerMessage[0])
+        const msg = msgpack.decode(data)
+        if (isServerPeerMessage(msg)) { //connect peers?
+          console.debug('peer', peerID, 'send message to', msg[0])
 
         const peerToSendTo = this.rooms
           .get(taskID, Map<PeerID, WebSocket>())
-          .get(peerMessage[0])
+          .get(msg[0])
         if (peerToSendTo === undefined) {
           throw new Error('no such peer to send to')
         }
-        const forwardMsg: PeerMessage = [peerID, peerMessage[1]]
+        const forwardMsg: ServerPeerMessage = [peerID, msg[1]]
         peerToSendTo.send(msgpack.encode(forwardMsg), { binary: true })
+        }
+        else if (isReadyMessage(msg)){
+          this.readyClients.push(msg[0])
+          if(this.readyClients.length>this.minConnected){
+            //send peerIds to everyone in readyClients
+            const connectedPeerIDs: connectedPeerIDsMessage = this.readyClients
+            for(let peer of this.readyClients){
+              const peerToSendTo = this.rooms.get(taskID, Map<PeerID, WebSocket>())
+               .get(peer) //iterate through readyClient peerIDs
+              if (peerToSendTo === undefined) {
+                throw new Error('no such peer to send to')
+                }
+              peerToSendTo.send(msgpack.encode(connectedPeerIDs), { binary: true })
+            }
+          }
+        }
       } catch (e) {
         console.error('when processing WebSocket message', e)
       }
