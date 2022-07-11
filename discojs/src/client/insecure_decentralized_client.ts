ac20a40c5dd11d4a0e397ab2e0cb80a33e34c127
Alyssa Unell <aunell@mit.edu>
2022-07-11 17:48:24 +0200

    beginning of rerouting information away from peer to peer and through signaling server, passes basic tests but need to fix ability of the client/server to differentiate between what kinds of messages they are receiving

--- discojs/src/client/insecure_decentralized_client.ts
+++ discojs/src/client/insecure_decentralized_client.ts
@@ -6,59 +6,15 @@ import SimplePeer from 'simple-peer'
 import * as decentralizedGeneral from './decentralized'
 
 import { aggregation, privacy, serialization, TrainingInformant, Weights } from '..'
+import { DecentralizedGeneral } from './decentralized'
 
 type PeerID = number
+type EncodedSignal = Uint8Array
 
-import { DecentralizedGeneral } from './decentralized'
-interface PeerMessage { epoch: number, weights: serialization.weights.Encoded }
-interface ConnectedPeerIDsMessage {peerIDs: Array<number>}
-interface ClientReadyMessage { peerId: PeerID, epoch: number}
-
-function isClientReadyMessage (data: unknown): data is ClientReadyMessage {
-  if (typeof data !== 'object') {
-    return false
-  }
-  if (data === null) {
-    return false
-  }
-
-  if (!Set(Object.keys(data)).equals(Set.of('peerId', 'epoch'))) {
-    return false
-  }
-  const { peerId, epoch } = data as Record<'peerId' | 'epoch', unknown>
-
-  if (
-    typeof epoch !== 'number' ||
-      typeof peerId !== 'number'
-  ) {
-    return false
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const _: ClientReadyMessage = { peerId, epoch }
-  return true
-}
-
-function isConnectedPeerIDsMessage (data: unknown): data is ConnectedPeerIDsMessage {
-  if (typeof data !== 'object') {
-    return false
-  }
-  if (data === null) {
-    return false
-  }
-
-  if (!Set(Object.keys(data)).equals(Set.of('peerIDs'))) {
-    return false
-  }
-  const { peerIDs } = data as Record<'peerIDs', unknown>
-
-  if (!(peerIDs instanceof Array)
-  ) {
-    return false
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const _: ConnectedPeerIDsMessage = { peerIDs }
-  return true
-}
+type clientReadyMessage = [PeerID: PeerID, round:number] //client is ready
+type clientWeightsMessageServer = [PeerID: number, weights: serialization.weights.Encoded] //client weights
+type clientPartialSumsMessageServer = [PeerID, EncodedSignal] //client partial sum
+type serverConnectedClients = List<PeerID> //server send to client who to connect to
 
 // Time to wait between network checks in milliseconds.
 const TICK = 100
@@ -66,105 +22,77 @@ const TICK = 100
 // Time to wait for the others in milliseconds.
 const MAX_WAIT_PER_ROUND = 10_000
 
+const minimumPeers = 3
+
 /**
  * Class that deals with communication with the PeerJS server.
  * Collects the list of receivers currently connected to the PeerJS server.
  */
 export class InsecureDecentralized extends DecentralizedGeneral {
-  private readonly receivedWeights = Map<SimplePeer.Instance, List<Weights | undefined>>()
-  private ID: number = 0
 
-  peerOnData (peer: SimplePeer.Instance, peerID: number, data: any): void {
-    const message = msgpack.decode(data)
-    if (decentralizedGeneral.isPeerMessage(message)) {
-      const weights = serialization.weights.decode(message.weights)
-      console.debug('peer', peerID, 'sent weights', weights)
-
-      if (this.receivedWeights.get(peer)?.get(message.epoch) !== undefined) {
-        throw new Error(`weights from ${peerID} already received`)
-      }
-      this.receivedWeights.set(peer,
-          this.receivedWeights.get(peer, List<Weights>())
-              .set(message.epoch, weights))
-    }
-    else if (isConnectedPeerIDsMessage(message)){
-            let peerIDList: Array<number> = message.peerIDs
-      for (peerID of peerIDList){
-        let initiator = false
-        if(this.ID<=peerID){ //THIS.ID IS NOT WORKING
-          initiator = true
-        }
-        this.connectNewPeer(peerID, initiator)
-      }
-    }
-  }
-  // sends message to server that they are ready to share
-  private sendReadyMessage (epoch: number, trainingInformant: TrainingInformant
+  private sendReadyMessage (round: number, trainingInformant: TrainingInformant
   ): void {
     // Broadcast our readiness
-    const msg: ClientReadyMessage = {
-      peerId: this.ID,
-      epoch: epoch
-    }
+    const msg: clientReadyMessage = [this.ID, round]
 
     const encodedMsg = msgpack.encode(msg)
     if (this.server === undefined){
       throw new Error('server undefined, could not connect peers')
     }
-    this.server.send(encodedMsg)
+    this.server.send(encodedMsg) //why sending an encoded message?
   }
 
   async onRoundEndCommunication (
     updatedWeights: Weights,
     staleWeights: Weights,
-    epoch: number,
+    round: number,
     trainingInformant: TrainingInformant
   ): Promise<Weights> {
-    console.log('onRoundEndCommuncation called. Epoch =', epoch)
     // send message to server that we ready
-    this.sendReadyMessage(epoch, trainingInformant)
+    this.sendReadyMessage(round, trainingInformant)
 
     const noisyWeights = privacy.addDifferentialPrivacy(updatedWeights, staleWeights, this.task)
+
     // Broadcast our weights
-    const msg: PeerMessage = {
-      epoch: epoch,
-      weights: await serialization.weights.encode(noisyWeights)
-    }
+    const msg: clientWeightsMessageServer = [round, await serialization.weights.encode(noisyWeights)]
     const encodedMsg = msgpack.encode(msg)
 
-    for (const [peerID_, peer_] of this.peers) {
-      console.log('Is peer', peerID_, 'connected? ', peer_.connected)
-      if (!peer_.connected) {
-        const connectedPeers = this.peers.filter((peer) => peer.connected)
-        console.log(connectedPeers)
-        console.log(connectedPeers.size)
-        console.log()
+    if(this.peers.size>=minimumPeers){
+      if(this.server ===undefined){
+        throw new Error ('server undefined so we cannot send weights through it')
       }
+      this.peerMessageTemp(encodedMsg)
     }
+    // for (const [peerID_, peer_] of this.peers) {
+    //   console.log('Is peer', peerID_, 'connected? ', peer_.connected)
+    //   if (!peer_.connected) {
+    //     const connectedPeers = this.peers.filter((peer) => peer.connected)
+    //   }
+    // }
 
-    if (this.peers.filter((peer) => peer.connected).size === 0) {
-      await new Promise<void>(resolve => setTimeout(resolve, 2 * 1000))
-      for (const [peerID_, peer_] of this.peers) {
-        console.log('Is peer', peerID_, 'connected? ', peer_.connected)
-      }
-    }
+    // if (this.peers.size === 0) {
+    //   await new Promise<void>(resolve => setTimeout(resolve, 5 * 1000))
+    //   for (const [peerID_, peer_] of this.peers) {
+    //     console.log('Is peer', peerID_, 'connected? ', peer_.connected)
+    //   }
+    // }
 
     // wait until receive message of peers from server
-    console.log('waiting for message of peers')
-    this.peers
-      .filter((peer) => peer.connected)
-      .forEach((peer, peerID) => {
-        trainingInformant.addMessage(`Sending weights to peer ${peerID}`)
-        trainingInformant.updateWhoReceivedMyModel(`peer ${peerID}`)
-        console.log('Sending weights to peer', peerID)
-        peer.send(encodedMsg)
-      })
+    // console.log('waiting for message of peers')
+    // this.peers
+    //   .filter((peer) => peer.connected)
+    //   .forEach((peer, peerID) => {
+    //     trainingInformant.addMessage(`Sending weights to peer ${peerID}`)
+    //     trainingInformant.updateWhoReceivedMyModel(`peer ${peerID}`)
+    //     console.log('Sending weights to peer', peerID)
+    //     peer.send(encodedMsg)
+    //   })
 
     // Get weights from the others
     const getWeights = (): Seq.Indexed<Weights | undefined> =>
       this.receivedWeights
         .valueSeq()
-        .map((epochesWeights) => epochesWeights.get(epoch))
+        .map((roundWeights) => roundWeights.get(round))
 
     const timeoutError = new Error('timeout')
     await new Promise<void>((resolve, reject) => {
--- server/tests/end_to_end_decentralized.ts
+++ server/tests/end_to_end_decentralized.ts
@@ -4,8 +4,9 @@ import { Server } from 'node:http'
 import { Range } from 'immutable'
 import * as tf from '@tensorflow/tfjs-node'
 import * as chai from 'chai'
+import * as http from 'http'
 
-import { dataset, ConsoleLogger, training, TrainingSchemes, TrainingInformant, EmptyMemory, tasks, client } from 'discojs'
+import { dataset, ConsoleLogger, training, TrainingSchemes, TrainingInformant, EmptyMemory, tasks, client, Weights} from 'discojs'
 
 import { getClient, startServer } from './utils'
 const expect = chai.expect
@@ -22,6 +23,14 @@ class NodeImageLoader extends dataset.ImageLoader<string> {
   }
 }
 
+function makeWeights (values: any): Weights {
+  const w: Weights = []
+  for (let i = 0; i < 1; i++) {
+    w.push(tf.tensor(values))
+  }
+  return w
+}
+
 describe('end to end', function () {
   this.timeout(50_000)
 
@@ -34,13 +43,13 @@ describe('end to end', function () {
   })
 
   // it('Connect 2 clients to titanic and check that they connect to each other.',
-  //   async () => {
-  //     const result = await titanicConnectTest()
-  //     expect(result).to.equal(true)
-  //   }
+  //     async () => {
+  //       const result = await titanicConnectTest()
+  //       expect(result).to.equal(true)
+  //     }
   // )
-  //
-  // async function titanicUser (): Promise<CLIENT_TYPE> {
+
+  // async function titanicUser(): Promise<CLIENT_TYPE> {
   //   const titanic = tasks.titanic.task
   //
   //   const cli = await getClient(CLIENT_CONSTRUCTOR, server, titanic)
@@ -48,7 +57,7 @@ describe('end to end', function () {
   //   return cli
   // }
 
-  // async function titanicConnectTest (): Promise<boolean> {
+  // async function titanicConnectTest(): Promise<boolean> {
   //   const clients = await Promise.all([titanicUser(), titanicUser()])
   //
   //   await new Promise<void>(resolve => setTimeout(resolve, CONNECTION_TIMEOUT))
@@ -60,59 +69,83 @@ describe('end to end', function () {
   //   }
   //   return (clients[0].getPeerIDs().size === 1 && clients[1].getPeerIDs().size === 1)
   // }
-  //
-  // it('onRoundEndCommunication leads to correct aggregation of weights')
+
 
   it('runs cifar 10 with two decentralized users', async () =>
-    await Promise.all([cifar10user(), cifar10user()]))
+      await Promise.all([cifar10user(), cifar10user()]))
 
-  async function cifar10user (): Promise<void> {
+  async function cifar10user(): Promise<void> {
     const dir = '../discojs/example_training_data/CIFAR10/'
     const files = (await fs.readdir(dir)).map((file) => path.join(dir, file))
     const labels = Range(0, 24).map((label) => (label % 10).toString()).toArray()
 
     const cifar10 = tasks.cifar10.task
 
-    const loaded = await new NodeImageLoader(cifar10).loadAll(files, { labels: labels })
+    const loaded = await new NodeImageLoader(cifar10).loadAll(files, {labels: labels})
 
     const cli = await getClient(client.InsecureDecentralized, server, cifar10)
     await cli.connect()
 
     const disco = new training.Disco(
-      cifar10,
-      new ConsoleLogger(),
-      new EmptyMemory(),
-      SCHEME,
-      new TrainingInformant(10, cifar10.taskID, SCHEME),
-      cli
+        cifar10,
+        new ConsoleLogger(),
+        new EmptyMemory(),
+        SCHEME,
+        new TrainingInformant(10, cifar10.taskID, SCHEME),
+        cli
     )
     await disco.startTraining(loaded)
   }
 
-  it('runs cifar 10 with three secure decentralized users', async () =>
-    await Promise.all([cifar10userIn(), cifar10userIn(), cifar10userIn()]))
 
-  async function cifar10userIn (): Promise<void> {
-    const dir = '../discojs/example_training_data/CIFAR10/'
-    const files = (await fs.readdir(dir)).map((file) => path.join(dir, file))
-    const labels = Range(0, 24).map((label) => (label % 10).toString()).toArray()
+// it('decentralized client test one round', async () =>
+//     await testWeightSharingInsecure())
+//
+//   /*
+//   makes client to connect to server and share input weights
+//    */
+//   async function makeClient(input: number[]): Promise<Weights>{
+//     const TASK = tasks.cifar10.task
+//     const clientCurrent: client.InsecureDecentralized = await getClient(client.InsecureDecentralized, server, TASK)
+//     const weights: Weights = makeWeights(input)
+//     const trainingInformant1: TrainingInformant = new TrainingInformant(0, '0', TrainingSchemes.DECENTRALIZED)
+//     clientCurrent.connect()
+//     return await clientCurrent.onRoundEndCommunication(weights, weights, 0, trainingInformant1)
+//   }
+//
+//   async function testWeightSharingInsecure(): Promise<Weights> {
+//     //expected --> [6, 7, 13]
+//     makeClient([3,3,3])
+//     makeClient([4,5,6])
+//     return makeClient([11,13,30])
+//   }
 
-    const cifar10 = tasks.cifar10.task
-
-    const loaded = await new NodeImageLoader(cifar10).loadAll(files, { labels: labels })
-
-    const cli = await getClient(client.SecureDecentralized, server, cifar10)
-    await cli.connect()
-
-    const disco = new training.Disco(
-      cifar10,
-      new ConsoleLogger(),
-      new EmptyMemory(),
-      SCHEME,
-      new TrainingInformant(10, cifar10.taskID, SCHEME),
-      cli
-    )
-    await disco.startTraining(loaded)
-  }
+// console.log(testDecentralized())
+//
+//   it('runs cifar 10 with three secure decentralized users', async () =>
+//     await Promise.all([cifar10userIn(), cifar10userIn(), cifar10userIn()]))
+//
+//   async function cifar10userIn (): Promise<void> {
+//     const dir = '../discojs/example_training_data/CIFAR10/'
+//     const files = (await fs.readdir(dir)).map((file) => path.join(dir, file))
+//     const labels = Range(0, 24).map((label) => (label % 10).toString()).toArray()
+//
+//     const cifar10 = tasks.cifar10.task
+//
+//     const loaded = await new NodeImageLoader(cifar10).loadAll(files, { labels: labels })
+//
+//     const cli = await getClient(client.SecureDecentralized, server, cifar10)
+//     await cli.connect()
+//
+//     const disco = new training.Disco(
+//       cifar10,
+//       new ConsoleLogger(),
+//       new EmptyMemory(),
+//       SCHEME,
+//       new TrainingInformant(10, cifar10.taskID, SCHEME),
+//       cli
+//     )
+//     await disco.startTraining(loaded)
+//   }
 }
 )
