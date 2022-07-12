edecb01bccbed9a45e3ab9c21eb9ff55d1dfb07a
Alyssa Unell <aunell@mit.edu>
2022-07-12 17:52:51 +0200

    es lint

--- discojs/src/client/insecure_decentralized_client.ts
+++ discojs/src/client/insecure_decentralized_client.ts
@@ -1,11 +1,7 @@
-import { List, Seq, Set, Map } from 'immutable'
-// import isomorphic from 'isomorphic-ws'
+import { List, Set } from 'immutable'
 import msgpack from 'msgpack-lite'
-import SimplePeer from 'simple-peer'
-// import { URL } from 'url'
-import * as decentralizedGeneral from './decentralized'
 
-import { aggregation, privacy, serialization, TrainingInformant, Weights} from '..'
+import { aggregation, privacy, serialization, TrainingInformant, Weights } from '..'
 import { DecentralizedGeneral } from './decentralized'
 import * as messages from '../messages'
 
@@ -24,16 +20,15 @@ const arbitraryNegativeNumber = -10
  * Collects the list of receivers currently connected to the PeerJS server.
  */
 export class InsecureDecentralized extends DecentralizedGeneral {
-
   private sendReadyMessage (round: number): void {
     // Broadcast our readiness
-    const msg: messages.clientReadyMessage = {type:messages.messageType.clientReadyMessage, round:round}
+    const msg: messages.clientReadyMessage = { type: messages.messageType.clientReadyMessage, round: round }
 
     const encodedMsg = msgpack.encode(msg)
-    if (this.server === undefined){
+    if (this.server === undefined) {
       throw new Error('server undefined, could not connect peers')
     }
-    this.server.send(encodedMsg) //why sending an encoded message?
+    this.server.send(encodedMsg) // why sending an encoded message?
   }
 
   async onRoundEndCommunication (
@@ -49,21 +44,21 @@ export class InsecureDecentralized extends DecentralizedGeneral {
     const noisyWeights = privacy.addDifferentialPrivacy(updatedWeights, staleWeights, this.task)
 
     // Broadcast our weights
-    const msg: messages.clientWeightsMessageServer = {type: messages.messageType.clientWeightsMessageServer, peerID: this.ID, weights: await serialization.weights.encode(noisyWeights)}
+    const msg: messages.clientWeightsMessageServer = { type: messages.messageType.clientWeightsMessageServer, peerID: this.ID, weights: await serialization.weights.encode(noisyWeights) }
     const encodedMsg = msgpack.encode(msg)
     let peerSize: number = arbitraryNegativeNumber
 
     const timeoutError = new Error('timeout')
     await new Promise<void>((resolve, reject) => {
       const interval = setInterval(() => {
-        if(this.peers.size>=minimumPeers){
-            peerSize = this.peers.size
-            if(this.server ===undefined){
-              throw new Error ('server undefined so we cannot send weights through it')
-            }
-            this.peerMessageTemp(encodedMsg)
+        if (this.peers.size >= minimumPeers) {
+          peerSize = this.peers.size
+          if (this.server === undefined) {
+            throw new Error('server undefined so we cannot send weights through it')
+          }
+          this.peerMessageTemp(encodedMsg)
         }
-        const gotAllWeights = (this.receivedWeights.size===peerSize)
+        const gotAllWeights = (this.receivedWeights.size === peerSize)
         if (gotAllWeights) {
           clearInterval(interval)
           resolve()
--- discojs/src/messages.ts
+++ discojs/src/messages.ts
@@ -1,5 +1,5 @@
-import {List, Set} from 'immutable'
-import {weights} from './serialization'
+import { List } from 'immutable'
+import { weights } from './serialization'
 
 export type PeerID = number
 export type EncodedSignal = Uint8Array
@@ -12,9 +12,8 @@ export enum messageType {
   serverConnectedClients
 }
 
-export interface serverClientIDMessage {type: messageType, peerID: PeerID} //server sends client id to client
-export interface clientReadyMessage {type: messageType, round:number} //client who sent is ready
-export interface clientWeightsMessageServer {type: messageType, peerID: PeerID, weights: weights.Encoded} //client weights
-export interface clientPartialSumsMessageServer {type: messageType, peerID: PeerID, partials: EncodedSignal} //client partial sum
-export interface serverConnectedClients {type: messageType, peerList: List<PeerID>} //server send to client who to connect to
-
+export interface serverClientIDMessage {type: messageType, peerID: PeerID} // server sends client id to client
+export interface clientReadyMessage {type: messageType, round: number} // client who sent is ready
+export interface clientWeightsMessageServer {type: messageType, peerID: PeerID, weights: weights.Encoded} // client weights
+export interface clientPartialSumsMessageServer {type: messageType, peerID: PeerID, partials: EncodedSignal} // client partial sum
+export interface serverConnectedClients {type: messageType, peerList: List<PeerID>} // server send to client who to connect to
