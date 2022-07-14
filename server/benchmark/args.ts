d250aecc133953b6f76fb386b9739a7729ff5282
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2022-07-14 12:43:27 +0200

    bench: refactor

--- server/benchmark/args.ts
+++ server/benchmark/args.ts
@@ -8,21 +8,20 @@ interface BenchmarkArguments {
   help?: boolean
 }
 
-const USERS = 1
-const EPOCHS = 2
-const ROUND_DURATION = 10
+const argExample = 'e.g. npm run benchmark -- -u 2 -e 3, runs 2 users for 3 epochs'
 
-export const args = parse<BenchmarkArguments>(
+// TODO: Default args for non boolean did not seem to work
+const unsafeArgs = parse<BenchmarkArguments>(
   {
     // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
     // @ts-ignore
-    numberOfUsers: { type: Number, alias: 'u', description: `Number of users, default: ${USERS}`, default: USERS, optional: true },
+    numberOfUsers: { type: Number, alias: 'u', description: 'Number of users', optional: true },
     // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
     // @ts-ignore
-    epochs: { type: Number, alias: 'e', description: `Number of epochs, default: ${EPOCHS}`, default: EPOCHS, optional: true },
+    epochs: { type: Number, alias: 'e', description: 'Number of epochs', optional: true },
     // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
     // @ts-ignore
-    roundDuration: { type: Number, alias: 'r', description: `Round duration, default: ${ROUND_DURATION}`, default: ROUND_DURATION, optional: true },
+    roundDuration: { type: Number, alias: 'r', description: 'Round duration', optional: true },
     // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
     // @ts-ignore
     save: { type: Boolean, alias: 's', description: 'Save logs of benchmark', default: false },
@@ -30,6 +29,18 @@ export const args = parse<BenchmarkArguments>(
   },
   {
     helpArg: 'help',
-    headerContentSections: [{ header: 'Disco benchmark', content: 'npx ts-node benchmark/benchmark.ts [Options]' }]
+    headerContentSections: [{ header: 'Disco benchmark', content: 'npm run benchmark -- [Options]\n' + argExample }]
   }
 )
+
+const numberOfUsers = unsafeArgs.numberOfUsers === undefined ? 1 : unsafeArgs.numberOfUsers
+const roundDuration = unsafeArgs.roundDuration === undefined ? 10 : unsafeArgs.roundDuration
+const epochs = unsafeArgs.epochs === undefined ? 10 : unsafeArgs.epochs
+
+export const args: BenchmarkArguments = {
+  numberOfUsers: numberOfUsers,
+  epochs: epochs,
+  roundDuration: roundDuration,
+  save: unsafeArgs.save,
+  help: false
+}
