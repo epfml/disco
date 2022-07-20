441aaf0faf33eb1fcc7b21e8cd2015ee6a9dd979
s314cy <hugo.elguedj@epfl.ch>
2022-07-20 18:07:36 +0200

    discojs+browser: add data harmonization (fix #377)

---
+++ browser/src/components/data/DataHarmonization.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+
+import { Task } from 'discojs'
+import IconCard from '@/components/containers/IconCard.vue'
+
+const task = inject<Task>('task')
+
+const images = require.context('../../../../discojs/example_training_data/', false, /.+?\.(jpg|png)$/)
+const exampleImage = computed(() => images(task.displayInformation.dataExampleImage))
+
+</script>
+<template>
+  <div>
+    <IconCard>
+      <template #title>
+        Data Format
+      </template>
+      <template #content>
+        It is <span class="font-bold">important</span> to harmonize your data to the expected format as described below.<br><br>
+        <span v-html="task.displayInformation.dataFormatInformation" />
+      </template>
+    </IconCard>
+    <IconCard>
+      <template #title>
+        Example Data
+      </template>
+      <template #content>
+        <span v-html="task.displayInformation.dataExampleText" /><br><br>
+        <!-- Tabular data example -->
+        <div
+          v-if="task.trainingInformation.dataType === 'tabular'"
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        >
+          <span
+            v-for="(column, idx) in task.displayInformation.dataExample"
+            :key="idx"
+          >
+            <span class="font-bold">{{ column.columnName }}:</span> {{ column.columnData }}
+          </span>
+        </div>
+        <!-- Image data example -->
+        <div v-if="task.trainingInformation.dataType === 'image'">
+          <img
+            class="mx-auto"
+            :src="exampleImage"
+            alt="Error! Image not found"
+          >
+        </div>
+      </template>
+    </IconCard>
+  </div>
+</template>
