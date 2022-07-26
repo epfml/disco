1d6c6c6bc2e0316857ec74626b1d9c8938ffeb84
Felix Hans Michel Grimberg <felix.grimberg@hotmail.com>
2022-07-26 18:19:35 +0200

    Use new dec client names in browser/src/clients.ts
    e.g., replace SecureDecentralized by DecentralizedSecAgg

--- browser/src/clients.ts
+++ browser/src/clients.ts
@@ -7,9 +7,9 @@ export function getClient (trainingScheme: TrainingSchemes, task: Task): Client 
   switch (trainingScheme) {
     case TrainingSchemes.DECENTRALIZED:
       if (task.trainingInformation.decentralizedSecure) {
-        return new client.SecureDecentralized(CONFIG.serverUrl, task)
+        return new client.DecentralizedSecAgg(CONFIG.serverUrl, task)
       } else {
-        return new client.InsecureDecentralized(CONFIG.serverUrl, task)
+        return new client.DecentralizedClearText(CONFIG.serverUrl, task)
       }
     case TrainingSchemes.FEDERATED:
       return new client.Federated(CONFIG.serverUrl, task)
