9b87b27a04130196e3e76df4a87e4974fcf2d71c
tharvik <tharvik@users.noreply.github.com>
2022-07-28 15:48:54 +0200

    discojs/client/dec: check msg validity

--- discojs/src/client/decentralized/messages.ts
+++ discojs/src/client/decentralized/messages.ts
@@ -1,6 +1,7 @@
-import { TaskID } from '@/task'
+import { TaskID, isTaskID } from '../../task'
 import { weights } from '../../serialization'
-import { PeerID } from './types'
+
+import { isPeerID, PeerID } from './types'
 
 export enum type {
   clientConnected,
@@ -81,3 +82,62 @@ export type PeerMessage =
   clientPartialSumsMessageServer
 
 export type Message = ServerMessage | PeerMessage
+
+export function isServerMessage (raw: unknown): raw is ServerMessage {
+  if (typeof raw !== 'object' || raw === null) {
+    return false
+  }
+
+  const o = raw as Record<string, unknown>
+  if (
+    !('type' in o && typeof o.type === 'number' && o.type in type)
+  ) {
+    return false
+  }
+
+  switch (o.type) {
+    case type.clientConnected:
+      return true
+    case type.serverClientIDMessage:
+      return 'peerID' in o && isPeerID(o.peerID)
+    case type.serverReadyClients:
+      return 'peerList' in o && Array.isArray(o.peerList) && o.peerList.every(isPeerID)
+  }
+
+  return false
+}
+
+export function isPeerMessage (raw: unknown): raw is PeerMessage {
+  if (typeof raw !== 'object' || raw === null) {
+    return false
+  }
+
+  const o = raw as Record<string, unknown>
+  if (
+    !('type' in o && typeof o.type === 'number' && o.type in type)
+  ) {
+    return false
+  }
+
+  switch (o.type) {
+    case type.clientReadyMessage:
+      return 'round' in o && typeof o.round === 'number' &&
+        'task' in o && isTaskID(o.task) &&
+        'peerID' in o && isPeerID(o.peerID)
+    case type.clientWeightsMessageServer:
+    case type.clientSharesMessageServer:
+      return 'peerID' in o && isPeerID(o.peerID) &&
+        'weights' in o && weights.isEncoded(o.weights) &&
+        'destination' in o && isPeerID(o.destination)
+    case type.clientPartialSumsMessageServer:
+      return 'peerID' in o && isPeerID(o.peerID) &&
+        'partials' in o && weights.isEncoded(o.partials) &&
+        'destination' in o && isPeerID(o.destination)
+  }
+
+  return false
+}
+
+export function isMessage (raw: unknown): raw is Message {
+  return isServerMessage(raw) || isPeerMessage(raw)
+}
