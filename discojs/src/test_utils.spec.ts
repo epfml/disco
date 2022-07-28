23f848f9dded6e699c7e4a95b5778799f4236f29
Felix Hans Michel Grimberg <felix.grimberg@hotmail.com>
2022-07-28 22:51:43 +0200

    Comment on possible but pointless efficiency gain.

--- discojs/src/test_utils.spec.ts
+++ discojs/src/test_utils.spec.ts
@@ -6,6 +6,8 @@ import * as secret_shares from './client/decentralized/secret_shares'
 
 export function assertWeightsEqual (w1: Weights, w2: Weights, epsilon: number = 0): void {
   const differenceBetweenWeights: Weights = secret_shares.subtractWeights(w1, w2)
+  // Inefficient because we wait for each layer to completely load before we start loading the next layer (using tf.Tensor.dataSync() in a for loop). Could be made more efficient by using Promise.all().
+  // Not worth making more efficient, because this function is only used for testing, where tf.Tensors are small.
   for (const t of differenceBetweenWeights) {
     assert.strictEqual(
       tf.lessEqual(t.abs(), epsilon).all().dataSync()[0],
