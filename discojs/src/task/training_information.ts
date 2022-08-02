1228b184f22ad285f08ef37b97fbf0280eb01c8b
Felix Hans Michel Grimberg <53295216+Grim-bot@users.noreply.github.com>
2022-08-02 17:01:28 +0200

    Slight improvement of trainingInformant comments
    Adds a comment for noiseScale (root of the variance of the Gaussian noise added in DP)

--- discojs/src/task/training_information.ts
+++ discojs/src/task/training_information.ts
@@ -10,16 +10,16 @@ export interface TrainingInformation {
   preprocessFunctions: string[]
   modelCompileData: ModelCompileData
   dataType: string
-  // maximum absolute value of a number in a randomly generated share for secure aggregation
-  // default is 100, must be a positive number, check the ~/disco/information/PRIVACY.md file for more information on significance of maxShareValue selection
-  // only relevant if secure aggregation is true (for either federated or decentralized learning)
-  maxShareValue?: number
-  // minimum number of peers who must be ready to participate in aggregation before model updates are shared between clients
+  // decentralizedSecure: true if the training scheme is decentralized and requires secure aggregation, false otherwise
+  decentralizedSecure?: boolean
+  // minimumReadyPeers: minimum number of peers who must be ready to participate in aggregation before model updates are shared between clients
   // default is 3, range is [3, totalNumberOfPeersParticipating]
   // only relevant in decentralized setting
   minimumReadyPeers?: number
-  // true if the training scheme is decentralized and requires secure aggregation, false otherwise
-  decentralizedSecure?: boolean
+  // maxShareValue: maximum absolute value of a number in a randomly generated share for secure aggregation
+  // default is 100, must be a positive number, check the ~/disco/information/PRIVACY.md file for more information on significance of maxShareValue selection
+  // only relevant if secure aggregation is true (for either federated or decentralized learning)
+  maxShareValue?: number
   receivedMessagesThreshold?: number
   inputColumns?: string[]
   outputColumns?: string[]
@@ -34,6 +34,7 @@ export interface TrainingInformation {
   RESIZED_IMAGE_H?: number
   RESIZED_IMAGE_W?: number
   LABEL_ASSIGNMENT?: DataExample[]
+  // noiseScale: Affects the variance of the Gaussian noise added to the models / model updates for differential privacy.
   scheme?: string
   noiseScale?: number
   clippingRadius?: number
