6f6bf99aa237e51e12a7b69b521b227d2a3b0e01
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2022-08-03 20:24:45 +0200

    simple_face: added mobilenetv2

--- discojs/src/tasks/simple_face.ts
+++ discojs/src/tasks/simple_face.ts
@@ -21,7 +21,7 @@ export const task: Task = {
     epochs: 50,
     roundDuration: 1,
     validationSplit: 0.2,
-    batchSize: 10,
+    batchSize: 5,
     preprocessFunctions: [],
     learningRate: 0.001,
     modelCompileData: {
@@ -38,48 +38,72 @@ export const task: Task = {
   }
 }
 
-export function model (imageHeight = 200, imageWidth = 200, imageChannels = 3, numOutputClasses = 2): tf.LayersModel {
+function addMobilenetBlock (model: tf.Sequential, filters: number, strides: number): void {
+  model.add(tf.layers.depthwiseConv2d({
+    kernelSize: 3,
+    strides: strides,
+    padding: 'same'
+  }))
+
+  model.add(tf.layers.batchNormalization())
+  model.add(tf.layers.reLU())
+
+  model.add(tf.layers.conv2d({
+    filters: filters,
+    kernelSize: 1,
+    strides: 1
+  }))
+
+  model.add(tf.layers.batchNormalization())
+  model.add(tf.layers.reLU())
+}
+
+function mobilenetv2 (height = 224, width = 224, channels = 3, numberOfClasses = 10): tf.Sequential {
   const model = tf.sequential()
 
-  // In the first layer of our convolutional neural network we have
-  // to specify the input shape. Then we specify some parameters for
-  // the convolution operation that takes place in this layer.
+  // stem of the model
   model.add(tf.layers.conv2d({
-    inputShape: [imageHeight, imageWidth, imageChannels],
-    kernelSize: 5,
-    filters: 8,
-    strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'varianceScaling'
+    inputShape: [height, width, channels],
+    filters: 32,
+    kernelSize: 3,
+    strides: 2,
+    padding: 'same'
   }))
 
-  // The MaxPooling layer acts as a sort of downsampling using max values
-  // in a region instead of averaging.
-  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+  model.add(tf.layers.batchNormalization())
+  model.add(tf.layers.reLU())
 
-  // Repeat another conv2d + maxPooling stack.
-  // Note that we have more filters in the convolution.
-  model.add(tf.layers.conv2d({
-    kernelSize: 5,
-    filters: 16,
+  // main part of the model
+  addMobilenetBlock(model, 64, 1)
+  addMobilenetBlock(model, 128, 2)
+  addMobilenetBlock(model, 128, 1)
+  addMobilenetBlock(model, 256, 2)
+  addMobilenetBlock(model, 256, 1)
+  addMobilenetBlock(model, 512, 2)
+
+  addMobilenetBlock(model, 512, 1)
+  addMobilenetBlock(model, 512, 1)
+  addMobilenetBlock(model, 512, 1)
+  addMobilenetBlock(model, 512, 1)
+  addMobilenetBlock(model, 512, 1)
+
+  addMobilenetBlock(model, 1024, 2)
+  addMobilenetBlock(model, 1024, 1)
+
+  model.add(tf.layers.averagePooling2d({
+    poolSize: 7,
     strides: 1,
-    activation: 'relu',
-    kernelInitializer: 'varianceScaling'
+    dataFormat: 'channelsFirst'
   }))
-  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
 
-  // Now we flatten the output from the 2D filters into a 1D vector to prepare
-  // it for input into our last layer. This is common practice when feeding
-  // higher dimensional data to a final classification output layer.
-  model.add(tf.layers.flatten())
-
-  // Our last layer is a dense layer which has 10 output units, one for each
-  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
   model.add(tf.layers.dense({
-    units: numOutputClasses,
-    kernelInitializer: 'varianceScaling',
+    units: numberOfClasses,
     activation: 'softmax'
   }))
 
   return model
+}
+
+export function model (imageHeight = 200, imageWidth = 200, imageChannels = 3, numOutputClasses = 2): tf.LayersModel {
+  return mobilenetv2(imageHeight, imageWidth, imageChannels, numOutputClasses)
 }
