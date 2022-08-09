02ebd5dac12603f29c7091ab58b635dd15445a64
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2022-08-09 14:07:52 +0200

    mobilenetv2 working, but only on nodejs

--- discojs/src/tasks/simple_face.ts
+++ discojs/src/tasks/simple_face.ts
@@ -1,4 +1,4 @@
-import * as tf from '@tensorflow/tfjs'
+import * as tf from '@tensorflow/tfjs-node'
 
 import { Task } from '../task'
 
@@ -38,72 +38,7 @@ export const task: Task = {
   }
 }
 
-function addMobilenetBlock (model: tf.Sequential, filters: number, strides: number): void {
-  model.add(tf.layers.depthwiseConv2d({
-    kernelSize: 3,
-    strides: strides,
-    padding: 'same'
-  }))
-
-  model.add(tf.layers.batchNormalization())
-  model.add(tf.layers.reLU())
-
-  model.add(tf.layers.conv2d({
-    filters: filters,
-    kernelSize: 1,
-    strides: 1
-  }))
-
-  model.add(tf.layers.batchNormalization())
-  model.add(tf.layers.reLU())
-}
-
-function mobilenetv2 (height = 224, width = 224, channels = 3, numberOfClasses = 10): tf.Sequential {
-  const model = tf.sequential()
-
-  // stem of the model
-  model.add(tf.layers.conv2d({
-    inputShape: [height, width, channels],
-    filters: 32,
-    kernelSize: 3,
-    strides: 2,
-    padding: 'same'
-  }))
-
-  model.add(tf.layers.batchNormalization())
-  model.add(tf.layers.reLU())
-
-  // main part of the model
-  addMobilenetBlock(model, 64, 1)
-  addMobilenetBlock(model, 128, 2)
-  addMobilenetBlock(model, 128, 1)
-  addMobilenetBlock(model, 256, 2)
-  addMobilenetBlock(model, 256, 1)
-  addMobilenetBlock(model, 512, 2)
-
-  addMobilenetBlock(model, 512, 1)
-  addMobilenetBlock(model, 512, 1)
-  addMobilenetBlock(model, 512, 1)
-  addMobilenetBlock(model, 512, 1)
-  addMobilenetBlock(model, 512, 1)
-
-  addMobilenetBlock(model, 1024, 2)
-  addMobilenetBlock(model, 1024, 1)
-
-  model.add(tf.layers.averagePooling2d({
-    poolSize: 7,
-    strides: 1,
-    dataFormat: 'channelsFirst'
-  }))
-
-  model.add(tf.layers.dense({
-    units: numberOfClasses,
-    activation: 'softmax'
-  }))
-
-  return model
-}
-
-export function model (imageHeight = 200, imageWidth = 200, imageChannels = 3, numOutputClasses = 2): tf.LayersModel {
-  return mobilenetv2(imageHeight, imageWidth, imageChannels, numOutputClasses)
+export async function model (): Promise<tf.LayersModel> {
+  const file = tf.io.fileSystem('file:/./../../../mobilenetv2_builder/mobileNetV2_35_alpha_2_classes/model.json')
+  return await tf.loadLayersModel(file)
 }
