e0f815ab98784af21da3eb357e538ac47fa7a6d1
s314cy <hugo.elguedj@epfl.ch>
2022-09-21 16:16:06 +0200

    discojs/task: handle optional fields from form

--- discojs/src/task/model_compile_data.ts
+++ discojs/src/task/model_compile_data.ts
@@ -1,7 +1,36 @@
-export class ModelCompileData {
-  constructor (
-    public readonly optimizer: string,
-    public readonly loss: string,
-    public readonly metrics: string[]
-  ) {}
+export function isModelCompileData (raw: unknown): raw is ModelCompileData {
+  if (typeof raw !== 'object') {
+    return false
+  }
+  if (raw === null) {
+    return false
+  }
+
+  const {
+    optimizer,
+    loss,
+    metrics
+  } = raw as Record<'optimizer' | 'loss' | 'metrics', unknown | undefined>
+
+  if (
+    typeof optimizer !== 'string' ||
+    typeof loss !== 'string'
+  ) {
+    return false
+  }
+
+  if (!(
+    Array.isArray(metrics) &&
+    metrics.every((e) => typeof e === 'string')
+  )) {
+    return false
+  }
+
+  return true
+}
+
+export interface ModelCompileData {
+  optimizer: string
+  loss: string
+  metrics: string[]
 }
