099666277834f696b8fef8deb3f07214b75f4368
Francesco Posa <francesco.posa@adnovum.ch>
2022-09-21 16:36:55 +0200

    Implementing byzantine-tolerant aggragator formula

--- discojs/src/aggregation.ts
+++ discojs/src/aggregation.ts
@@ -33,3 +33,12 @@ export function averageWeights (peersWeights: List<Weights>): Weights {
   const numberOfPeers = peersWeights.size
   return sumWeights(peersWeights).map((w) => w.div(numberOfPeers))
 }
+
+export function averageCenteredClipping (peersWeights: List<Weights>, previousWeights: Weights, tau: number): Weights {
+  const subtractedWeights: List<List<tf.Tensor>> = peersWeights.map(weights => List(weights).zip(List(previousWeights)).map(([w1, w2]) => w1.sub(w2)))
+  const norm: List<number> = subtractedWeights.map(weights => Math.sqrt(weights.map((w) => w.square().sum().dataSync()[0]).reduce((a: number, b) => a + b)))
+
+  const centeredMean: List<List<tf.Tensor>> = subtractedWeights.map(weights => weights.map((w, i) => tf.prod(w, Math.min(1, 1 / (norm.get(i) ?? 1)))))
+
+  return averageWeights(centeredMean.map(weights => Array.from(weights)))
+}
