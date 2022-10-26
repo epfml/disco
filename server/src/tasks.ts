9ba33798a2a569b7c7d9d511bf273b1f48ced4c7
s314cy <hugo.elguedj@epfl.ch>
2022-10-26 17:13:37 +0200

    server: rm model files

--- server/src/tasks.ts
+++ server/src/tasks.ts
@@ -1,6 +1,6 @@
 import { Set, Map, merge } from 'immutable'
 import { EventEmitter } from 'node:events'
-import path from 'path'
+import fs from 'fs'
 
 import { tf, node, tasks, Task } from '@epfml/discojs-node'
 import { CONFIG } from './config'
@@ -34,8 +34,25 @@ export class TasksAndModels extends EventEmitter {
     const ret = new TasksAndModels()
 
     const tasks = await Promise.all(Object.values(defaultTasks))
-    await Promise.all(tasks.map(async (i) =>
-      ret.addTaskAndModel(i.task, await i.model(MODEL_PATH.get(i.task.taskID) ?? ''))))
+    await Promise.all(tasks.map(async (i) => {
+      let model: tf.LayersModel | undefined
+      const modelPath = `./models/${i.task.taskID}/`
+      if (fs.existsSync(modelPath)) {
+        // either a model has already been trained, or the pretrained
+        // model has already been downloaded
+        model = await tf.loadLayersModel(`file://${modelPath}/model.json`)
+      } else {
+        // otherwise, init the task's model and save it
+        try {
+          model = await i.model()
+        } catch (e: any) {
+          console.error(e instanceof Error ? e.message : e.toString())
+          return
+        }
+        await model.save(`file://${modelPath}`)
+      }
+      ret.addTaskAndModel(i.task, model)
+    }))
 
     return ret
   }
