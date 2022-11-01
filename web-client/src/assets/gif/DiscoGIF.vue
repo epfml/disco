2d2587ad815883807c6fbe883defd9ec6c8f2cb9
s314cy <hugo.elguedj@epfl.ch>
2022-11-01 12:20:22 +0100

    web-client: fix landing page gif

--- web-client/src/assets/gif/DiscoGIF.vue
+++ web-client/src/assets/gif/DiscoGIF.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="https://storage.googleapis.com/deai-313515.appspot.com/gifs/decentralized_render.gif">
+  <img src="https://storage.googleapis.com/deai-313515.appspot.com/gifs/disco_middle.gif">
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
