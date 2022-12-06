7064af1f0cc027d44ba90c4ceb5523888e439745
Morgan Ridel <mail@morganridel.fr>
2022-12-06 13:06:37 +0100

    add docstring for training function

--- discojs/discojs-core/src/training/training_functions.ts
+++ discojs/discojs-core/src/training/training_functions.ts
@@ -1,5 +1,14 @@
 import { tf, TrainingInformation } from '..'
 
+/**
+ * The training function is the function that will be called to train the model.
+ * The model is trained locally with the given dataset, which means that it will only have access to the data of the user local machine.
+ * This all happens in the client, so this data is not sent to the server, only the resulting model is.
+ *
+ * Every few batches, represented by the round duration ('onRoundEnd' trainer function), the model will be updated with the new aggregated weights (in the case of the distributed trainer). Depending on the training scheme, weights are aggregated on the server (federated), or between peers (decentralized).
+ * The following trainings will be done with the updated model.
+ * You need to be aware that the model is subject to regular changes in your training functions.
+ */
 export type TrainingFunction =
   (model: tf.LayersModel,
     trainingInformation: TrainingInformation,
