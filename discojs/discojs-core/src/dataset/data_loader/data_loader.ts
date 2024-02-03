cb67111dc1ab72fc84dc009abf45e6bb5aa2c082
peacefulotter <zankro67@gmail.com>
2024-02-03 17:47:09 +0100

    More generic core DataLoader

--- discojs/discojs-core/src/dataset/data_loader/data_loader.ts
+++ discojs/discojs-core/src/dataset/data_loader/data_loader.ts
@@ -1,15 +1,25 @@
-import { Task } from '../..'
+import { Task } from '../../'
 import { Dataset } from '../dataset'
 import { Data, DataSplit } from '../data'
 
-export interface DataConfig { features?: string[], labels?: string[], shuffle?: boolean, validationSplit?: number, inference?: boolean }
+export interface DataConfig {
+    features?: string[]
+    labels?: string[]
+    shuffle?: boolean
+    validationSplit?: number
+    inference?: boolean
+}
 
-export abstract class DataLoader<Source> {
-  constructor (protected task: Task) {}
+export abstract class DataLoader<
+    Source,
+    Sources = Source | Source[],
+    Config extends DataConfig = DataConfig
+> {
+    constructor(protected task: Task) {}
 
-  abstract createData (dataset: Dataset, size?: number): Promise<Data>
+    abstract createData(dataset: Dataset, size?: number): Promise<Data>
 
-  abstract load (source: Source, config: DataConfig): Promise<Dataset>
+    abstract load(source: Source, config: Config): Promise<Dataset>
 
-  abstract loadAll (sources: Source[], config: DataConfig): Promise<DataSplit>
+    abstract loadAll(sources: Sources, config: Config): Promise<DataSplit>
 }
