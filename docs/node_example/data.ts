9643964bb9f6dedcccb3d99320fe39fbc0b6a1ca
Julien Vignoud <julien.vignoud@epfl.ch>
2024-02-22 18:16:05 +0100

    docs/node_example: Integrate server startup to example

--- docs/node_example/data.ts
+++ docs/node_example/data.ts
@@ -1,29 +1,12 @@
 import fs from 'fs'
-import Rand from 'rand-seed'
-
 import { node, data, Task, defaultTasks } from '@epfml/discojs-node'
-
-const rand = new Rand('1234')
-
-function shuffle<T, U> (array: T[], arrayTwo: U[]): void {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(rand.next() * (i + 1))
-    const temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-
-    const tempTwo = arrayTwo[i]
-    arrayTwo[i] = arrayTwo[j]
-    arrayTwo[j] = tempTwo
-  }
-}
 
 function filesFromFolder (dir: string, folder: string): string[] {
   const f = fs.readdirSync(dir + folder)
   return f.map(file => dir + folder + '/' + file)
 }
 
-export async function loadData (task: Task): Promise<data.DataSplit> {
+export async function loadSimpleFaceData (task: Task): Promise<data.DataSplit> {
   const dir = '../../example_training_data/simple_face/'
   const youngFolders = ['child']
   const oldFolders = ['adult']
@@ -41,13 +24,10 @@ export async function loadData (task: Task): Promise<data.DataSplit> {
   const labels = filesPerFolder.flatMap((files, index) => Array(files.length).fill(index))
   const files = filesPerFolder.flat()
 
-  shuffle(files, labels)
-
   return await new node.data.NodeImageLoader(task).loadAll(files, { labels: labels })
 }
 
-export async function loadTitanicData (task:Task): Promise<data.
-DataSplit> {
+export async function loadTitanicData (task: Task): Promise<data.DataSplit> {
   const files = ['../../example_training_data/titanic_train.csv']
   const titanicTask = defaultTasks.titanic.getTask()
   return await new node.data.NodeTabularLoader(task, ',').loadAll(files, {
--- docs/node_example/example.ts
+++ docs/node_example/example.ts
@@ -1,5 +1,6 @@
 import { data, Disco, fetchTasks, Task } from '@epfml/discojs-node'
 import { loadTitanicData } from './data'
+import { startServer } from './server'
 
 /**
  * Example of discojs API, we load data, build the appropriate loggers, the disco object
@@ -15,23 +16,31 @@ async function runUser (url: URL, task: Task, dataset: data.DataSplit): Promise<
 }
 
 async function main (): Promise<void> {
-  
   // First have a server instance running before running this script
-  const serverUrl = new URL('http://localhost:8080/')
-
+  const [server, serverUrl] = await startServer()
+  // Get all pre-defined tasks
   const tasks = await fetchTasks(serverUrl)
-  
-  // Choose your task to train
-  const task = tasks.get('titanic') as Task
 
-  const dataset = await loadTitanicData(task)
+  // Choose the task and load local data
+  const task = tasks.get('titanic') as Task // Choose the Titanic task to train
+  const dataset = await loadTitanicData(task) // Make sure you first ran ./get_training_data
+  // Alternatively:
+  // const task = tasks.get('simple_face') as Task
+  // const dataset = await loadSimpleFace(task) // you will have to import { loadSimpleFace} from './data'
 
-  // Add more users to the list to simulate more clients
+  // Add more users to the list to simulate more than 3 clients
   await Promise.all([
     runUser(serverUrl, task, dataset),
     runUser(serverUrl, task, dataset),
-    runUser(serverUrl, task, dataset),
+    runUser(serverUrl, task, dataset)
   ])
+
+  console.log('Shutting down the server...')
+  await new Promise((resolve, reject) => {
+    server.once('close', resolve)
+    server.close(reject)
+  })
 }
 
+// You can run this example with "npm start" from this folder
 main().catch(console.error)
