18e90b4319edbfc43b46a2fb97850727915149bb
tharvik <tharvik@users.noreply.github.com>
2024-03-19 19:48:03 +0100

    web-client: readd cypress

--- web-client/cypress/e2e/tasks.cy.ts
+++ web-client/cypress/e2e/tasks.cy.ts
@@ -1,39 +1,30 @@
 /* eslint-disable no-undef */
-import { defaultTasks } from '@epfml/discojs-core'
 
-// most basic disco tasks
-export const TASK_LIST = [
-  defaultTasks.titanic.getTask(),
-  defaultTasks.mnist.getTask(),
-  defaultTasks.cifar10.getTask()
-]
+import TASK_LIST from './tasks'
 
 describe('tasks page', () => {
   it('displays tasks', () => {
-    cy.intercept('tasks', TASK_LIST).as('tasks')
-    cy.visit('list')
-    cy.wait('@tasks').then((interception) => {
-      assert.lengthOf(interception.response.body, TASK_LIST.length)
-    })
+    cy.intercept({ hostname: 'server', pathname: 'tasks' }, TASK_LIST).as('tasks')
+
+    cy.visit('/#/list')
     cy.get('div[id="tasks"]').children().should('have.length', TASK_LIST.length)
   })
+
   it('redirects to training', () => {
-    cy.intercept('tasks', TASK_LIST).as('tasks')
-    cy.visit('list')
-    cy.wait('@tasks')
+    cy.intercept({ hostname: 'server', pathname: 'tasks' }, TASK_LIST).as('tasks')
+
+    cy.visit('/#/list')
     TASK_LIST.forEach((task) => {
       cy.get(`div[id="${task.id}"]`).find('button').click()
-      cy.url().should('eq', Cypress.config().baseUrl + task.id)
+      cy.url().should('eq', `${Cypress.config().baseUrl}#/${task.id}`)
       cy.get('button').contains('previous', { matchCase: false }).click()
-      cy.url().should('eq', Cypress.config().baseUrl + 'list')
     })
   })
+
   it('displays error message', () => {
-    cy.intercept('tasks', (req) => {
-      req.reply({ statusCode: 404 })
-    }).as('tasks')
-    cy.visit('list')
-    cy.wait('@tasks')
+    cy.intercept({ hostname: 'server', pathname: 'tasks' }, { statusCode: 404 }).as('tasks')
+
+    cy.visit('/#/list')
     cy.get('button').contains('reload page', { matchCase: false })
   })
 })
