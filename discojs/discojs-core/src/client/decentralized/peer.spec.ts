6768745070a5d71fc250ca7cb1acd931c6123364
tharvik <tharvik@users.noreply.github.com>
2024-04-03 18:29:41 +0200

    discojs-core/decentralized: direct Peer constructor

--- discojs/discojs-core/src/client/decentralized/peer.spec.ts
+++ discojs/discojs-core/src/client/decentralized/peer.spec.ts
@@ -8,8 +8,8 @@ describe('peer', function () {
   let peer2: Peer
 
   beforeEach(async () => {
-    peer1 = await Peer.init('1')
-    peer2 = await Peer.init('2', true)
+    peer1 = new Peer('1')
+    peer2 = new Peer('2', true)
     const peers = Set.of(peer1, peer2)
 
     peer1.on('signal', (signal) => { peer2.signal(signal) })
