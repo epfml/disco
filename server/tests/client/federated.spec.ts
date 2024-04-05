f721977923313d23689cf5d6cae7131a383c21d0
tharvik <tharvik@users.noreply.github.com>
2024-04-05 16:21:36 +0200

    discojs-core/client/federated: rm dead stats

--- server/tests/client/federated.spec.ts
+++ server/tests/client/federated.spec.ts
@@ -1,69 +1,62 @@
-import type * as http from 'http'
+import type * as http from "http";
 
-import { aggregator as aggregators, client as clients, informant, defaultTasks } from '@epfml/discojs-core'
+import {
+  aggregator as aggregators,
+  client as clients,
+  defaultTasks,
+} from "@epfml/discojs-core";
 
-import { startServer } from '../../src/index.js'
+import { startServer } from "../../src/index.js";
 
-const TASK = defaultTasks.titanic.getTask()
+const TASK = defaultTasks.titanic.getTask();
 
-describe('federated client', function () {
-  this.timeout(60_000)
+describe("federated client", function () {
+  this.timeout(60_000);
 
-  let server: http.Server
-  let url: URL
-  beforeEach(async () => { [server, url] = await startServer() })
-  afterEach(() => { server?.close() })
+  let server: http.Server;
+  let url: URL;
+  beforeEach(async () => {
+    [server, url] = await startServer();
+  });
+  afterEach(() => {
+    server?.close();
+  });
 
-  it('connect to & disconnect from valid task', async () => {
-    const client = new clients.federated.FederatedClient(url, TASK, new aggregators.MeanAggregator())
-    await client.connect()
-    await client.disconnect()
-  })
+  it("connect to & disconnect from valid task", async () => {
+    const client = new clients.federated.FederatedClient(
+      url,
+      TASK,
+      new aggregators.MeanAggregator(),
+    );
+    await client.connect();
+    await client.disconnect();
+  });
 
-  it('connect to non valid task', async () => {
+  it("connect to non valid task", async () => {
     const client = new clients.federated.FederatedClient(
       url,
       {
-        id: 'nonValidTask',
+        id: "nonValidTask",
         displayInformation: {},
         trainingInformation: {
-          modelID: 'irrelevant',
+          modelID: "irrelevant",
           epochs: 1,
           roundDuration: 1,
           validationSplit: 0,
           batchSize: 1,
-          scheme: 'federated',
-          dataType: 'tabular'
-        }
+          scheme: "federated",
+          dataType: "tabular",
+        },
       },
-      new aggregators.MeanAggregator()
-    )
+      new aggregators.MeanAggregator(),
+    );
 
     try {
-      await client.connect()
+      await client.connect();
     } catch {
-      return
+      return;
     }
 
-    throw new Error("connect didn't fail")
-  })
-
-  it('checks that getAsyncWeightInformantStatistics returns a JSON with the expected statistics', async () => {
-    const client = new clients.federated.FederatedClient(url, TASK, new aggregators.MeanAggregator())
-    await client.connect()
-
-    const ti = new informant.FederatedInformant(TASK, 0)
-    await client.receiveStatistics(ti)
-
-    await client.disconnect()
-
-    // TODO commented code
-    //   expect(ti.round()).to.be.greaterThanOrEqual(0)
-    //   expect(ti.participants()).to.be.greaterThanOrEqual(1)
-    //   expect(ti.totalParticipants()).to.be.greaterThanOrEqual(1)
-    //   expect(ti.averageParticipants()).to.be.greaterThanOrEqual(1)
-
-    //   await client.disconnect()
-    // })
-  })
-})
+    throw new Error("connect didn't fail");
+  });
+});
