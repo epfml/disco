ce47746412ae5dbc8e22ebbf16b8553db59252d8
tharvik <tharvik@users.noreply.github.com>
2024-04-09 16:45:05 +0200

    discojs-core/informant/training: rm

--- discojs/discojs-core/src/informant/index.ts
+++ discojs/discojs-core/src/informant/index.ts
@@ -1,3 +1,1 @@
 export { GraphInformant } from './graph_informant.js'
-export { Base as TrainingInformant } from './training_informant/index.js'
-export * as informant from './training_informant/index.js'
--- discojs/discojs-core/src/informant/training_informant/index.ts
+++
@@ -1,5 +0,0 @@
-export { Base } from './base.js'
-
-export { FederatedInformant } from './federated.js'
-export { DecentralizedInformant } from './decentralized.js'
-export { LocalInformant } from './local.js'
