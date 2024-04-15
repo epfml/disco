86882131ab4deca47f2f2ddcbbfe1d0881147e9f
tharvik <tharvik@users.noreply.github.com>
2024-04-15 15:48:00 +0200

    *: mv default tasks in server

--- web-client/src/components/training/Training.vue
+++ web-client/src/components/training/Training.vue
@@ -1,9 +1,6 @@
 <template>
   <div v-if="task !== undefined && datasetBuilder !== undefined">
-    <Description
-      v-show="trainingStore.step === 1"
-      :task="task"
-    />
+    <Description v-show="trainingStore.step === 1" :task="task" />
     <Data
       v-show="trainingStore.step === 2"
       :task="task"
@@ -15,67 +12,67 @@
       :task="task"
       :dataset-builder="datasetBuilder"
     />
-    <Finished
-      v-show="trainingStore.step === 4"
-      :task="task"
-    />
+    <Finished v-show="trainingStore.step === 4" :task="task" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
+import { computed, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-import type { TaskID } from '@epfml/discojs-core'
-import { data } from '@epfml/discojs-core'
-import { WebImageLoader, WebTabularLoader, WebTextLoader } from '@epfml/discojs'
+import type { TaskID } from "@epfml/discojs-core";
+import { data } from "@epfml/discojs-core";
+import {
+  WebImageLoader,
+  WebTabularLoader,
+  WebTextLoader,
+} from "@epfml/discojs";
 
-import { useTrainingStore } from '@/store/training'
-import { useTasksStore } from '@/store/tasks'
-import Description from '@/components/training/Description.vue'
-import Trainer from '@/components/training/Trainer.vue'
-import Finished from '@/components/training/Finished.vue'
-import Data from '@/components/data/Data.vue'
+import { useTrainingStore } from "@/store/training";
+import { useTasksStore } from "@/store/tasks";
+import Data from "@/components/data/Data.vue";
 
-const router = useRouter()
-const trainingStore = useTrainingStore()
-const tasksStore = useTasksStore()
+import Description from "./Description.vue";
+import Trainer from "./Trainer.vue";
+import Finished from "./Finished.vue";
 
-// task ID given by the route
-interface Props { id: TaskID }
-const props = defineProps<Props>()
+const props = defineProps<{ id: TaskID }>();
+
+const router = useRouter();
+const trainingStore = useTrainingStore();
+const tasksStore = useTasksStore();
 
 const task = computed(() => {
-  const task = tasksStore.tasks.get(props.id)
+  const task = tasksStore.tasks.get(props.id);
   if (task === undefined) {
-    router.replace({ name: 'not-found' })
-    return
+    router.replace({ name: "not-found" });
+    return;
   }
-  return task
-})
+  return task;
+});
 
 const datasetBuilder = computed(() => {
-  if (task.value === undefined) return
+  if (task.value === undefined) return;
 
-  let dataLoader: data.DataLoader<File>
+  let dataLoader: data.DataLoader<File>;
   switch (task.value.trainingInformation.dataType) {
-    case 'tabular':
-      dataLoader = new WebTabularLoader(task.value)
-      break
-    case 'image':
-      dataLoader = new WebImageLoader(task.value)
-      break
-    case 'text':
-      dataLoader = new WebTextLoader(task.value)
-      break
+    case "tabular":
+      dataLoader = new WebTabularLoader(task.value);
+      break;
+    case "image":
+      dataLoader = new WebImageLoader(task.value);
+      break;
+    case "text":
+      dataLoader = new WebTextLoader(task.value);
+      break;
     default:
-      throw new Error('not implemented')
+      throw new Error("not implemented");
   }
-  return new data.DatasetBuilder(dataLoader, task.value)
-})
+  return new data.DatasetBuilder(dataLoader, task.value);
+});
 
 onMounted(() => {
-  trainingStore.setTask(props.id)
-  trainingStore.setStep(1)
-})
+  trainingStore.setTask(props.id);
+  trainingStore.setStep(1);
+});
 </script>
--- web-client/src/components/training/__tests__/Trainer.spec.ts
+++ web-client/src/components/training/__tests__/Trainer.spec.ts
@@ -2,20 +2,39 @@ import { expect, it, vi } from "vitest";
 import { mount } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
 import * as fs from "node:fs/promises";
+import * as tf from "@tensorflow/tfjs";
 
-import { data, defaultTasks, serialization } from "@epfml/discojs-core";
+import type { TaskProvider } from "@epfml/discojs-core";
+import { data, models, serialization } from "@epfml/discojs-core";
 import { WebTabularLoader } from "@epfml/discojs";
 
 import Trainer from "../Trainer.vue";
 import TrainingInformation from "../TrainingInformation.vue";
 
+const TASK: TaskProvider = {
+  getTask: () => {
+    return {
+      id: "simple",
+      displayInformation: {},
+      trainingInformation: {},
+    };
+  },
+  getModel: () => {
+    const model = tf.sequential({
+      layers: [tf.layers.dense({ units: 1, inputShape: [1] })],
+    });
+
+    model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+
+    return Promise.resolve(new models.TFJS(model));
+  },
+};
+
 vi.mock("axios", async () => {
   async function get(url: string) {
     if (url === "http://localhost:8080/tasks/titanic/model.json") {
       return {
-        data: await serialization.model.encode(
-          await defaultTasks.titanic.getModel(),
-        ),
+        data: await TASK.getModel().then(serialization.model.encode),
       };
     }
     throw new Error("unhandled get");
@@ -32,7 +51,8 @@ vi.mock("axios", async () => {
 });
 
 async function setupForTask() {
-  const task = defaultTasks.titanic.getTask();
+  const task = TASK.getTask()
+
   const builder = new data.DatasetBuilder(new WebTabularLoader(task), task);
   builder.addFiles([
     new File(
