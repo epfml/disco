6038ff4b7da07b5783123d4756ffb67fcb25a1d9
tharvik <tharvik@users.noreply.github.com>
2024-04-25 10:05:34 +0200

    web-client/vite: support node's Buffer

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 8081 },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      include: ["buffer"],
+      globals: { Buffer: true },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
       // TODO until simple-peer#883 is fixed
-      'simple-peer': 'simple-peer/simplepeer.min.js',
-    }
+      "simple-peer": "simple-peer/simplepeer.min.js",
+    },
   },
-  cacheDir: '../node_modules/.vite'
-})
+  cacheDir: "../node_modules/.vite",
+});
