c66925d7ea5f8b67410e9368120a685fd41b7fe9
Julien Vignoud <julien.vignoud@epfl.ch>
2024-05-08 14:07:59 +0200

    Make task information URLs type URL

--- discojs/discojs-core/src/task/display_information.ts
+++ discojs/discojs-core/src/task/display_information.ts
@@ -15,7 +15,7 @@ export interface DisplayInformation {
   // TODO no need for undefined
   headers?: string[]
   // Displays the image at this URL in the UI as an example when connecting data
-  dataExampleImage?: string
+  dataExampleImage?: URL
   // URL to download a dataset for the task, is displayed in the UI when asking to connect data
   sampleDatasetLink?: URL
   limitations?: string
@@ -45,11 +45,11 @@ export function isDisplayInformation (raw: unknown): raw is DisplayInformation {
   if (
     typeof taskTitle !== 'string' ||
     (dataExampleText !== undefined && typeof dataExampleText !== 'string') ||
-    (sampleDatasetLink !== undefined && typeof sampleDatasetLink !== 'string') ||
+    (sampleDatasetLink !== undefined && !(sampleDatasetLink instanceof URL)) ||
     (dataFormatInformation !== undefined && typeof dataFormatInformation !== 'string') ||
     (tradeoffs !== undefined && typeof tradeoffs !== 'string') ||
     (model !== undefined && typeof model !== 'string') ||
-    (dataExampleImage !== undefined && typeof dataExampleImage !== 'string') ||
+    (dataExampleImage !== undefined && !(dataExampleImage instanceof URL)) ||
     (labelDisplay !== undefined && !isLabelType(labelDisplay)) ||
     (limitations !== undefined && typeof limitations !== 'string')
   ) {
