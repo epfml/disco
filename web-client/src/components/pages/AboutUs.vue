1ae94e3e5ccc3f76ee38fd8348b4182dc6cb987b
Julien Vignoud <julien.vignoud@epfl.ch>
2024-05-08 15:42:52 +0200

    Update Aobut Us design

--- web-client/src/components/pages/AboutUs.vue
+++ web-client/src/components/pages/AboutUs.vue
@@ -1,25 +1,13 @@
 <template>
-  <div class="grid grid-cols-1 gap-4 p-4 lg:grid-cols-1 xl:grid-cols-1">
-    <Card custom-class="hover:text-primary dark:hover:text-light">
-      <div class="ml-10">
-        <h6
-          class="
-                text-xl
-                font-large
-                leading-none
-                tracking-wider
-                dark:group-hover:text-light
-                p-2
-                py-6
-              "
-        >
-          About Us
-        </h6>
+  <div class="flex justify-center">
+    <IconCard class="justify-self-center w-full lg:w-4/5">
+      <template #title>
+        About Us
+      </template>
+      <template #content>
         <img src="../../assets/images/mlologo.png">
-        <ul class="text-lg ont-semibold text-gray-500 dark:text-light p-4">
-          This app was developed in the Machine Learning and Optimization
-          Laboratory at EPFL.
-          <br>
+        This app was developed in the Machine Learning and Optimization Laboratory at EPFL.
+        <ul>
           Visit the our
           <a
             href="https://www.epfl.ch/labs/mlo/"
@@ -52,11 +40,11 @@
           >Slack</a>
           page to discuss the app.
         </ul>
-      </div>
-    </Card>
+      </template>
+    </IconCard>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import Card from '@/components/containers/Card.vue'
+import IconCard from '@/components/containers/IconCard.vue'
 </script>
