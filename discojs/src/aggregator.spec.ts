76d50f6e7117054f9d4097dd2b49824bd09df6c0
tharvik <tharvik@users.noreply.github.com>
2024-05-24 16:42:02 +0200

    discojs/aggregator: rework tests

---
+++ discojs/src/aggregator.spec.ts
@@ -0,0 +1,132 @@
+import { expect } from "chai";
+import { Map, Range, Set } from "immutable";
+
+import { Model, WeightsContainer } from "./index.js";
+import {
+  Aggregator,
+  MeanAggregator,
+  SecureAggregator,
+} from "./aggregator/index.js";
+import { NodeID } from "./client/types.js";
+
+const AGGREGATORS: Set<[name: string, new () => Aggregator]> = Set.of<
+  new (model?: Model) => Aggregator
+>(MeanAggregator, SecureAggregator).map((Aggregator) => [
+  Aggregator.name,
+  Aggregator,
+]);
+
+AGGREGATORS.forEach(([name, Aggregator]) =>
+  describe(`${name} implements Aggregator contract`, () => {
+    it("starts at round zero", () => {
+      const aggregator = new Aggregator();
+
+      expect(aggregator.round).to.equal(0);
+    });
+
+    it("moves forward with enough contributions", async () => {
+      const aggregator = new Aggregator();
+      aggregator.setNodes(Set.of("client 0", "client 1", "client 2"));
+
+      const results = aggregator.receiveResult();
+
+      for (let i = 0; i < 3; i++)
+        for (let r = 0; r < aggregator.communicationRounds; r++)
+          aggregator.add(`client ${i}`, WeightsContainer.of([i]), 0, r);
+
+      await results; // nothing to test
+
+      expect(aggregator.round).to.equal(1);
+    });
+
+    it("gives same results on each node", async () => {
+      const network = setupNetwork(Aggregator);
+
+      const results = Set(
+        await Promise.all(
+          (
+            await communicate(
+              Map(
+                network
+                  .entrySeq()
+                  .zip(Range(1312))
+                  .map(([[id, agg], ws]) => [
+                    id,
+                    [agg, WeightsContainer.of([ws])],
+                  ]),
+              ),
+            )
+          )
+            .valueSeq()
+            .map(wsIntoArrays)
+            .toArray(),
+        ),
+      );
+
+      results.reduce((first, cur) => {
+        expect(cur).to.be.deep.equal(first);
+        return first;
+      });
+    });
+  }),
+);
+
+export async function wsIntoArrays(ws: WeightsContainer): Promise<number[][]> {
+  return (await Promise.all(ws.weights.map(async (w) => await w.data()))).map(
+    (arr) => [...arr],
+  );
+}
+
+export function setupNetwork<A extends Aggregator>(
+  Aggregator: new () => A,
+): Map<NodeID, A> {
+  const ret = Map(
+    Range(0, 3).map((i) => [`client ${i}`, new Aggregator()] as [NodeID, A]),
+  );
+  ret.forEach((secure) => secure.setNodes(ret.keySeq().toSet()));
+
+  return ret;
+}
+
+// run all rounds of communication
+export async function communicate<A extends Aggregator>(
+  networkWithContributions: Map<NodeID, [A, WeightsContainer]>,
+): Promise<Map<NodeID, WeightsContainer>> {
+  const communicationsRound =
+    networkWithContributions.first()?.[0].communicationRounds;
+  if (communicationsRound === undefined) throw new Error("empty network");
+
+  const network = networkWithContributions.map(([agg, _]) => agg);
+  let contributions = networkWithContributions.map(
+    ([_, contribution]) => contribution,
+  );
+
+  for (let r = 0; r < communicationsRound; r++) {
+    const nextContributions = network
+      .entrySeq()
+      .map<Promise<[NodeID, WeightsContainer]>>(async ([id, agg]) => [
+        id,
+        await agg.receiveResult(),
+      ])
+      .toArray();
+
+    network
+      .entrySeq()
+      .map<[NodeID, A, WeightsContainer]>(([id, agg]) => {
+        const contribution = contributions.get(id);
+        if (contribution === undefined)
+          throw new Error(`no contribution for ${id}`);
+        return [id, agg, contribution];
+      })
+      .forEach(([id, agg, contrib]) =>
+        agg
+          .makePayloads(contrib)
+          .entrySeq()
+          .forEach(([to, payload]) => network.get(to)?.add(id, payload, 0, r)),
+      );
+
+    contributions = Map(await Promise.all(nextContributions));
+  }
+
+  return contributions;
+}
