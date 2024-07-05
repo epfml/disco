73c0cdc412f7ecd7ea379224980bbecc7905d638
Julien Vignoud <julien.vignoud@epfl.ch>
2024-07-05 17:32:58 +0200

    Remove unused privacy parameters

--- discojs/src/default_tasks/mnist.ts
+++ discojs/src/default_tasks/mnist.ts
@@ -33,7 +33,7 @@ export const mnist: TaskProvider = {
         LABEL_LIST: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
         scheme: 'decentralized',
         noiseScale: undefined,
-        clippingRadius: 20,
+        clippingRadius: undefined,
         decentralizedSecure: true,
         minimumReadyPeers: 3,
         maxShareValue: 100,
