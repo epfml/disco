682a3a1d2808a6fa4bad529aa512cba51bc3bf34
Julien Vignoud <julien.vignoud@epfl.ch>
2024-07-23 13:55:43 +0200

    Make disco GIF responsive

--- webapp/src/components/home/Home.vue
+++ webapp/src/components/home/Home.vue
@@ -15,9 +15,11 @@
       </a>
   </div>
     <!-- Disco logo -->
-    <div class="flex flex-col justify-center mb-8 space-y-4">
-      <DiscoGIF class="mx-auto" />
-      <span class="text-xl text-center text-slate-600">
+    <div class="flex flex-col justify-center items-center mb-8 space-y-4">
+      <div class="md:max-w-md lg:max-w-lg">
+        <DiscoGIF class="mx-auto" />
+      </div>
+      <span class="text-lg md:text-3xl text-center text-slate-600">
         <span class="font-disco text-disco-cyan font-semibold">DIS</span
         >tributed
         <span class="font-disco text-disco-blue font-semibold">CO</span
