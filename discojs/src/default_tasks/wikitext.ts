9cea7f79e65ab12f1d9cd933dadaeaf96c24a946
tharvik <tharvik@users.noreply.github.com>
2024-07-24 14:40:26 +0200

    discojs/default/wikitext: add params count in text

--- discojs/src/default_tasks/wikitext.ts
+++ discojs/src/default_tasks/wikitext.ts
@@ -11,7 +11,13 @@ export const wikitext: TaskProvider = {
           preview: 'Train a language model (L)LM in your browser, collaboratively and from scratch.',
           overview: "You can train a GPT-2 model in your browser and in a collaborative manner on any textual dataset. As an example, you can try the Wikitext-103 dataset, composed of Wikipedia articles, widely used in natural language modeling, which you can download <a class='underline text-blue-400' target='_blank' href='https://dax-cdn.cdn.appdomain.cloud/dax-wikitext-103/1.0.1/wikitext-103.tar.gz'>here</a>. More information on how to connect the dataset at the next step."
         },
-        model: 'The model follows the exact GPT-2 architecture and is implemented in TensorFlow.js. The tokenizer used for preprocessing is the GPT-2 Byte-Pair encoding tokenizer. The model is trained via an Adam optimizer with unit gradient clipping and softmax cross-entropy loss. To accommodate all devices, the context length is currently kept at 128 and the batch size at 1.',
+        model: [
+          "The model follows the exact GPT-2 architecture and is implemented in TensorFlow.js.",
+          "The tokenizer used for preprocessing is the GPT-2 Byte-Pair encoding tokenizer.",
+          "The model is trained via an Adam optimizer with unit gradient clipping and softmax cross-entropy loss.",
+          "It has around 5M parameters.",
+          "To accommodate all devices, the context length is currently kept at 128 and the batch size at 1.",
+        ].join(" "),
         dataFormatInformation: 'You can use any natural language (text) dataset you like. For example the Wikitext-103 dataset is organized as a large text file, with each line representing a segment of raw text from Wikipedia articles.',
         dataExampleText: 'An example excerpt from the dataset is: <i>"For the first twenty years of its existence , the only staged performances of Parsifal took place in the Bayreuth Festspielhaus , the venue for which Wagner conceived the work ( except eight private performances for Ludwig II at Munich in 1884 and 1885 ) ."</i>',
         sampleDatasetLink: 'https://dax-cdn.cdn.appdomain.cloud/dax-wikitext-103/1.0.1/wikitext-103.tar.gz',
