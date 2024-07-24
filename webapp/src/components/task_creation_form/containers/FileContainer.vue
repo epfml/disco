aa544489386e99d5b4e2e357ab652c9f7504b3b6
Emmanuel <emmanuel.b.omont@gmail.com>
2024-07-24 19:13:27 +0200

    added the possibility to see that you actually uploaded a file

--- webapp/src/components/task_creation_form/containers/FileContainer.vue
+++ webapp/src/components/task_creation_form/containers/FileContainer.vue
@@ -23,8 +23,21 @@
             >
               <path
                 d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
-              /></svg><span class="block text-gray-400 font-normal">Drag and drop your file anywhere</span>
-            <span class="block text-gray-400 font-normal">or</span>
+              />
+            </svg>
+            <span
+              v-if="!fileName"
+              class="block text-gray-400 font-normal"
+            >
+              Drag and drop your file anywhere
+            </span>
+            <span
+              v-else
+              class="block text-gray-600 font-normal"
+            >
+              {{ fileName }}
+            </span>
+            <span v-if="!fileName" class="block text-gray-400 font-normal">or</span>
             <label
               :for="`hidden_${field.id}`"
               class="
@@ -39,7 +52,9 @@
                 focus:outline-none
               "
               :class="available ? 'hover:cursor-pointer hover:text-disco-cyan' : 'hover:cursor-not-allowed'"
-            >select file</label>
+            >
+              {{ fileName ? 'Change file' : 'Select file' }}
+            </label>
             <VeeField
               :id="field.id"
               v-model="fileName"
