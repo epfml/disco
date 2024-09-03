69b9dbeb885e64a38cd263ee8eed72642d9844eb
tharvik <tharvik@users.noreply.github.com>
2024-09-03 22:20:46 +0200

    webapp/sidebar: simplify

--- webapp/src/components/sidebar/SideBar.vue
+++ webapp/src/components/sidebar/SideBar.vue
@@ -1,69 +1,34 @@
 <template>
   <div>
     <!-- Mini Sidebar (LHS) -->
-    <nav
-      class="
-      flex flex-col flex-shrink-0
-      h-full
-      sm:px-4
-      px-2
-      py-4
-      border-r
-      "
-    >
+    <nav class="flex flex-col flex-shrink-0 h-full sm:px-4 px-2 py-4 border-r">
       <!-- Brand -->
       <div class="hidden sm:flex flex-shrink-0">
-        <a
-          class="
-          inline-block
-          text-2xl
-          font-bold
-          font-disco
-          tracking-wider
-          cursor-pointer
-        "
-          @click="goToHome"
+        <RouterLink
+          to="/"
+          class="inline-block text-2xl font-bold font-disco tracking-wider cursor-pointer"
         >
-        <DISCO />
-        </a>
+          <DISCO />
+        </RouterLink>
       </div>
+
       <!-- Mini Sidebar content-->
       <div class="flex flex-col items-center justify-center flex-1 space-y-4">
-        <!-- Go to Home page -->
-        <!-- Active classes "bg-primary text-white" -->
-        <!-- inActive classes "bg-primary-50 text-primary-lighter" -->
-        <SidebarButton
-          hover-text="home"
-          @click="goToHome()"
-        >
-          <HomeIcon />
+        <SidebarButton to="/" text="Home"> <HomeIcon /> </SidebarButton>
+
+        <SidebarButton to="/list" text="DISCOllaboratives">
+          <TasksIcon customClass="w-6 h-6" />
         </SidebarButton>
-        <!-- Go to Task List page -->
-        <SidebarButton
-          hover-text="DISCOllaboratives"
-          @click="goToTaskList()"
-        >
-          <TasksIcon customClass="w-6 h-6"/>
-        </SidebarButton>
-        <!-- Go to custom task creation page -->
-        <SidebarButton
-          hover-text="Create a new DISCOllaborative"
-          @click="goToNewCustomTask()"
-        >
+
+        <SidebarButton to="/create" text="Create a new DISCOllaborative">
           <CreateIcon />
         </SidebarButton>
-        <!-- Go to model evaluation page -->
-        <SidebarButton
-          hover-text="Evaluate models"
-          @click="goToEvaluate()"
-        >
+
+        <SidebarButton to="/evaluate" text="Evaluate models">
           <EvaluateIcon />
         </SidebarButton>
-        <!-- Go to Information page -->
-        <SidebarButton
-          hover-text="More on DISCO"
-          @click="goToInformation()"
-        >
+
+        <SidebarButton to="/information" text="More on DISCO">
           <InfoIcon />
         </SidebarButton>
       </div>
@@ -71,45 +36,14 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { RouterLink } from "vue-router";
 
-import SidebarButton from './containers/SidebarButton.vue'
-import HomeIcon from '@/assets/svg/HomeIcon.vue'
-import TasksIcon from '@/assets/svg/TasksIcon.vue'
-import CreateIcon from '@/assets/svg/CreateIcon.vue'
-import EvaluateIcon from '@/assets/svg/EvaluateIcon.vue'
-import InfoIcon from '@/assets/svg/InfoIcon.vue'
-import DISCO from '@/components/simple/DISCO.vue'
+import HomeIcon from "@/assets/svg/HomeIcon.vue";
+import TasksIcon from "@/assets/svg/TasksIcon.vue";
+import CreateIcon from "@/assets/svg/CreateIcon.vue";
+import EvaluateIcon from "@/assets/svg/EvaluateIcon.vue";
+import InfoIcon from "@/assets/svg/InfoIcon.vue";
+import DISCO from "@/components/simple/DISCO.vue";
 
-import { useValidationStore } from '@/store/validation'
-
-const router = useRouter()
-
-const validationStore = useValidationStore()
-
-function goToHome () {
-  router.push({ path: '/' })
-}
-
-function goToTaskList () {
-  router.push({ path: '/list' })
-}
-
-function goToNewCustomTask () {
-  router.push({ path: '/create' })
-}
-
-function goToEvaluate () {
-  // Reset the store
-  validationStore.step = 0
-  validationStore.modelID = undefined;
-  validationStore.mode = undefined;
-
-  router.push({ path: '/evaluate' })
-}
-
-function goToInformation () {
-  router.push({ path: '/information' })
-}
-
+import SidebarButton from "./SidebarButton.vue";
 </script>
---
+++ webapp/src/components/sidebar/SidebarButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-2 text-slate-500">
+    <span class="sr-only"> {{ text }} </span>
+    <RouterLink
+      :to="to"
+      v-tippy="{ content: text, placement: 'right', delay: 0, animation: 0 }"
+      class="hover:cursor-pointer"
+    >
+      <slot />
+    </RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  to: string;
+  text: string;
+}>();
+</script>
