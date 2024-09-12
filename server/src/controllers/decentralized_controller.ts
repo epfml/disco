61aeb9029da37719ec7d403600ff059dc760bd78
Julien Vignoud <julien.vignoud@epfl.ch>
2024-09-12 14:52:35 +0200

    Make peers wait when not enough participants

--- server/src/controllers/decentralized_controller.ts
+++ server/src/controllers/decentralized_controller.ts
@@ -2,7 +2,7 @@ import createDebug from "debug";
 import { v4 as randomUUID } from 'uuid'
 import msgpack from 'msgpack-lite'
 import type WebSocket from 'ws'
-import { Map, Set } from 'immutable'
+import { Set } from 'immutable'
 
 import { client } from '@epfml/discojs'
 
@@ -18,19 +18,17 @@ export class DecentralizedController extends TrainingController {
    * Set of nodes who have contributed.
    */
   private readyNodes = Set<client.NodeID>()
-  /**
-   * Map associating node ids to their open WebSocket connections.
-   */
-  private connections: Map<client.NodeID, WebSocket> = Map()
 
   handle (ws: WebSocket): void {
-    const minimumReadyPeers = this.task.trainingInformation.minNbOfParticipants
+    const minNbOfParticipants = this.task.trainingInformation.minNbOfParticipants
 
     // Peer id of the message sender
     let peerId = randomUUID()
     while (this.connections.has(peerId)) {
       peerId = randomUUID()
     }
+    const shortId = peerId.slice(0, 4)
+
 
     // How the server responds to messages
     ws.on('message', (data: Buffer) => {
@@ -42,24 +40,27 @@ export class DecentralizedController extends TrainingController {
         switch (msg.type) {
           // A new peer joins the network for a task
           case MessageTypes.ClientConnected: {
+            debug(`peer [%s] joined ${this.task.id}`, shortId)
             this.connections = this.connections.set(peerId, ws)
 
             // Answer with client id in an NewNodeInfo message
+            const waitForMoreParticipants = this.connections.size < minNbOfParticipants
             const msg: messages.NewDecentralizedNodeInfo = {
               type: MessageTypes.NewDecentralizedNodeInfo,
               id: peerId,
-              waitForMoreParticipants: this.readyNodes.size  < minimumReadyPeers // ground work for #718
+              waitForMoreParticipants
             }
-            debug(`peer ${peerId} joined ${this.task.id}`);
-
             ws.send(msgpack.encode(msg), { binary: true })
+            debug("Wait for more participant flag: %o", waitForMoreParticipants)
+            // Send an update to participants if we can start/resume training
+            this.checkIfEnoughParticipants(waitForMoreParticipants, peerId)
             break
           }
           // Send by peers at the beginning of each training round to get the list
           // of active peers for this round.
           case MessageTypes.PeerIsReady: {
             const peers = this.readyNodes.add(peerId)
-            if (peers.size >= minimumReadyPeers) {
+            if (peers.size >= minNbOfParticipants) {
               this.readyNodes = Set()
 
               peers
@@ -105,5 +106,28 @@ export class DecentralizedController extends TrainingController {
         debug("when processing WebSocket message: %o", e);
       }
     })
+    // Setup callback for client leaving the session
+    ws.on('close', () => {
+      // Remove the participant when the websocket is closed
+      this.connections = this.connections.delete(peerId)
+      debug("client [%s] left", shortId)
+
+      // Check if we dropped below the minimum number of participant required
+      // or if we are already waiting for new participants to join
+      if (this.connections.size >= minNbOfParticipants ||
+        this.waitingForMoreParticipants
+      ) return
+
+      this.waitingForMoreParticipants = true
+      // Tell remaining participants to wait until more participants join
+      this.connections
+        .forEach((participantWs, participantId) => {
+          debug("Telling remaining client [%s] to wait for participants", participantId.slice(0, 4))
+          const msg: client.messages.WaitingForMoreParticipants = {
+            type: MessageTypes.WaitingForMoreParticipants
+          }
+          participantWs.send(msgpack.encode(msg))
+        })
+    }) 
   }
 }
