be730f32ec8ac361743df77f987444f1018ad279
tharvik <tharvik@users.noreply.github.com>
2024-10-08 15:32:09 +0200

    webapp/store/models: fix memory exhaustion
    Closes #800

--- webapp/src/store/models/__tests__/models.spec.ts
+++ webapp/src/store/models/__tests__/models.spec.ts
@@ -1,27 +1,23 @@
 import { createPinia, setActivePinia } from "pinia";
-import piniaPersited from "pinia-plugin-persistedstate";
+import { createPersistedStatePlugin } from "pinia-plugin-persistedstate-2";
 import { beforeEach, expect, it } from "vitest";
+import { createApp } from "vue";
 
 import { models as discoModels } from "@epfml/discojs";
 
-import { useModelsStore } from "../models";
-import { createApp } from "vue";
+import { useModelsStore } from "../index";
 
 const app = createApp({});
 beforeEach(() => {
-  const pinia = setActivePinia(createPinia().use(piniaPersited));
+  const pinia = setActivePinia(createPinia().use(createPersistedStatePlugin()));
   app.use(pinia);
   setActivePinia(pinia);
 });
 
-it("persists", async () => {
+it("stores model", async () => {
   const models = useModelsStore();
 
   const id = await models.add("task id", new discoModels.GPT());
-
-  models.$persist();
-  models.$hydrate();
-
   expect(models.infos.size).to.equal(1);
   expect(await models.get(id)).to.be.an.instanceof(discoModels.GPT);
 });
