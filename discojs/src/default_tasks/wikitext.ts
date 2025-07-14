9cd61425e23e0c83131674f4aa1c0eccd605a52b
Julien Vignoud <julien.vignoud@epfl.ch>
2025-07-14 14:58:54 +0200

    datasets/populate and discojs/default_tasks: fix wikitext broken link

--- discojs/src/default_tasks/wikitext.ts
+++ discojs/src/default_tasks/wikitext.ts
@@ -20,7 +20,7 @@ export const wikitext: TaskProvider<'text'> = {
         ].join(" "),
         dataFormatInformation: 'You can use any natural language (text) dataset you like. For example the Wikitext-103 dataset is organized as a large text file, with each line representing a segment of raw text from Wikipedia articles.',
         dataExampleText: 'An example excerpt from the dataset is: <i>"For the first twenty years of its existence , the only staged performances of Parsifal took place in the Bayreuth Festspielhaus , the venue for which Wagner conceived the work ( except eight private performances for Ludwig II at Munich in 1884 and 1885 ) ."</i>',
-        sampleDatasetLink: 'https://dax-cdn.cdn.appdomain.cloud/dax-wikitext-103/1.0.1/wikitext-103.tar.gz',
+        sampleDatasetLink: 'https://storage.googleapis.com/deai-313515.appspot.com/wikitext.zip',
         sampleDatasetInstructions: 'Opening the link should start downloading a zip file. Unzip it and drag and drop the training set named "wiki.train.tokens" in the field below (or use the "Select File" button). Even though the file extension is ".tokens" it is indeed a text file. You can use "wiki.test.tokens" at the evaluation step after training a language model.'
       },
       trainingInformation: {
