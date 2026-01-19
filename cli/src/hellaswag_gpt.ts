9fbe6b34e460ad87d143fc9e2352c766ef8ca28a
Julien Vignoud <julien.vignoud@epfl.ch>
2026-01-19 16:23:24 +0100

    cli/hellaswag: remove logging to file and string casting

--- cli/src/hellaswag_gpt.ts
+++ cli/src/hellaswag_gpt.ts
@@ -1,36 +1,26 @@
-// import fs from 'fs';
 import fsPromise from 'node:fs/promises';
-
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { parse } from 'ts-command-line-args'
 
 import '@tensorflow/tfjs-node';
-import fs from 'node:fs';
 import path from 'node:path';
 import { models, serialization, Tokenizer } from '@epfml/discojs';
 import { loadHellaSwag } from '@epfml/discojs-node';
-// import { AutoTokenizer } from '@xenova/transformers';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const logLines: string[] = [];
-function log(message: string) {
-    console.log(message);
-    logLines.push(message);
-}
 
 async function evaluateModel(model: models.GPT | models.ONNXModel, numDataPoints = -1) {
     const hellaswagDataset: models.HellaSwagDataset = await loadHellaSwag(numDataPoints)
     const tokenizer = await Tokenizer.from_pretrained('Xenova/gpt2');
-    log('Starting the HellaSwag benchmark...');
+    console.log('Starting the HellaSwag benchmark...');
 
     const start = Date.now();
     const accuracy = await models.evaluate_hellaswag(model, tokenizer, hellaswagDataset, true);
     const duration = ((Date.now() - start) / 1000).toFixed(2);
 
-    log(`Final accuracy: ${(accuracy * 100).toFixed(2)}%`);
-    log(`Evaluation Time: ${duration} seconds`);
+    console.log(`Final accuracy: ${(accuracy * 100).toFixed(2)}%`);
+    console.log(`Evaluation Time: ${duration} seconds`);
 }
 
 const ModelTypes = ['onnx', 'gpt-tfjs-random', 'gpt-tfjs-pretrained'] as const;
@@ -44,11 +34,15 @@ interface HellaSwagArgs {
     help?: boolean
 }
 
+function castModelType(raw: string): ModelType {
+    for (const t of ModelTypes) if (raw === t) return t
+    throw new Error(`Invalid model type: ${raw}`)
+}
+
 async function main(): Promise<void> {
-    const defaultPretrainedModelPath = path.join(__dirname, "..", "..", "onnx-converter", "assets", "model.json")
     const args = parse<HellaSwagArgs>({
         model: {
-            type: (raw: string) => raw as ModelType,
+            type: (raw: string) => castModelType(raw),
             description: `Model type, one of ${ModelTypes.toString()}`,
             defaultValue: 'onnx'
         },
@@ -64,7 +58,7 @@ async function main(): Promise<void> {
         pretrainedModelPath: {
             type: String,
             description: 'If specifying gpt-tfjs-pretrained, provide the relative path to the TF.js pretrained model',
-            defaultValue: defaultPretrainedModelPath
+            defaultValue: path.join(__dirname, "..", "..", "onnx-converter", "assets", "model.json")
         },
         help: {
             type: Boolean,
@@ -74,21 +68,18 @@ async function main(): Promise<void> {
         }
     }, { helpArg: 'help' })
 
-    const logFile = path.join(__dirname, args.logFile);
-    fs.writeFileSync(logFile, '', 'utf-8'); // Clear the log file
-
     let model: models.GPT | models.ONNXModel | undefined;
     switch (args.model) {
         case 'onnx':
-            log("Using ONNX pretrained model Xenova/gpt2")
+            console.log("Using ONNX pretrained model Xenova/gpt2")
             model = await models.ONNXModel.init_pretrained('Xenova/gpt2');
             break;
-            case 'gpt-tfjs-random':
-            log("Using GPT-TFJS with random initialization")
+        case 'gpt-tfjs-random':
+            console.log("Using GPT-TFJS with random initialization")
             model = new models.GPT({ seed: 42 });
             break;
-            case 'gpt-tfjs-pretrained':
-            log("Using GPT-TFJS with pretrained weights")
+        case 'gpt-tfjs-pretrained':
+            console.log("Using GPT-TFJS with pretrained weights")
             if (args.pretrainedModelPath === undefined) {
                 throw new Error("If choosing gpt-tfjs-pretrained, provide the relative path to the TF.js pretrained model `pretrainedModelPath")
             }
@@ -98,8 +89,7 @@ async function main(): Promise<void> {
     } 
     await evaluateModel(model, args.numDataPoints);
 
-    fs.writeFileSync(logFile, logLines.join('\n'), 'utf-8');
-    console.log(`\nResults written to ${logFile}`);
+    console.log("Benchmark completed!")
 }
 
 main().catch(console.error);
