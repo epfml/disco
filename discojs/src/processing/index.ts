afeb69039f63d8d4c8ae7d3285ee1034ab6d3f0c
hugo <hugo.lepeytre@epfl.ch>
2026-06-25 11:06:08 +0200

    chore: fix lint errors in discojs

--- discojs/src/processing/index.ts
+++ discojs/src/processing/index.ts
@@ -109,7 +109,7 @@ export function postprocess<D extends DataType>(
   switch (task.dataType) {
     case "image": {
       // cast as typescript doesn't reduce generic type
-      const index = encoded as DataFormat.ModelEncoded["image"][1];
+      const index = encoded;
       const labels = List(task.trainingInformation.LABEL_LIST);
 
       const v = labels.get(index);
@@ -118,13 +118,13 @@ export function postprocess<D extends DataType>(
     }
     case "tabular": {
       // cast as typescript doesn't reduce generic type
-      const v = encoded as DataFormat.ModelEncoded["tabular"][1];
+      const v = encoded;
 
       return v as DataFormat.Inferred[D];
     }
     case "text": {
       // cast as typescript doesn't reduce generic type
-      const token = encoded as DataFormat.ModelEncoded["text"][1];
+      const token = encoded;
 
       return task.trainingInformation.tokenizer.decode([
         token,
