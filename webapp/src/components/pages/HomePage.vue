2322376b3af1ff2d1e68c0188cc1a0ee5f339ca7
hugo <hugo.lepeytre@epfl.ch>
2026-07-09 10:57:03 +0200

    feat: scroll indicator fixed on the page

--- webapp/src/components/pages/HomePage.vue
+++ webapp/src/components/pages/HomePage.vue
@@ -36,6 +36,9 @@
       <div class="flex justify-center">
         <CTAButton> Start training </CTAButton>
       </div>
+      <div class="flex justify-center">
+        <ScrollIndicator />
+      </div>
     </div>
 
     <!-- 2nd screen -->
@@ -291,20 +294,10 @@
         </div>
       </div>
     </div>
-
-    <!-- End of content -->
-    <div ref="bottomSentinel" class="h-px"></div>
-
-    <!-- Scroll indicator -->
-    <div v-if="showScrollIndicator" class="flex justify-center">
-      <ScrollIndicator />
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, onBeforeUnmount, useTemplateRef } from "vue";
-
 import DiscoGIF from "@/assets/gif/DiscoGIF.vue";
 import DiscoLogo from "@/assets/logos/DiscoLogo.vue";
 import TensorflowLogo from "@/assets/logos/TensorflowLogo.vue";
@@ -326,21 +319,4 @@ import DiscoParticlesIcon from "@/assets/svg/DiscoParticlesIcon.vue";
 import ScrollIndicator from "../simple/ScrollIndicator.vue";
 
 const themeStore = useThemeStore();
-
-// Scroll indicator. Disappears when scrolling to the bottom of the page.
-const bottomSentinel = useTemplateRef<HTMLElement>("bottomSentinel");
-const showScrollIndicator = ref(true);
-let observer: IntersectionObserver;
-
-onMounted(() => {
-  if (!bottomSentinel.value) return;
-  observer = new IntersectionObserver(([entry]) => {
-    showScrollIndicator.value = !entry.isIntersecting;
-  });
-  observer.observe(bottomSentinel.value);
-});
-
-onBeforeUnmount(() => {
-  observer?.disconnect();
-});
 </script>
--- webapp/src/components/simple/ScrollIndicator.vue
+++ webapp/src/components/simple/ScrollIndicator.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed flex flex-col items-center bottom-2 py-2 text-lg text-white bg-disco-cyan font-disco rounded-full duration-200 hover:bg-transparent dark:hover:bg-transparent hover:outline-solid hover:outline-2 hover:outline-disco-cyan dark:hover:outline-disco-light-cyan hover:text-disco-cyan dark:hover:text-disco-light-cyan"
+    class="flex flex-col items-center py-2 px-2 text-disco bg-disco font-disco rounded-full duration-200 hover:outline-solid hover:outline-2 hover:outline-disco-cyan dark:hover:outline-disco-light-cyan hover:text-disco-cyan dark:hover:text-disco-light-cyan"
     @click="scrollDown"
   >
     <i class="fa-solid fa-chevron-down animate-bounce" />
