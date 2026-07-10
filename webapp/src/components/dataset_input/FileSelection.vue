66aa19a7e07dd8eedcb5c9a00ae2cc1d6b57fafe
hugo <hugo.lepeytre@epfl.ch>
2026-07-10 18:55:23 +0200

    fix: make select button look similar to clear button

--- webapp/src/components/dataset_input/FileSelection.vue
+++ webapp/src/components/dataset_input/FileSelection.vue
@@ -36,7 +36,7 @@
         </p>
         <label class="mb-6">
           <span
-            class="px-4 py-2 min-w-32 text-lg uppercase text-white bg-disco-cyan rounded-sm duration-200 hover:bg-white dark:hover:bg-slate-950 hover:outline-solid hover:outline-disco-cyan hover:outline-2 hover:text-disco-cyan hover:cursor-pointer"
+            class="px-4 py-2 min-w-32 text-lg capitalize text-white bg-disco-cyan font-disco rounded-full duration-200 hover:bg-transparent dark:hover:bg-transparent hover:outline-solid hover:outline-2 hover:outline-disco-cyan dark:hover:outline-disco-light-cyan hover:text-disco-cyan dark:hover:text-disco-light-cyan"
           >
             select
           </span>
