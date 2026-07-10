40b3bc4673ca5bd485e8d8981c290a5f74da0b0f
hugo <hugo.lepeytre@epfl.ch>
2026-07-10 15:02:58 +0200

    fix: more uniform scrolling

--- webapp/src/components/simple/ScrollIndicator.vue
+++ webapp/src/components/simple/ScrollIndicator.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-center py-2 px-2 text-disco bg-disco font-disco rounded-full duration-200 hover:outline-solid hover:outline-2 hover:outline-disco-cyan dark:hover:outline-disco-light-cyan hover:text-disco-cyan dark:hover:text-disco-light-cyan"
+    class="flex flex-col items-center py-2 px-2 text-disco bg-disco font-disco rounded-full duration-200 hover:text-disco-cyan hover:scale-150 cursor-pointer"
     @click="scrollDown"
   >
     <i class="fa-solid fa-chevron-down animate-bounce" />
@@ -8,15 +8,5 @@
 </template>
 
 <script lang="ts" setup>
-import { inject, type Ref } from "vue";
-
-// Inject scrollContainer from BaseLayout
-const scrollContainer = inject<Ref<HTMLElement | null>>("scrollContainer");
-
-function scrollDown() {
-  scrollContainer?.value?.scrollBy({
-    top: window.innerHeight,
-    behavior: "smooth",
-  });
-}
+import { scrollDown } from "@/utils";
 </script>
