8fc48edd053adffaa575d0217dd0aa37034a9524
Julien Vignoud <julien.vignoud@epfl.ch>
2026-08-18 14:12:35 +0200

    feat: dynamically set the dev server URL to LAN IP depending on vite --host

--- webapp/env.d.ts
+++ webapp/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** Full URL of the Disco server. When set to "localhost", the URL is derived from window.location. */
+  readonly VITE_SERVER_URL: string;
+  readonly VITE_SERVER_PORT?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
