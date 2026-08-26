dd48dfdc17710a75ea7729d4f342fca97b6c594a
Julien Vignoud <julien.vignoud@epfl.ch>
2026-08-26 12:39:40 +0200

    fix: make button look consistent

--- webapp/src/components/simple/CTAButton.vue
+++ webapp/src/components/simple/CTAButton.vue
@@ -2,7 +2,7 @@
 <template>
   <RouterLink
     to="/list"
-    class="px-6 py-3 min-w-32 text-xl text-white tracking-wide font-disco bg-disco-cyan rounded-full duration-200 hover:scale-105 transform"
+    class="px-6 py-3 min-w-32 text-xl text-white tracking-wide font-disco bg-disco-cyan rounded-full duration-200 hover:scale-105 capitalize hover:bg-transparent dark:hover:bg-transparent hover:outline-solid hover:outline-2 hover:outline-disco-cyan dark:hover:outline-disco-light-cyan hover:text-disco-cyan dark:hover:text-disco-light-cyan"
   >
     <slot />
   </RouterLink>
